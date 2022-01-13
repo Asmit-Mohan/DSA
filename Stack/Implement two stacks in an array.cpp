0f715c5da307f96b88e4856d5dcdfbc21c9042f5
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-13 12:58:00 +0530

    Create Implement two stacks in an array.cpp

---
+++ Stack/Implement two stacks in an array.cpp
@@ -0,0 +1,36 @@
+void twoStacks :: push1(int x)
+{
+    if(top1<top2)
+    {
+        arr[++top1]=x;
+    }
+}
+void twoStacks ::push2(int x)
+{
+    if(top1<top2)
+    {
+        arr[--top2]=x;
+    }
+}
+int twoStacks ::pop1()
+{
+	if(top1==-1)
+	{
+	    return -1;
+	}
+	else
+	{
+	return arr[top1--];
+	}
+}
+int twoStacks :: pop2()
+{
+	if(top2==size)
+	{
+	    return -1;
+	}
+	else
+	{
+	    return arr[top2++];
+	}
+}
