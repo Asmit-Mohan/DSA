61141c50b097b5c48119c7e8d216d8f2533c4920
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-14 13:45:50 +0530

    Create Game with String.cpp

---
+++ Heap/Game with String.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+public:
+    int minValue(string s, int k)
+    {
+        int hash[27]={0};
+        priority_queue <int> pq;
+        int sum=0;
+        for(int i=0;i<s.size();i++)
+        {
+            hash[s[i]-'a']++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(hash[i]>0)
+            {
+                pq.push(hash[i]);
+            }
+        }
+        while(k--)
+        {
+             int temp=pq.top();
+             pq.pop();
+             temp--;
+             pq.push(temp);
+        }
+        while(pq.size()>0)
+        {
+            int temp=pq.top();
+            pq.pop();
+            sum=sum+(temp*temp);
+        }
+        return sum;
+    }
+};
