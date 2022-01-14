101e50f82ad1dcc46aaa3d8ceac733d78381aab3
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-14 11:36:18 +0530

    Create Top K Frequent Elements.cpp

---
+++ Heap/Top K Frequent Elements.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k)
+    {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        unordered_map<int, int> ump;
+        for (int i=0;i<nums.size();i++)
+        {
+            ump[nums[i]]++;
+        }
+        for (auto i=ump.begin();i!=ump.end();i++)
+        {
+            pq.push({i->second, i->first});   
+            if (pq.size() > k)
+            {
+                pq.pop();
+            }
+        }
+        vector<int> v;
+        while (!pq.empty())
+        {    
+            v.push_back(pq.top().second);
+            pq.pop();
+        }
+        return v;
+    }
+};
