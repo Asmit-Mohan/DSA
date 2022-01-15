4f33950156eb252ad1c1f99940060c3bbec9d481
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-15 17:47:38 +0530

    Create Equal Sum Partitions.cpp

---
+++ Dynamic Programming/Equal Sum Partitions.cpp
@@ -0,0 +1,51 @@
+class Solution
+{
+public:
+    bool canPartition(vector<int>& arr)
+    {
+        int n=arr.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+        }
+        if(sum%2!=0)
+        {
+            return 0;
+        }
+        else
+        {
+        sum=sum/2;
+        int w=sum;
+        int dp[n+1][w+1];
+       for(int i=0;i<n+1;i++)
+       {
+           for(int j=0;j<w+1;j++)
+           {
+               if(i==0)
+               {
+                  dp[i][j]=0; 
+               }
+               if(j==0)
+               {
+                  dp[i][j]=1;
+               }
+           }
+       }
+       for(int i=1;i<n+1;i++)
+       {
+           for(int j=1;j<w+1;j++)
+           {
+               if(arr[i-1]<=j)
+               {
+                   dp[i][j]=max(dp[i-1][j-arr[i-1]],dp[i-1][j]);
+               }
+               else
+               {
+                   dp[i][j]=dp[i-1][j];
+               }
+           }
+       }
+       return dp[n][w];
+      }
+};
