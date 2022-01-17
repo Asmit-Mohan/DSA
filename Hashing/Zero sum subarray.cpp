ee9d916898d8a8a14356b7d616819e46b0fb7522
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-17 15:27:22 +0530

    Create Zero sum subarray.cpp

---
+++ Hashing/Zero sum subarray.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+    ll findSubarray(vector<ll> arr, int n)
+    {
+         ll sum=0;
+         ll count=0;
+         unordered_map<ll,ll>mp;
+         for(int i=0;i<n;i++)
+         {
+             sum=sum+arr[i];
+             if(sum==0)
+             {
+                 count++;
+             }
+             if(mp.find(sum)!=mp.end())
+             {
+                 count+=mp[sum];
+             }
+             mp[sum]++;
+         }
+         return count;
+    }
+};
