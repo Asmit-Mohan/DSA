61f6b669edde15edf16bdc65ba0e90722bca5185
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-17 17:57:12 +0530

    Create Remove Duplicates.cpp

---
+++ STL/Remove Duplicates.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string str="Geeks for Geeks A Computer Science portal for Geeks";
+	string res;
+	unordered_set<string>s;
+	stringstream ss(str);
+	string word;
+	while(ss>>word)
+	{
+		s.insert(word);
+	}
+	for(auto itr = s.begin(); itr != s.end(); ++itr)
+	{
+		res=res+(*itr)+" ";
+	}
+	cout<<res;
+	return 0;
+}
