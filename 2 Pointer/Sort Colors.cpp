84b3599891dfd0e680ec9bddc22d00e307b5b293
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-17 11:48:32 +0530

    Create Sort Colors.cpp

---
+++ 2 Pointer/Sort Colors.cpp
@@ -0,0 +1,73 @@
+/*Brute Force*/
+class Solution
+{
+public:
+    void sortColors(vector<int>& nums)
+    {
+        int z=0;
+        int o=0;
+        int t=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+            {
+                z++;
+            }
+            else if(nums[i]==1)
+            {
+                o++;
+            }
+            else
+            {
+                t++;
+            }
+        }
+        nums.clear();
+        while(z--)
+        {
+            nums.push_back(0);
+        }
+        while(o--)
+        {
+            nums.push_back(1);
+        }
+        while(t--)
+        {
+            nums.push_back(2);
+        }  
+    }
+};
+
+
+/*2 pointer*/
+class Solution
+{
+public:
+    void sortColors(vector<int>& nums)
+    {
+        int i = 0;
+        int l = 0;
+        int r = nums.size()-1;
+        while(l<=r)
+        {
+           if(nums[l]==1)
+           {
+               l++;
+               continue;
+           }
+           if(nums[l]==0)
+           {
+               swap(nums[i],nums[l]);
+               l++;
+               i++;
+               continue;
+           }
+           if(nums[r]==2)
+           {
+               r--;
+               continue;
+           }
+           swap(nums[l],nums[r]);
+        }
+    }
+};
