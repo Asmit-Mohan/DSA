6fec4a7c7bdb5ab2cff35795345c614117ea0ab8
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-17 11:47:55 +0530

    Create Two numbers with sum closest to zero.cpp

---
+++ 2 Pointer/Two numbers with sum closest to zero.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+        int closestToZero(int arr[], int n)
+        {
+            sort(arr,arr+n);
+            int ans=INT_MAX;
+            int res=-1;
+            int l=0,r=n-1;
+            while(l<r)
+            {
+                int diff=arr[l]+arr[r];
+                if(diff==0)
+                {
+                    return 0;
+                }
+                else if(diff<0)
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+                if(abs(diff)<ans)
+                {
+                    ans=abs(diff);
+                    res=diff;
+                }
+                else if(abs(diff)==ans)
+                {
+                    res=max(res,diff);
+                }
+            }
+            return res;
+        }
+};
