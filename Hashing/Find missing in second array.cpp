24a7dc7dfe1df001e62d1835bf312c08731e7e71
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-17 15:24:09 +0530

    Create Find missing in second array.cpp

---
+++ Hashing/Find missing in second array.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+	public:
+	vector<long long> findMissing(long long A[], long long B[], int N, int M) 
+	{ 
+	    unordered_set<long long>s;
+	    vector<long long>v;
+	    for(int i=0;i<M;i++)
+	    {
+	        s.insert(B[i]);
+	    }
+	    for(int i=0;i<N;i++)
+	    {
+           if(s.find(A[i])==s.end())
+           {
+               v.push_back(A[i]);
+           }
+	    }
+	    return v;
+	} 
+};
