83508769eb29d76d9d61b83ffb085e2d4ab686a0
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-17 14:56:23 +0530

    Create Find all pairs with a given sum.cpp

---
+++ Hashing/Find all pairs with a given sum.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
+    {
+        vector<pair<int,int>>v;
+        unordered_set<int>s;
+        for(int i=0;i<M;i++)
+        {
+            s.insert(B[i]);
+        }
+        for(int i=0;i<N;i++)
+        {
+            if(s.find(X-A[i])!=s.end())
+            {
+                v.push_back({A[i],X-A[i]});
+            }
+        }
+        sort(v.begin(), v.end());
+        return v;
+    }
+};
