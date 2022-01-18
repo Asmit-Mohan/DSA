9b954f58cb9e2de0ba0bd94af9d1a91e80ae7f0d
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-18 12:49:46 +0530

    Create Print N-bit binary numbers having more 1s than 0s.cpp

---
+++ Recursion/Print N-bit binary numbers having more 1s than 0s.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:	
+    void solve(string op,int one,int zero,int n,vector<string>&ans)
+    {
+        if(n==0)
+        {
+            ans.push_back(op);
+            return;
+        }
+        string op1=op;
+        op1.push_back('1');
+        solve(op1,one-1,zero,n-1,ans);
+        if(zero>one)
+        {
+            string op2=op;
+            op2.push_back('0');
+            solve(op2,one,zero-1,n-1,ans);
+        }
+        return;
+    }
+	vector<string> NBitBinary(int N)
+	{
+	    string op;
+	    int one=N-1;
+	    int zero=N;
+	    op.push_back('1');
+	    vector<string>ans;
+	    solve(op,one,zero,N-1,ans);
+	    return ans;
+	}
+};
