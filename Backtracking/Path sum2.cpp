8781c0f38db9674c0e6f651bd60b5ed0f2e23b3e
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-19 20:39:23 +0530

    Create Path sum2.cpp

---
+++ Backtracking/Path sum2.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    void solve(TreeNode* root,int target,vector<int>temp,vector<vector<int>>&ans)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        temp.push_back(root->val);
+        if(root->val==target&&root->left==NULL&&root->right==NULL)  //leaf node
+        {
+            ans.push_back(temp);
+            return;
+        }
+        solve(root->left,target-root->val,temp,ans);
+        solve(root->right,target-root->val,temp,ans);
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int sum)
+    {
+           vector<vector<int>>v;
+           vector<int>temp;
+           solve(root,sum,temp,v);
+           return v;
+    }
+};
