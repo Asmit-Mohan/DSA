cb79ebe2ff2a2bd4ec23735e316bdc317048e416
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-21 17:22:48 +0530

    Create Nth node from end of linked list.cpp

---
+++ Linked List/Nth node from end of linked list.cpp
@@ -0,0 +1,24 @@
+int getNthFromLast(Node *head, int n)
+{
+    Node* temp=head;
+    int m=0;
+    while(temp!=NULL)
+    {
+        temp=temp->next;
+        m++;
+    }
+    if(n>m)
+    {
+        return -1;
+    }
+    else
+    {
+    m=m-n+1;
+    temp=head;
+    while(--m)
+    {
+        temp=temp->next;
+    }
+    return temp->data;
+    }
+}
