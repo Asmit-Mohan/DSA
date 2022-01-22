ce2f1b6e66bc4fd7cecb7e0679497d265cf94a32
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-22 23:53:04 +0530

    Create Check if string is rotated by two places.cpp

---
+++ string/Check if string is rotated by two places.cpp
@@ -0,0 +1,52 @@
+class Solution
+{
+    public:
+    bool isRotated(string str1, string str2)
+    {
+        if(str1.length()!=str2.length())
+        {
+            return 0;
+        }
+        string op;
+        int flag1=1;
+        int flag2=1;
+        for(int i=2;i<str1.length();i++)
+        {
+            op+=str1[i];
+        }
+        op+=str1[0];
+        op+=str1[1];
+        for(int i=0;i<str1.length();i++)
+        {
+            if(op[i]!=str2[i])
+            {
+                flag1=0;
+            }
+        }
+        
+        op.clear();
+        
+        op+=str1[str1.length()-2];
+        op+=str1[str1.length()-1];
+        for(int i=0;i<str1.length()-1;i++)
+        {
+            op+=str1[i];
+        }
+        for(int i=0;i<str1.length();i++)
+        {
+            if(op[i]!=str2[i])
+            {
+                flag2=0;
+            }
+        }
+        
+        if(flag1==0&&flag2==0)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+};
