d08450f44f17d030c48990b251cd6e6050cc396f
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-22 11:29:59 +0530

    Create Starting Point of the loop(LC Cycle 2).cpp

---
+++ Linked List/Starting Point of the loop(LC Cycle 2).cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return NULL;
+        }
+        
+        ListNode* slow=head;
+        ListNode* fast=head;
+        int flag=0;
+        
+        while(fast!=NULL&&fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            {
+                flag=1;
+                break;
+            }
+        }
+        
+        if (slow != fast||flag==0)  /*No Cycle*/
+        {
+            return NULL;
+        }
+        else
+        {
+            slow=head;
+            while(slow!=fast)
+            {
+                slow=slow->next;
+                fast=fast->next;
+            }
+        }
+        return fast;
+    }
+};
