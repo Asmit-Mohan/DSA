779ca2fc9e2641f45a8974ca2ec44165b5bd9536
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-22 13:49:29 +0530

    Update Absolute List sorting.cpp

--- Linked List/Absolute List sorting.cpp
+++ Linked List/Absolute List sorting.cpp
@@ -1,18 +1,15 @@
 class Solution
 {
 public: 
-    void sortList  /*Selection Sort Mechanisms*/
+    void sortList(Node** head)  /*Selection Sort Mechanisms*/
     {
         Node* ptr1=*head;  /*will always main node*/
         Node* ptr2=*head;  /*will iterate from main+1 to end to compare*/
         while(ptr1!=NULL)
         {
-            int x;
             if(ptr1->data>ptr2->data) /*Comparing 1st node with every next node*/
             {
-                x=ptr1->data;
-                ptr1->data=ptr2->data;
-                ptr2->data=x;
+                 swap(ptr1->data,ptr2->data);
             }
             ptr2=ptr2->next;
             if(ptr2==NULL&&ptr1!=NULL) /*if last node ends then again comparision start from 2nd node*/
