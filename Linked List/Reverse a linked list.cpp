50941dc5305ea11babb728020055aeaade376e63
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-22 09:23:40 +0530

    Create Reverse a linked list.cpp

---
+++ Linked List/Reverse a linked list.cpp
@@ -0,0 +1,40 @@
+/*Approach 1 :- Recursive O(N) Time and O(N) Recursive Space*/
+
+class Solution
+{
+    public:
+    struct Node* reverseList(struct Node *head)
+    {
+        if(head==NULL||head->next==NULL)
+        {
+            return head;  /*always return head of reversed linked list*/
+        }
+        
+        struct Node* newHead = reverseList(head->next);  /*newHead Will Remain Contant ie. always point to end of list*/
+        
+        struct Node* headNext = head->next;
+        headNext->next=head;
+        head->next=NULL;
+        return newHead;
+    }
+};
+
+/*Approach 2 :- Iterative O(N) Time and O(1) Constant Space*/
+
+class Solution
+{
+    public:
+    struct Node* reverseList(struct Node *head)
+    {
+        Node* curr=head;
+        Node* prev=NULL;
+        while(curr!=NULL)
+        {
+            Node* temp=curr->next;   /*Always points to next node*/
+            curr->next=prev;         /*current node will store references of previous node*/
+            prev=curr;               /*prev moves forward*/
+            curr=temp;               /*current also moves forward that is why its next was stored in temp earlier.*/
+        }
+        return prev;
+    }
+};
