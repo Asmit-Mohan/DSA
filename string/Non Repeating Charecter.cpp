7abe7eb4adf6d9c7cd4c572749d360ce5b3fc1d1
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-22 23:08:03 +0530

    Create Non Repeating Charecter.cpp

---
+++ string/Non Repeating Charecter.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    char nonrepeatingCharacter(string str)
+    {
+        unordered_map<char,int>mp;
+        for(int i=0;i<str.length();i++)
+        {
+            mp[str[i]]++;
+        }
+        for(int i=0;i<str.length();i++)
+        {
+            if(mp[str[i]]==1)
+            {
+                return str[i];
+            }
+        }
+        return '$';
+    }
+};
