4a746eb0c264298925ddd464603d627457601870
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-23 17:48:21 +0530

    Update Toposort.cpp

--- Graph/Toposort.cpp
+++ Graph/Toposort.cpp
@@ -37,5 +37,46 @@ class Solution
 	}
 };
 
-/*Approach 2 (BFS)*/
+/*Approach 2 (BFS) --> (kahn's Algorithms)*/
 
+class Solution
+{
+	public:
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int>ans;
+	    queue<int>q;
+	    vector<int>indegree(V,0);
+	    
+	    for(int i=0;i<V;i++)
+	    {
+	        for(auto it : adj[i])
+	        {
+	            indegree[it]++;
+	        }
+	    }
+	    for(int i=0;i<indegree.size();i++)
+	    {
+	        if(indegree[i]==0)
+	        {
+	            q.push(i);
+	        }
+	    }
+	    
+	    while(!q.empty())
+	    {
+	        int temp=q.front();
+	        q.pop();
+	        ans.push_back(temp);
+	        for(auto it : adj[temp])
+	        {
+	            indegree[it]--;
+	            if(indegree[it]==0)
+	            {
+	                q.push(it);
+	            }
+	        }
+	    }
+	    return ans;
+	}
+};
