33d2537c397e015d551d04f73869196d62c1b9e6
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-23 22:32:42 +0530

    Create Winner of the election.cpp

---
+++ string/Winner of the election.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+    public:
+    vector<string> winner(string arr[],int n)
+    {
+        unordered_map<string,int>mp;
+        vector<string>res;
+        string ans;
+        int fre=-1;
+        for(int i=0;i<n;i++)
+        {
+            mp[arr[i]]++;
+        }
+        for(auto x : mp)
+        {
+            if(x.second>fre)
+            {
+                fre=x.second;
+                ans=x.first;
+            }
+            else if(x.second==fre)
+            {
+                int comp = ans.compare(x.first);
+                if(comp>0)
+                {
+                    ans=x.first;
+                }
+            }
+        }
+        res.push_back(ans);
+        res.push_back(to_string(fre));
+        return res;
+    }
+};
