d1cca8af99655d7fdfdba791928acb05a273c872
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-23 23:04:48 +0530

    Create Check For Subsequence.cpp

---
+++ string/Check For Subsequence.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+    bool isSubSequence(string A, string B) 
+    {
+        int i=0;
+        int j=0;
+        int temp=A.length();
+        int count=0;
+        while(i<A.length()&&j<B.length())
+        {
+            if(A[i]==B[j])
+            {
+                i++;
+                j++;
+                count++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        if(temp==count)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
