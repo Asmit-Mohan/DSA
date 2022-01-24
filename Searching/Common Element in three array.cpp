aed64de7b1e78254490219a9e83753e90b83f6ab
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-24 23:39:56 +0530

    Update Common Element in three array.cpp

--- Searching/Common Element in three array.cpp
+++ Searching/Common Element in three array.cpp
@@ -39,50 +39,79 @@ class Solution
 class Solution
 {
    public:    
-   vector <int> commonElements (int a[], int b[], int c[], int n1, int n2, int n3)
-       {
-           int n = min(n1,min(n1,n2));
-           set<int>ans;
-           if(n == n1)
-           {
-               for(int i=0;i<n1;i++)
-               {
-                   if(binary_search(b,b+n2,a[i])== true && binary_search(c,c+n3,a[i])==true)
-                   {
-                       ans.insert(a[i]);
-                   }
-               }
-           }
-           else if(n==n2)
-           {
-               for(int i =0;i<n2;i++)
-               {
-                   if(binary_search(a,a+n1,b[i])== true && binary_search(c,c+n3,b[i])==true)
-                   {
-                       ans.insert(b[i]);
-                   }
-               }
-           }
-           else if(n == n3)
-           {
-               vector<int>v(n3,0);
-               for(int i =0;i<n3;i++)
-               {
-                   if(binary_search(a,a+n1,c[i])== true && binary_search(b,b+n2,c[i])==true)
-                   {
-                       ans.insert(c[i]);
-                   }
-               }
-           }
-           vector<int>res;
-           for(auto it:ans)
-           {
-               res.push_back(it);
-           }
-           if(res.size()==0)
-           {
-               res.push_back(-1);
-           }
-           return res;
-       }
+   int bSearch(int target,int n,int arr[])
+    {
+        int low=0;
+        int high=n;
+        while(low<=high)
+        {
+            int  mid=low+(high-low)/2;
+            if(arr[mid]==target)
+            {
+                return 1;
+            }
+            else if(arr[mid]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return 0;
+    }
+    vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+    {
+        vector<int>ans;
+        set<int>s;
+        int range=min(min(n1,n2),n3);
+        
+        if(range==n1)
+        {
+            for(int i=0;i<n1;i++)
+            {
+                int flag1=bSearch(A[i],n2,B);
+                int flag2=bSearch(A[i],n3,C);
+                if(flag1&&flag2)
+                {
+                    s.insert(A[i]);
+                }
+            }
+        }
+        else if(range==n2)
+        {
+            for(int i=0;i<n2;i++)
+            {
+                int flag1=bSearch(B[i],n1,A);
+                int flag2=bSearch(B[i],n3,C);
+                if(flag1&&flag2)
+                {
+                    s.insert(B[i]);
+                }
+            }
+        }
+        else
+        {
+            for(int i=0;i<n3;i++)
+            {
+                int flag1=bSearch(C[i],n1,A);
+                int flag2=bSearch(C[i],n2,B);
+                if(flag1&&flag2)
+                {
+                    s.insert(C[i]);
+                }
+            }
+        }
+        
+        for(auto x : s)
+        {
+            ans.push_back(x);
+        }
+        if(ans.size()==0)
+        {
+            ans.push_back(-1);
+        }
+        return ans;
+    }
 };
