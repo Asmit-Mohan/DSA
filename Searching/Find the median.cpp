b8023836e6ac42e09b5ec1ec211beae1e6eb67f9
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-24 22:48:03 +0530

    Create Find the median.cpp

---
+++ Searching/Find the median.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+	int find_median(vector<int> v)
+	{
+		 sort(v.begin(),v.end());
+		 if(v.size()%2!=0)
+		 {
+		     return v[v.size()/2];
+		 }
+		 else
+		 {
+		     int a=v[v.size()/2];
+		     int b=v[v.size()/2-1];
+		     return (a+b)/2;
+		 }
+	}
+};
