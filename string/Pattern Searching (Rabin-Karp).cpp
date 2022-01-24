c35b4eeeb084d2f3c32346540c1ce431c1f273b7
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-24 17:14:00 +0530

    Create Pattern Searching (Rabin-Karp).cpp

---
+++ string/Pattern Searching (Rabin-Karp).cpp
@@ -0,0 +1,116 @@
+/*Approach 1 (Naive) Time :- O(|text|*|pat|) */
+
+class Solution
+{
+    public:
+    bool solve(int l,int r,string str,string pat)
+    {
+        int flag=1;
+        int i=0;
+        while(l<r)
+        {
+            if(pat[i]!=str[l])
+            {
+                flag=0;
+                break;
+            }
+            l++;
+            i++;
+        }
+        return flag;
+    }
+    
+        vector <int> search(string pat, string txt)
+        {
+            vector<int>v;
+            int n=pat.size();
+            
+            for(int i=0;i<=txt.size()-n;i++)
+            {
+                if(solve(i,i+n,txt,pat))
+                {
+                    v.push_back(i+1);
+                }
+            }
+            if(v.size()==0)
+            {
+                v.push_back(-1);
+            }
+            return v;
+        }
+};
+
+/*Approach 2 (Rabin Karp) Time :- O(|text|-|pat|+1) --> Average case O(|text|*|pat|) --> Worst case*/
+
+/*Logic :- Using Rolling Hash Function to avoid spurious hit*/
+/*When the hash value of the pattern matches with the hash value of a window of the text but the window is not the actual pattern then it is called a spurious hit or collision.*/
+/*Mod with prime to avoid overflow, the larger the prime is, the fewer collisions it would cause.*/
+
+class Solution
+{
+    public:
+    void solve(string txt,string pat,int mod,int d,vector<int>&ans)
+    {
+        
+    int M = pat.length();
+    int N = txt.length();
+    int i, j;
+    int p = 0; // hash value for pattern
+    int t = 0; // hash value for txt
+    int h = 1;
+ 
+    
+    for (i = 0; i < M - 1; i++)
+    {
+        h = (h * d) % mod;
+    }
+  
+    for (i = 0; i < M; i++)
+    {
+        p = (d * p + pat[i]) % mod;
+        t = (d * t + txt[i]) % mod;
+    }
+    
+       for (i = 0; i <= N - M; i++)
+       {
+            if ( p == t )
+            {  
+                bool flag = true;
+                for (j = 0; j < M; j++)
+                {
+                    if (txt[i+j] != pat[j])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (j == M)
+                {
+                    ans.push_back(i+1);
+                }
+            }
+      
+            if ( i < N-M )
+            {
+                t = (d*(t - txt[i]*h) + txt[i+M])%mod;
+                if (t < 0)
+                {
+                    t = (t + mod);
+                }
+            }
+       }
+       
+    }
+    vector <int> search(string pat, string txt)
+    {
+         vector<int>ans;
+         int mod=11; /*Can be any prime number, but larger the prime minimum will be the collision*/
+         int d=256;
+         solve(txt,pat,mod,d,ans);
+         if(ans.size()==0)
+         {
+             ans.push_back(-1);
+         }
+         return ans;
+    }
+};
