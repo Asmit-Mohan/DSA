52a77f6b90c76436efc05d259f19eabbb5173815
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-25 08:54:44 +0530

    Create Square Root.cpp

---
+++ Searching/Square Root.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        if(x==1)
+        {
+            return 1;
+        }
+        long long int low=0;
+        long long int high=x/2;
+        long long int ans;
+        
+        while(low<=high)
+        {
+            long long int mid=low+(high-low)/2;
+            long long int temp=mid*mid;
+            
+            if(temp==x)
+            {
+                return mid;
+            }
+            else if(temp>x)
+            {
+                 high=mid-1;
+            }
+            else
+            {
+                ans=mid;
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
