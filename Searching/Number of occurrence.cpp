823dcb053edb5262f0f1b211828d106230bab2e1
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-25 08:35:27 +0530

    Create Number of occurrence.cpp

---
+++ Searching/Number of occurrence.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+public:	
+	int count(int arr[], int n, int x)
+	{
+	    int low=0;
+	    int high=n-1;
+	    int flag=0;
+	    int mid=0;
+	    
+	    while(low<=high)
+	    {
+	        mid=low+(high-low)/2;
+	        if(arr[mid]==x)
+	        {
+	            flag=1;
+	            break;
+	        }
+	        else if(arr[mid]>x)
+	        {
+	            high=mid-1;
+	        }
+	        else 
+	        {
+	            low=mid+1;
+	        }
+	    }
+	    if(flag==0)
+	    {
+	        return 0;
+	    }
+	    else
+	    {
+	        low=mid;
+	        high=mid;
+	        while(arr[low]==x)
+	        {
+	            low--;
+	        }
+	        while(arr[high]==x)
+	        {
+	            high++;
+	        }
+	        return high-low-1;
+	      }
+     }
+  };
