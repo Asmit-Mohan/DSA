2c939893af3c0988cfd5ecea3ec872fe085e59b4
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 09:05:34 +0530

    Create Mirror Tree.cpp

---
+++ Binary Tree/Mirror Tree.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+  public:
+    void mirror(Node* root)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        mirror(root->left);
+        mirror(root->right);
+        Node* temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+    }
+};
