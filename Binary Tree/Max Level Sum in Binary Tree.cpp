dfc9c817cc768ea37e4f2e24b9b70bead9489bdb
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 12:11:37 +0530

    Create Max Level Sum in Binary Tree.cpp

---
+++ Binary Tree/Max Level Sum in Binary Tree.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+  public:
+    int maxLevelSum(Node* root)
+    {
+     int res=INT_MIN;
+     queue<Node*>q;
+     q.push(root);
+     while(q.size())
+     {
+         int size=q.size();
+         int ans=0;
+         while(size--)
+         {
+             Node* temp=q.front();
+             ans=ans+temp->data;
+             q.pop();
+             if(temp->left)
+             {
+                 q.push(temp->left);
+             }
+             if(temp->right)
+             {
+                 q.push(temp->right);
+             }
+         }
+         res=max(res,ans);
+     }
+     return res;
+    }
+};
