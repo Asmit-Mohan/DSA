b4be8d2ad8340efc0e098c7a76790aaaa0f4171e
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 12:29:27 +0530

    Create Symmetric Tree.cpp

---
+++ Binary Tree/Symmetric Tree.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+    bool solve(struct Node* root1,struct Node* root2)
+    {
+        if(root1==NULL&&root2==NULL)
+        {
+            return true;
+        }
+        if(root1==NULL||root2==NULL)
+        {
+            return false;
+        }
+        if(root1->data!=root2->data)
+        {
+            return false;
+        }
+        return solve(root1->left,root2->right)&&solve(root1->right,root2->left);
+    }
+    bool isSymmetric(struct Node* root)
+    {
+	      return solve(root,root);
+    }
+};
