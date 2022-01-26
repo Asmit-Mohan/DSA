82bd177f4d12a21d9b6c1f17391d651a2299c131
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 09:47:36 +0530

    Create Largest value in each level.cpp

---
+++ Binary Tree/Largest value in each level.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+    public:
+    vector<int> largestValues(Node* root)
+    {
+     vector<int> v;
+     queue<Node*>q;
+     q.push(root);
+     while(q.size())
+     {
+         int res=INT_MIN;
+         int size=q.size();
+         while(size--)
+         {
+             Node* temp=q.front();
+             res=max(res,temp->data);
+             q.pop();
+             if(temp->left)
+             {
+                 q.push(temp->left);
+             }
+             if(temp->right)
+             {
+                 q.push(temp->right);
+             }
+         }
+         v.push_back(res);
+     }
+     return v;
+    }
+};
