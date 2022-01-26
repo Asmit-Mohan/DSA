3741c8105183351dbfc70e9e1460c923960cf9b9
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 16:49:39 +0530

    Create Bottom View.cpp

---
+++ Binary Tree/Bottom View.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+    vector <int> bottomView(Node *root)  
+    {
+        map<int,int>mp;
+        vector<int>ans;
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        
+        while(!q.empty())
+        {
+            Node* temp=q.front().first;
+            int placeValue=q.front().second;
+            q.pop();
+            mp[placeValue]=temp->data;
+            if(temp->left)
+            {
+                q.push({temp->left,placeValue-1});
+            }
+            if(temp->right)
+            {
+                q.push({temp->right,placeValue+1});
+            }
+        }
+        for(auto x : mp)
+        {
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
