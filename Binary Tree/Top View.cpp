417bbf4c473e2411ed2b16a0cd90b5a92cc2f7e5
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 16:38:32 +0530

    Create Top View.cpp

---
+++ Binary Tree/Top View.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+    vector<int> topView(Node *root)
+    {
+        map<int,int>mp;
+        vector<int>ans;
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        
+        while(!q.empty())
+        {
+            Node* temp=q.front().first;
+            int placeValue=q.front().second;
+            q.pop();
+            if(mp.find(placeValue)==mp.end())
+            {
+                mp[placeValue]=temp->data;
+            }
+            if(temp->left)
+            {
+                q.push({temp->left,placeValue-1});
+            }
+            if(temp->right)
+            {
+                q.push({temp->right,placeValue+1});
+            }
+        }
+        for(auto x : mp)
+        {
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
