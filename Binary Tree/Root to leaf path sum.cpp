e22dca1ab202a6a6d677e4b12576532353e3156d
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 12:20:45 +0530

    Create Root to leaf path sum.cpp

---
+++ Binary Tree/Root to leaf path sum.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    bool hasPathSum(Node *root, int S)
+    {
+        if(root==NULL)
+        {
+            return false;
+        }
+        if(root->left==NULL&&root->right==NULL&&(S-root->data==0))
+        {
+            return true;
+        }
+        return hasPathSum(root->left,S-root->data)||hasPathSum(root->right,S-root->data);
+    }
+};
