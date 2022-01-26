275459a2d29afd88a7c57bb520149a4b6d27fda4
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 17:47:30 +0530

    Update Cousins of a given node.cpp

--- Binary Tree/Cousins of a given node.cpp
+++ Binary Tree/Cousins of a given node.cpp
@@ -1,61 +1,46 @@
 class Solution
 {
-    public:
-    vector<int> printCousins(Node* root, Node* find)
+public:
+vector<int> printCousins(Node* root, Node* find)
+{
+    queue<Node*>q;
+    int flag=0;
+    q.push(root);
+    while(q.size())
     {
-       queue<pair<Node*,int>>q;
-       q.push({root,-1});
-       vector<int>ans;
-       
-       while(!q.empty())
-       {
-           int n=q.size();
-           int flag=0;
-           vector<pair<int,int>>v;
-           while(n--)
-           {
-               Node* temp=q.front().first;
-               int par=q.front().second;
-               q.pop();
-               
-               v.push_back({temp->data,par});
-               if(temp->data==find->data)
-               {
-                   flag=1;
-               }
-               if(temp->left)
-               {
-                   q.push({temp->left,temp->data});
-               }
-               if(temp->right)
-               {
-                   q.push({temp->right,temp->data});
-               }
-           }
-           if(flag==1)
-           {
-               int par;
-               for(int i=0;i<v.size();i++)
-               {
-                   if(v[i].first==find->data)
-                   {
-                       par=v[i].second;
-                       break;
-                   }
-               }
-               for(int i=0;i<v.size();i++)
-               {
-                   if(v[i].second!=par)
-                   {
-                       ans.push_back(v[i].first);
-                   }
-               }
-           }
-       }
-       if(ans.size()==0)
-       {
-           ans.push_back(-1);
-       }
-       return ans;
+        int width=q.size();
+        vector<int>res;
+        while(width--)
+        {
+            Node *temp=q.front();
+            q.pop();
+            if(temp->left==find or temp->right==find)
+            {
+                flag=1;
+            }
+            else
+            {
+                if(temp->left)
+                {
+                    res.push_back(temp->left->data);
+                    q.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    res.push_back(temp->right->data);
+                    q.push(temp->right);
+                }
+            }
+        }
+        if(flag==1)
+        {
+            if(res.size()==0)
+            {
+                return {-1};
+            }
+            return res;
+        }
     }
+    return {-1};
+   }
 };
