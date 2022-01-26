2c888b641e788b0bc773442a08cb90932e92de7e
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 08:43:30 +0530

    Create Postorder Traversal.cpp

---
+++ Binary Tree/Postorder Traversal.cpp
@@ -0,0 +1,20 @@
+void post(Node* root,vector<int>&v)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    else
+    {
+        post(root->left,v);
+        post(root->right,v);
+        v.push_back(root->data);
+    }
+}
+
+vector <int> postOrder(Node* root)
+{
+    vector<int>v;
+    post(root,v);
+    return v;
+}
