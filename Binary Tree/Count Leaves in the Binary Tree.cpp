0aa1a4ca660a1421f5f5f117da3ce623309a475d
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 08:51:27 +0530

    Create Count Leaves in the Binary Tree.cpp

---
+++ Binary Tree/Count Leaves in the Binary Tree.cpp
@@ -0,0 +1,26 @@
+void solve(Node* root,int &count)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    if(root->left==NULL&&root->right==NULL)
+    {
+        count++;
+    }
+    if(root->left)
+    {
+        solve(root->left,count);
+    }
+    if(root->right)
+    {
+        solve(root->right,count);
+    }
+}
+
+int countLeaves(Node* root)
+{
+    int count=0;
+    solve(root,count);
+    return count;
+}
