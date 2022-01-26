c31790e4a32e9764d8b869b80fb3f45cf3c8a684
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 08:40:20 +0530

    Create Inorder Traversal.cpp

---
+++ Binary Tree/Inorder Traversal.cpp
@@ -0,0 +1,46 @@
+/*Recursion*/
+
+class Solution
+{
+  public:
+    vector<int>v;
+    vector<int> inOrder(Node* root)
+    {
+         if(root!=NULL)
+         {
+             inOrder(root->left);
+             v.push_back(root->data);
+             inOrder(root->right);
+         }
+         return v;
+    }
+};
+
+/*Iterative*/
+
+class Solution
+{
+  public:
+    vector<int> inOrder(Node* root)
+    {
+       stack<Node*>s;
+       vector<int>v;
+       while(1)
+       {
+           while(root)
+           {
+               s.push(root);
+               root=root->left;
+           }
+           if(s.empty())
+           {
+               break;
+           }
+           root=s.top();
+           s.pop();
+           v.push_back(root->data);
+           root=root->right;
+       }
+         return v;
+    }
+};
