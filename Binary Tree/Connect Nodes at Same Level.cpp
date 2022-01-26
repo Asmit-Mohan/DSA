f0d19aaba475a369fd92e3dc7437a0083701c323
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 16:16:54 +0530

    Create Connect Nodes at Same Level.cpp

---
+++ Binary Tree/Connect Nodes at Same Level.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+    void connect(Node *root)
+    {
+       queue<Node*>q;
+       q.push(root);
+       
+       while(!q.empty())
+       {
+           int n=q.size();
+           while(n--)
+           {
+               Node* temp=q.front();
+               q.pop();
+               if(n==0)
+               {
+                   temp->nextRight=NULL;
+               }
+               else
+               {
+                   temp->nextRight=q.front();
+               }
+               if(temp->left)
+               {
+                   q.push(temp->left);
+               }
+               if(temp->right)
+               {
+                   q.push(temp->right);
+               }
+           }
+        }
+     }
+};
