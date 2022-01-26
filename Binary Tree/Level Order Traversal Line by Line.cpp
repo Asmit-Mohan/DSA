8370aa92e05ae9bd04b07216da626b1cf92ae708
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 09:00:51 +0530

    Create Level Order Traversal Line by Line.cpp

---
+++ Binary Tree/Level Order Traversal Line by Line.cpp
@@ -0,0 +1,35 @@
+vector<vector<int>> levelOrder(Node* root)
+{
+      vector<vector<int>>ans;
+       if(root==NULL)
+       {
+          return ans; 
+       }
+       
+       queue<Node*>q;
+       q.push(root);
+       
+       while(!q.empty())
+       {
+           int n=q.size();
+           vector<int>v;
+           while(n--)
+           {
+               Node* temp=q.front();
+               q.pop();
+               v.push_back(temp->data);
+               if(temp->left)
+               {
+                   q.push(temp->left);
+               }
+               if(temp->right)
+               {
+                   q.push(temp->right);
+               }
+           }
+           ans.push_back(v);
+       }
+       return ans;
+}
+
+
