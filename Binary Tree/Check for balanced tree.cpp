ea51db7c614d0dde01bc30a96bec171539b42188
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 09:35:57 +0530

    Create Check for balanced tree.cpp

---
+++ Binary Tree/Check for balanced tree.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public:
+    int height(Node* root,int &flag)
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        int lh=height(root->left,flag);
+        int rh=height(root->right,flag);
+        if(abs(lh-rh)>1)
+        {
+            flag=0;
+        }
+        else
+        {
+            return 1+max(lh,rh);
+        }
+    }
+    bool isBalanced(Node *root)
+    {
+       int flag=1;
+       height(root,flag);
+       return flag;
+    }
+};
