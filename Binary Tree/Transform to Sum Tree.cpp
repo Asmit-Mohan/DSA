78ecdc5b6210595c91188ed8aaeb4f98fe8da2d7
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-26 19:16:04 +0530

    Create Transform to Sum Tree.cpp

---
+++ Binary Tree/Transform to Sum Tree.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+  public:
+    int solve(Node* root)
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        int left=solve(root->left);
+        int right=solve(root->right);
+        int temp=root->data+left+right;
+        root->data=left+right;
+        return temp;
+    }
+    
+    void toSumTree(Node *node)
+    {
+        solve(node);
+    }
+};
