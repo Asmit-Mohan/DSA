3decd78f838a470bcec644c9a76d7ef3f1674b47
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-27 11:38:19 +0530

    Create Sum of all leaf nodes.cpp

---
+++ Binary Search Tree/Sum of all leaf nodes.cpp
@@ -0,0 +1,18 @@
+void solve(Node *root,int &sum)
+{
+    if(root!=NULL)
+    {
+        if(root->left==NULL&&root->right==NULL)
+        {
+            sum+=root->data;
+        }
+        solve(root->left,sum);
+        solve(root->right,sum);
+    }
+}
+int sumOfLeafNodes(Node *r )
+{
+     int sum=0;
+     solve(r,sum);
+     return sum;
+}
