1f28e7298b15f212857d89e6386acf48d0b83167
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-27 15:08:02 +0530

    Create Binary Tree to BST.cpp

---
+++ Binary Search Tree/Binary Tree to BST.cpp
@@ -0,0 +1,33 @@
+
+class Solution
+{
+  public:
+  Node* change(Node* &root,vector<int>&v,int &i)
+  {
+      if(root==NULL)
+      {
+          return NULL;
+      }
+      change(root->left,v,i);
+      root->data=v[i++];
+      change(root->right,v,i);
+  }
+  Node* inorder(Node* root,vector<int>&v)
+  {
+      if(root!=NULL)
+      {
+          inorder(root->left,v);
+          v.push_back(root->data);
+          inorder(root->right,v);
+      }
+  }
+    Node *binaryTreeToBST (Node *root)
+    {
+        vector<int>v;
+        int i=0;
+        inorder(root,v);
+        sort(v.begin(),v.end());
+        change(root,v,i);
+        return root;
+    }
+};
