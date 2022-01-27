ac82bb4d2f7d049bc2f4945c8d100b602f7a46f3
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-27 16:44:42 +0530

    Create Check for BST.cpp

---
+++ Binary Search Tree/Check for BST.cpp
@@ -0,0 +1,58 @@
+class Solution
+{
+    public:
+    
+    bool isLarge(Node* root,int val)
+    {
+        if(root==NULL)
+        {
+            return 1;
+        }
+        if(root->data>val&&isLarge(root->left,val)&&isLarge(root->right,val))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    
+    bool isSmall(Node* root,int val)
+    {
+        if(root==NULL)
+        {
+            return 1;
+        }
+        if(root->data<val&&isSmall(root->left,val)&&isSmall(root->right,val))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    
+    bool isBST(Node* root) 
+    {
+         if(root==NULL)
+         {
+             return 1;
+         }
+         
+         int temp1=isSmall(root->left,root->data);
+         int temp2=isLarge(root->right,root->data);
+         int temp3=isBST(root->left);
+         int temp4=isBST(root->right);
+         
+         if(temp1&&temp2&&temp3&&temp4)
+         {
+             return 1;
+         }
+         else
+         {
+             return 0;
+         }
+    }
+};
