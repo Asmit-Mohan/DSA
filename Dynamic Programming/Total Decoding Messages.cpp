dafd05d2b19107e07851d50e97f0a409e68c2a2a
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-28 18:04:19 +0530

    Update Total Decoding Messages.cpp

--- Dynamic Programming/Total Decoding Messages.cpp
+++ Dynamic Programming/Total Decoding Messages.cpp
@@ -10,27 +10,28 @@ class Solution
 	        return dp[pos];
 	    }
 	    if(op.length()==pos)
-		{
-		    return 1;     
-		}
-		if(op[pos]=='0')
-		{
-		    return 0;
-		}
+	    {
+		return 1;     
+            }
+	    if(op[pos]=='0')
+	    {
+	        return 0;
+            }
+		
 	    int res=solve(op,pos+1);   /*For 1 charecter at a time*/
 	    if((pos+1)<op.length())   /*For 2 charecter at a time*/
 	    {
-    	    if(op[pos]=='1'||(op[pos]=='2'&&op[pos+1]<='6'))  /*starting from 1 or 2 then <=6 because total 26 alphabets are there*/
-    	    {
-    	        res=(res+solve(op,pos+2))%mod;
-    	    }
+		 if(op[pos]=='1'||(op[pos]=='2'&&op[pos+1]<='6'))  /*starting from 1 or 2 then <=6 because total 26 alphabets are there*/
+		 {
+	            res=res+(solve(op,pos+2)%mod);
+		 }
 	    }
 	    return dp[pos]=res%mod;
 	}
-		int CountWays(string str)
-		{
-		      int pos=0;
-		      memset(dp,-1,sizeof(dp));
-		      return solve(str,pos);
-    		}
+	int CountWays(string str)
+	{
+	    int pos=0;
+	    memset(dp,-1,sizeof(dp));
+	    return solve(str,pos);
+    	}
 };
