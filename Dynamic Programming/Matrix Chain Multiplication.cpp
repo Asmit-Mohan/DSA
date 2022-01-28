f977b06a9533ace1db375f19c2200b523fcd0d56
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-28 20:50:58 +0530

    Create Matrix Chain Multiplication.cpp

---
+++ Dynamic Programming/Matrix Chain Multiplication.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int dp[100][100];
+    int solve(int arr[],int i,int j)
+    {
+        if(i>=j)
+        {
+            return 0;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int ans=INT_MAX;
+        for(int k=i;k<j;k++)
+        {
+            int temp=solve(arr,i,k)+solve(arr,k+1,j)+(arr[i-1]*arr[k]*arr[j]);
+            ans=min(ans,temp);
+        }
+        return dp[i][j]=ans;
+    }
+    int matrixMultiplication(int n, int arr[])
+    {
+        memset(dp,-1,sizeof(dp));
+        return solve(arr,1,n-1);
+    }
+};
