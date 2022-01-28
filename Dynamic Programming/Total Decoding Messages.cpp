9e04ad4e632e5c10b3fa2a96cf4195040e193a49
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-28 17:54:04 +0530

    Create Total Decoding Messages.cpp

---
+++ Dynamic Programming/Total Decoding Messages.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+	public:
+	int dp[10001];
+	long long mod=pow(10,9)+7;
+	int solve(string &op,int pos)
+	{
+	    if(dp[pos]!=-1)
+	    {
+	        return dp[pos];
+	    }
+	    if(op.length()==pos)
+		{
+		    return 1;     
+		}
+		if(op[pos]=='0')
+		{
+		    return 0;
+		}
+	    int res=solve(op,pos+1);   /*For 1 charecter at a time*/
+	    if((pos+1)<op.length())   /*For 2 charecter at a time*/
+	    {
+    	    if(op[pos]=='1'||(op[pos]=='2'&&op[pos+1]<='6'))  /*starting from 1 or 2 then <=6 because total 26 alphabets are there*/
+    	    {
+    	        res=(res+solve(op,pos+2))%mod;
+    	    }
+	    }
+	    return dp[pos]=res%mod;
+	}
+		int CountWays(string str)
+		{
+		      int pos=0;
+		      memset(dp,-1,sizeof(dp));
+		      return solve(str,pos);
+    }
+};
