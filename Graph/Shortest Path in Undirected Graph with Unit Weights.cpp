b650fa4aba94f88826bd96388f7629e0b2dc44a9
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-01-29 14:48:08 +0530

    Create Shortest Path in Undirected Graph with Unit Weights.cpp

---
+++ Graph/Shortest Path in Undirected Graph with Unit Weights.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+	public:
+    vector <int> shortestPath(int V, vector<int> adj[], int src)
+    {
+        vector<int>dis(V);
+        for(int i=0;i<V;i++)
+        {
+        	dis[i]=INT_MAX;
+        }
+        
+        queue<int>q;
+        q.push(src);
+        dis[src]=0;
+        
+        while(!q.empty())
+        {
+        	int node=q.front();
+        	q.pop();
+        	for(auto it : adj[node])
+        	{
+        		if(dis[it]>dis[node]+1)
+        		{
+        			dis[it]=dis[node]+1;
+        			q.push(it);
+        		}
+        	}
+        }
+        return dis;
+    }
+};
