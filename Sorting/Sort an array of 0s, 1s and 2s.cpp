7db348e21aa01f13303b855e8a0ae50db5030467
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-02-01 13:40:12 +0530

    Update Sort an array of 0s, 1s and 2s.cpp

--- Sorting/Sort an array of 0s, 1s and 2s.cpp
+++ Sorting/Sort an array of 0s, 1s and 2s.cpp
@@ -1,9 +1,11 @@
+/* Approach 1 Brute Optimal Solution */
+
 void sort012(int a[], int n)
 {
     int one=0;
     int two=0;
     int zero=0;
-
+    
     for(int i=0;i<n;i++)
     {
         if(a[i]==0)
@@ -19,17 +21,58 @@ void sort012(int a[], int n)
             two++;
         }
     }
-    int i=0;
-    while(zero--)
+    
+    for(int i=0;i<n;i++)
     {
-        a[i++]=0;
-    }
-    while(one--)
-    {
-         a[i++]=1;
-    }
-    while(two--)
-    {
-         a[i++]=2;
+       if(zero!=0&&one!=0&&two!=0)
+       {
+            a[i]=0;
+            zero--;
+       }
+       else if(zero==0&&one!=0&&two!=0)
+       {
+            a[i]=1;
+            one--;
+       }
+       else
+       {
+            a[i]=2;
+            two--;
+       }
     }
 }
+
+/* Approach 2 Efficient using The Dutch Nation Flag Problem */
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        int low=0;
+        int high=n-1;
+        int mid=0;
+        
+        while(mid<=high)
+        {
+            if(a[mid] ==0)
+            {
+                int temp = a[mid];
+                a[mid] = a[low];
+                a[low] = temp;
+                low++;
+                mid++;
+            }
+            else if(a[mid] ==1)
+            {
+                mid++;
+            }
+            else if(a[mid] ==2)
+            {
+                int temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+                high--;
+            }
+        }
+    }
+};
