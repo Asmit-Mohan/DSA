489fbc5d6bd851050e38821f8dbe91b13d18ff9a
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-03-02 13:11:11 +0530

    Create Flood Fill.cpp

---
+++ Graph/Flood Fill.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    void dfs(int i,int j,int oldColor,int newColor,vector<vector<int>>& image)
+    {
+        int row=image.size();
+        int col=image[0].size();
+        
+        if(i<0 || j<0 || i==row || j==col)
+        {
+            return;
+        }
+        
+        if(image[i][j]!=oldColor)
+        {
+            return;
+        }
+        
+        image[i][j]=newColor;
+        
+        dfs(i-1,j,oldColor,newColor,image);
+        dfs(i,j-1,oldColor,newColor,image);
+        dfs(i+1,j,oldColor,newColor,image);
+        dfs(i,j+1,oldColor,newColor,image);
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)
+    {
+        if(image[sr][sc]!=newColor)
+        {
+            dfs(sr,sc,image[sr][sc],newColor,image);
+        }
+        return image;
+    }
+};
+
+/*Similar To Rat In The Maze Problem*/  /*DFS Application*/
