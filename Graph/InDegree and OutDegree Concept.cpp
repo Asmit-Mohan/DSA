79c8d6b6da3e1c600177a03a30a21830f67bc821
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-03-09 17:01:45 +0530

    Create InDegree and OutDegree Concept.cpp

---
+++ Graph/InDegree and OutDegree Concept.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int findJudge(int n, vector<vector<int>>& edges)
+    {
+        if(n==1)
+        {
+            return 1;
+        }
+       vector<int>hashInDegree(n+1,0);
+       vector<int>hashOutDegree(n+1,0); 
+        
+       for(int i=0;i<edges.size();i++)
+       {
+           hashOutDegree[edges[i][0]]++;
+           hashInDegree[edges[i][1]]++;
+       }
+       
+        for(int i=0;i<n+1;i++)
+        {
+            if(hashOutDegree[i]==0&&hashInDegree[i]==n-1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
