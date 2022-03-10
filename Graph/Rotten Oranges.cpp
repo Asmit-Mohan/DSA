0e5b9f0a8213d396cfda0c5b42bb209173cd2ac7
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-03-10 07:34:59 +0530

    Update Rotten Oranges.cpp

--- Graph/Rotten Oranges.cpp
+++ Graph/Rotten Oranges.cpp
@@ -1,3 +1,106 @@
+/* BFS Approach */
+
+class Solution
+{
+public:    
+    bool isValid(int i, int j, int row, int col, vector<vector<int>>& grid)
+    {
+        if(i>=0 && i<row && j>=0 && j<col && grid[i][j] == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    int orangesRotting(vector<vector<int>>& grid)
+    {
+        int row = grid.size();
+        int col = grid[0].size();
+        
+        int fresh = 0;
+        int time = 0;
+        queue<pair<int, int>>q;
+        
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                if(grid[i][j] == 2)
+                {
+                    q.push({i, j});
+                }
+                
+                else if(grid[i][j] == 1)
+                {
+                    fresh++;
+                }
+            }
+        }
+        
+        if(fresh == 0)  /*No Fresh Oranges*/
+        {
+            return 0;
+        }
+        if(q.size()==0)  /*No Rotten Oranges*/
+        {
+            return -1;
+        }
+        
+        while(!q.empty())
+        {
+            
+            int size = q.size();
+            int flag = 0;
+            while(size--)
+            {   
+                pair<int, int>temp = q.front();
+                q.pop();
+                
+                int first = temp.first;
+                int second = temp.second;
+                
+                int x[4] = { 1, -1, 0, 0};
+                int y[4] = { 0, 0 , 1 , -1};
+                
+                for(int i=0; i<4; i++)
+                {
+                    int xNew  = x[i] + first;
+                    int yNew  = y[i] + second;
+                    
+                    if(isValid(xNew, yNew, row, col, grid))
+                    {
+                        flag=1;
+                        grid[xNew][yNew] = 2;
+                        q.push({xNew, yNew});
+                    }
+                }
+            }
+            
+            if(flag==1)
+            {
+                time++;
+            }
+        }
+        
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                if(grid[i][j] == 1)  /*At last when any fresh oranges left*/
+                {
+                    time = 0;
+                    break;
+                }
+            }
+        }
+        return (time == 0) ? -1 : time ;
+    }
+};
+
+/* DFS Approach */
 class Solution
 {
 public:
