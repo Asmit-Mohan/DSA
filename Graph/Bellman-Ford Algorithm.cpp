17f25cb80699519b1b47754b74afdd953fdd109c
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-03-15 17:51:49 +0530

    Create Bellman-Ford Algorithm.cpp

---
+++ Graph/Bellman-Ford Algorithm.cpp
@@ -0,0 +1,53 @@
+class Solution
+{
+	public:
+    vector <int> bellman_ford(int n, vector<vector<int>> adj, int S)
+    {
+        vector<int>dist(n,100000000);
+        dist[S]=0;
+        
+        for(int i=1;i<n;i++)
+        {
+            for(auto itr : adj)
+            {
+                int u=itr[0];
+                int v=itr[1];
+                int wt=itr[2];
+                if((dist[u]+wt)<dist[v])
+                {
+                    dist[v]=dist[u]+wt;
+                }
+            }
+        }
+        
+        int flag=1;
+        
+        for(int i=1;i<n;i++)
+        {
+            for(auto itr : adj)
+            {
+                int u=itr[0];
+                int v=itr[1];
+                int wt=itr[2];
+                if((dist[u]+wt)<dist[v])
+                {
+                    flag=0;
+                    break;
+                }
+            }
+            if(flag==0)
+            {
+                break;
+            }
+        }
+        
+        if(flag==0)
+        {
+            // cout<<"Have a Negative Weight Cycle";
+        }
+        else
+        {
+           return dist;    
+        }
+    }
+};
