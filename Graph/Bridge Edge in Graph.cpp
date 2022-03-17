baabcc5ff4422b3c6e6cf21ca56a0d8877eb9812
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-03-17 23:03:41 +0530

    Create Bridge Edge in Graph.cpp

---
+++ Graph/Bridge Edge in Graph.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+public:
+
+void dfs(int src, vector<int>&vis, vector<int> adj[])
+{
+    vis[src]=1;
+    for(auto x : adj[src])
+    {
+        if(vis[x]==0)
+        {
+           dfs(x,vis,adj);
+        }
+    }
+}
+
+    int isBridge(int V, vector<int> graph[], int c, int d) 
+    {
+        vector<int> adj[V];
+        vector<int>vis(V,0);
+        
+        for(int i=0;i<V;i++)
+        {
+            for( auto x : graph[i])
+            {
+                if((x==c and i==d) || (x==d and i==c))
+                {
+                    continue;
+                }
+                adj[i].push_back(x);
+            }
+        }
+        
+        dfs(c,vis,adj);
+        
+        if(vis[d]==1)  /*It Means c and d lies in same component then it's not a bridge*/
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+};
