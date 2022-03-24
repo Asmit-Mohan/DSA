1af1f1e96cab7223a01b3f25eb104e03ea6d44d9
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-03-24 18:33:49 +0530

    Create Floyd Warshall.cpp

---
+++ Graph/Floyd Warshall.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+  public:
+	void shortest_distance(vector<vector<int>>&matrix)
+	{
+	   int INF=1e9+10; 
+       int n=matrix.size();
+       
+       for(int i=0;i<n;i++)
+       {
+          for(int j=0;j<n;j++)
+          {
+               if(matrix[i][j]==-1)
+               {
+                   matrix[i][j]=INF;  /*For comparision purpose we have initialised with INF*/
+               }
+          }    
+       }
+       
+       for(int k=0;k<n;k++)
+       {
+           for(int i=0;i<n;i++)
+           {
+               for(int j=0;j<n;j++)
+               {
+                   matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+               }
+           }
+       }
+       
+       for(int i=0;i<n;i++)
+       {
+          for(int j=0;j<n;j++)
+          {
+             if(matrix[i][j]==INF)
+             {
+                 matrix[i][j]=-1;
+             }
+          }    
+       }
+	  }
+};
+
+/* LUV */
