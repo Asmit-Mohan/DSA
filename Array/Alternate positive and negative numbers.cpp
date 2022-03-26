0f3eeea57e719f3c8b5e623d99ab3b10e8f62639
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-03-26 11:45:11 +0530

    Update Alternate positive and negative numbers.cpp

--- Array/Alternate positive and negative numbers.cpp
+++ Array/Alternate positive and negative numbers.cpp
@@ -18,18 +18,47 @@ public:
 	    }
 	    int i=0;
 	    while(!negetive.empty() || !positive.empty() && i<n)
-        {
-            if(!positive.empty())
             {
-                arr[i++]=positive.front();
-                positive.pop();
+		    if(!positive.empty())
+		    {
+			arr[i++]=positive.front();
+			positive.pop();
+		    }
+
+		    if(!negetive.empty())
+		    {
+			arr[i++]=negetive.front();
+			negetive.pop();
+		    }
             }
-            
-            if(!negetive.empty())
-            {
-                arr[i++]=negetive.front();
-                negetive.pop();
-            }
-        }
 	}
-}
+};
+
+/* When Order Does Not Matter Time :- O(N) Space :- O(1) */
+
+class Solution
+{
+public:
+	void rearrange(int arr[], int n)
+	{
+		int i = -1;
+		for (int j = 0; j < n; j++)
+		{
+			if (arr[j] < 0)
+			{
+				i++;
+				swap(arr[i], arr[j]);
+			}
+		}
+
+		int posIdx = i + 1;
+		int negIdx = 0;
+
+		while (posIdx < n && negIdx < pos && arr[negIdx] < 0)
+		{
+			swap(arr[neg], arr[pos]);
+			pos++;
+			neg = neg + 2;
+		}
+	}
+};
