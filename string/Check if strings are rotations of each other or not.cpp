9daa7d26c5d8f13df08eac3a2d8a348b0b0eaf8f
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-06 17:08:08 +0530

    Update Check if strings are rotations of each other or not.cpp

--- string/Check if strings are rotations of each other or not.cpp
+++ string/Check if strings are rotations of each other or not.cpp
@@ -1,3 +1,48 @@
+/*Approach 1*/
+
+class Solution
+{
+    public:
+    bool areRotations(string s1,string s2)
+    {
+        if(s1.size()!=s2.size())
+        {
+           return 0;
+        }
+        if(s1.size()==s2.size() && s1==s2)
+        {
+           return 1;
+        }
+        
+        queue<char>q1,q2;
+        
+        for(int i=0;i<s1.length();i++)
+        {
+            q1.push(s1[i]);
+        }
+        for(int i=0;i<s2.length();i++)
+        {
+            q2.push(s2[i]);
+        }
+        
+        int size=s1.length();
+        
+        while(size--)
+        {
+            char temp=q1.front();
+            q1.pop();
+            q1.push(temp);
+            if(q1==q2)
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+};
+
+/*Approach 2*/
+
 class Solution
 {
     public:
