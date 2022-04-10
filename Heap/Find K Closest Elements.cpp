4c27ce1ba0803884e89b74ba4924fa42570b13cb
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-10 10:43:14 +0530

    Update Find K Closest Elements.cpp

--- Heap/Find K Closest Elements.cpp
+++ Heap/Find K Closest Elements.cpp
@@ -3,25 +3,23 @@ class Solution
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x)
     {
-       priority_queue <pair<int,int>> pq;
-       //First int is Diff(key here) and second one is array value(which is to return).
-       // In case key is same then max heap method is applied to second element of pair.
+       priority_queue<pair<int,int>>pq;
+       vector <int> v;
+       
        for(int i=0;i<arr.size();i++)
        {
            pq.push({abs(arr[i]-x),arr[i]});
-           if(pq.size()>k)
+           while(pq.size()>k)
            {
                pq.pop();
            }
        }
-       vector <int> v;
        while(!pq.empty())
        {
-           //pair<int,int> p=pq.top();
            v.push_back(pq.top().second);
            pq.pop();
        }
-        sort(v.begin(),v.end());
-        return v;
+       sort(v.begin(),v.end()); 
+       return v;
     }
 };
