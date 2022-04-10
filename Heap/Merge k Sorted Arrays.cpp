1c791ec3b41a6816c29fa22d564fc19ec5601b42
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-10 11:36:33 +0530

    Update Merge k Sorted Arrays.cpp

--- Heap/Merge k Sorted Arrays.cpp
+++ Heap/Merge k Sorted Arrays.cpp
@@ -1,27 +1,27 @@
+/* Naive Approach */
+
 class Solution
 {
 public:
 vector<int> mergeKArrays(vector<vector<int>> arr, int k)
 {
-priority_queue<int,vector<int>,greater<int>>pq;
-vector<int>v;
-for(int i=0;i<k;i++)
-{
-     for(int j=0;j<k;j++)
+     priority_queue<int,vector<int>,greater<int>>pq;
+     for(int i=0;i<k;i++)
      {
-           pq.push(arr[i][j]);
-           if(pq.size()>k)
-           {
-              v.push_back(pq.top());
-              pq.pop();
-           }
+          for(int j=0;j<k;j++)
+          {
+            pq.push(arr[i][j]);
+          }
      }
-}
-for(int i=0;i<k;i++)
-{
-    v.push_back(pq.top());
-    pq.pop();
-}
-return v;
+      vector <int> v;
+      while(pq.empty()==0)
+      {
+          v.push_back(pq.top());
+          pq.pop();
+      }
+      return v;
 }
 };
+
+/* Efficient Approach */
+
