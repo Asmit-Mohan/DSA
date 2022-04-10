492af2dc0c5d7f572ed909726a0175b0651b9021
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-10 11:05:14 +0530

    Update K Closest Points to Origin.cpp

--- Heap/K Closest Points to Origin.cpp
+++ Heap/K Closest Points to Origin.cpp
@@ -1,25 +1,27 @@
 class Solution
 {
 public:
-vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
-{
-priority_queue<pair<int,vector<int>>> pq;
-for(int i=0;i<points.size();i++)
-{
-  int x=points[i][0];
-  int y=points[i][1];
-  pq.push({(x*x)+(y*y),{x,y}});
-  if(pq.size()>k)
-  {
-      pq.pop();
-  }
-}
-vector<vector<int>> v;
-while(pq.size())
-{
-   v.push_back(pq.top().second); 
-   pq.pop();
-}
-return v;   
-}
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
+    {
+        priority_queue<pair<int,vector<int>>>pq;
+        vector<vector<int>>ans;
+        
+        for(int i=0;i<points.size();i++)
+        {
+            int x=points[i][0];
+            int y=points[i][1];
+            int distance=((x*x)+(y*y));
+            pq.push({distance,points[i]});
+            while(pq.size()>k)
+            {
+                pq.pop();
+            }
+        }
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
 };
