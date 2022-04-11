043c7b600bc5619f0ef4c20a58b8319cc669c7d1
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-11 15:43:36 +0530

    Update Symmetric Tree.cpp

--- Binary Tree/Symmetric Tree.cpp
+++ Binary Tree/Symmetric Tree.cpp
@@ -1,3 +1,5 @@
+/*Approach 1 */
+
 class Solution
 {
     public:
@@ -20,5 +22,51 @@ class Solution
     bool isSymmetric(struct Node* root)
     {
 	      return solve(root,root);
+    }
+};
+
+/* Approach 2 */
+
+class Solution
+{
+public:
+    bool isSame(TreeNode* root1,TreeNode* root2)
+    {
+        if(root1==NULL&&root2==NULL)
+        {
+            return true;
+        }
+        if(root1==NULL||root2==NULL)
+        {
+            return false;
+        }
+        if(root1->val!=root2->val)
+        {
+            return false;
+        }
+        return isSame(root1->left,root2->left)&&isSame(root1->right,root2->right);
+    }
+    
+    void mirror(TreeNode* root)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        mirror(root->left);
+        mirror(root->right);
+        TreeNode* temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+    }
+    
+    bool isSymmetric(TreeNode* root)
+    {
+        if(root==NULL||root->left==NULL&&root->right==NULL)
+        {
+            return true;
+        }
+        mirror(root->right);
+        return isSame(root->left,root->right);
     }
 };
