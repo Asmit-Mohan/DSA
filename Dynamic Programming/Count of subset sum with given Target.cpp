130743a5c64cedfcba79b1d8c8ec5cbf4f889279
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-13 18:27:12 +0530

    Update Count of subset sum with given Target.cpp

--- Dynamic Programming/Count of subset sum with given Target.cpp
+++ Dynamic Programming/Count of subset sum with given Target.cpp
@@ -1,32 +1,40 @@
 class Solution
 {
-	public:
-	int perfectSum(int arr[], int n, int sum)
-	{
-        int t[n+1][sum+1];
-        long long int mod=pow(10,9)+7;
+       public:
+       int perfectSum(int arr[], int n, int w)
+       {
+       int dp[n+1][w+1];
+       int mod = pow(10,9)+7;
+       
         for(int i=0;i<n+1;i++)
         {
-            for(int j=0;j<sum+1;j++)
+            for(int j=0;j<w+1;j++)
             {
+                if(i==0)
+                {
+                   dp[i][j]=0;   
+                }
                 if(j==0)
                 {
-                    t[i][j]=1;
-                }
-                else if(i==0)
-                {
-                    t[i][j]=0;
-                }
-                else if(arr[i-1]<=j)
-                {
-                    t[i][j]=(t[i-1][j-arr[i-1]]%mod)+(t[i-1][j]%mod);
-                }
-                else
-                {
-                    t[i][j]=(t[i-1][j]%mod);
+                    dp[i][j]=1;
                 }
             }
         }
-        return (t[n][sum]%mod);
-	  } 
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=0;j<w+1;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                   dp[i][j]=(dp[i-1][j-arr[i-1]]%mod)+(dp[i-1][j]%mod);   
+                }
+                else
+                {
+                    dp[i][j]=(dp[i-1][j]%mod);
+                }
+            }
+        }
+        return dp[n][w]%mod;
+     }
 };
