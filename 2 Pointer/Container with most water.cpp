c27f5642cf2680da7eb66c5d0e1114deeb716697
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-28 19:06:32 +0530

    Create Container with most water.cpp

---
+++ 2 Pointer/Container with most water.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int maxArea(vector<int>& arr)
+    {
+        int l=0;
+        int r=arr.size()-1;
+        int area=0;
+        
+        while(l<r)
+        {
+            if(arr[l]<=arr[r])
+            {
+                area=max(area,(r-l)*arr[l]);
+                l++;
+            }
+            else
+            {
+                area=max(area,(r-l)*arr[r]);
+                r--;
+            }
+        }
+        return area;
+    }
+};
