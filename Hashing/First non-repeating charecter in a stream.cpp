d55da6f3b8d57bf5a36236de8e06639e55f632d3
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-29 00:19:47 +0530

    Create First non-repeating charecter in a stream.cpp

---
+++ Hashing/First non-repeating charecter in a stream.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+	public:
+	string FirstNonRepeating(string A)
+	{
+     unordered_map<char,int>mp;
+     queue<char>q;
+     string ans="";
+     
+     for(int i=0;i<A.size();i++)
+     {
+         if(mp[A[i]]==0)
+         {
+             q.push(A[i]);
+         }
+         
+         mp[A[i]]++;
+         
+         while(!q.empty() && mp[q.front()]>1)
+         {
+             q.pop();
+         }
+         
+         if(!q.empty())
+         {
+             ans+=q.front();
+         }
+         else
+         {
+             ans+='#';
+         }
+     }
+     return ans;
+	}
+};
