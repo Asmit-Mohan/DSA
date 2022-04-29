55de25746e47a9962d694ae8a1b2f6857299e790
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-29 15:28:24 +0530

    Create Longest-repeating-character-replacement (Same as Max Consecutive Ones III).cpp

---
+++ Sliding Window/Longest-repeating-character-replacement (Same as Max Consecutive Ones III).cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+public:
+    int maximum(unordered_map<char,int>mp)
+    {
+        int ans=-1;
+        for(auto it : mp)
+        {
+            ans=max(ans,it.second);
+        }
+        return ans;
+    }
+    
+    int characterReplacement(string s, int k)
+    {
+        int i=0;
+        int j=0;
+        int ans=INT_MIN;
+        unordered_map<char,int>mp;
+        
+        while(j<s.length())
+        {
+            mp[s[j]]++;
+            int window=j-i+1;
+            int max=maximum(mp);
+            
+            if((window-max)>k)
+            {
+                mp[s[i]]--;
+                if(mp[s[i]]==0)
+                {
+                    mp.erase(s[i]);
+                }
+                i++;
+            }
+            else
+            {
+                if(ans<window)
+                {
+                    ans=window;
+                }
+            }
+            j++;
+        }
+        return ans;
+    }
+};
