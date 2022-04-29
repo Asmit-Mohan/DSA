346bc55f27c81715cce8a78189d059f6a9d930ca
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-29 17:02:47 +0530

    Update Sliding window Maximum.cpp

--- Sliding Window/Sliding window Maximum.cpp
+++ Sliding Window/Sliding window Maximum.cpp
@@ -1,3 +1,37 @@
+/* Approach 1 */
+
+class Solution
+{
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k)
+    {
+        deque<int> dq;
+        vector<int> ans;
+        int j=0;
+        
+        while(j<nums.size())
+        {
+            if (!dq.empty() && dq.front() == j-k) 
+            {
+                dq.pop_front();
+            }
+            while (!dq.empty() && nums[dq.back()] < nums[j])
+            {
+                dq.pop_back();  
+            }
+            dq.push_back(j);
+            if (j>=k-1)        /* Greater than for next such window */
+            {
+                ans.push_back(nums[dq.front()]);
+            }
+            j++;
+        }
+        return ans;
+    }
+};
+
+/* Approach 2 */
+
 class Solution
 {
   public:
