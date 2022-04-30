baddedd440f4f33e8b2960d20789a2ca15c8062e
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-30 10:42:08 +0530

    Update Search an element in sorted and rotated array.cpp

--- Searching/Search an element in sorted and rotated array.cpp
+++ Searching/Search an element in sorted and rotated array.cpp
@@ -1,3 +1,5 @@
+/* For GeeksForGeeks */
+
 int binary(vector<int> arr,int l,int r,int k)
 {
     int mid=l+(r-l)/2;
@@ -66,3 +68,129 @@ int Search(vector<int> vec, int K)
          return -1;
      }
 }
+
+/* For Leetcode Approach 1 */
+
+class Solution
+{
+public:
+ int findMinEle(vector<int>& nums)
+ {
+        int n=nums.size();
+        int start=0;
+        int end=n-1;
+        if(nums[start]<nums[end])
+        {
+            return 0;
+        }
+        while(start<=end)
+        {
+            int mid=start + (end-start)/2;
+            int next=(mid+1)%n;
+            int prev=(mid+n-1)%n;
+            if(nums[mid]<=nums[prev] && nums[mid]<=nums[next])
+            {
+                return mid;
+            }
+            else if(nums[mid]<nums[end])
+            {
+                end=mid-1;
+            }
+            else if(nums[mid]>nums[end])
+            {
+                start=mid+1;
+            }
+        }
+        return 0;
+    }
+    
+    int binarySearch(vector<int>& arr, int target,int i,int j)
+    {
+        int start=i;
+        int end=j;
+        while(start<=end)
+        {
+            int mid=start + (end-start)/2;
+            if(arr[mid]==target)
+            {
+                return mid;
+            }
+            else if(arr[mid]>target)
+            {
+                end=mid-1;
+            }
+            else if(arr[mid]<target)
+            {
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+    
+    int search(vector<int>& nums, int target)
+    {
+        int n=nums.size();
+        int minEle=findMinEle(nums);
+        
+        int a=binarySearch(nums,target,0,minEle-1);
+        int b=binarySearch(nums,target,minEle,n-1);
+        
+        if(a==-1 && b==-1)
+        {
+            return -1;
+        }
+        else if(a==-1)
+        {
+            return b;
+        }
+        else
+        {
+            return a;
+        }
+    }
+};
+
+/* For Leetcode Approach 2 */
+
+class Solution
+{
+public:
+    int search(vector<int>& nums, int target)
+    {
+        int low=0;
+        int high=nums.size()-1;
+        int mid;
+        
+        while(low<=high)
+        {
+            mid=low+(high-low)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>=nums[low])
+            {
+                if(nums[mid]>=target && target>=nums[low])
+                {
+                    high=mid-1;
+                }
+                else 
+                {
+                    low=mid+1;
+                }
+            }
+            else 
+            {
+                if(nums[high]>=target && target>=nums[mid])
+                {
+                    low=mid+1;
+                }
+                else 
+                {
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
