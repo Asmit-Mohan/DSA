9f3ce9fa01e97077b75b66ce4de9263f23ed26cd
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-04-30 16:46:11 +0530

    Create Minimum element in a sorted and rotated array II.cpp

---
+++ Searching/Minimum element in a sorted and rotated array II.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+   int findMin(vector<int>& a)
+   {
+        int n=a.size();
+        int l=0;
+        int r=n-1;
+        while(l<r)
+        {
+            int mid=l+((r-l)/2);
+            if(a[mid]>a[r])
+            {
+               l=mid+1; 
+            }
+            else if(a[mid]<a[r])
+            {
+               r=mid;
+            }
+            else
+            {
+                r--;
+            }
+        }
+        return a[l];
+    }
+};
