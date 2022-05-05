f54cd752c5af63d9b2e521561f7eaf77dfc38ca1
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-05-05 23:50:15 +0530

    Create Top K Frequent Words.cpp

---
+++ Heap/Top K Frequent Words.cpp
@@ -0,0 +1,49 @@
+class comp
+{
+public:
+    bool operator()(pair<string, int> a, pair<string, int> b)
+    {
+        if (b.second < a.second)
+        {
+            return true;
+        }
+        if (b.second == a.second && b.first > a.first)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+class Solution
+{
+public:
+    vector<string> topKFrequent(vector<string>& words, int k)
+    {
+        priority_queue<pair<string, int>, vector<pair<string, int>>,comp>pq;
+        unordered_map<string,int> mp;
+        for (auto x : words)
+        {
+            mp[x]++;
+        }
+        
+        for (auto x : mp)
+        {
+            pq.push(x);
+            if (pq.size() > k)
+            {
+                pq.pop();
+            }
+        }
+        
+        vector<string> ans;
+        while (!pq.empty())
+        {
+            ans.push_back(pq.top().first);
+            pq.pop();
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
