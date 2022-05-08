ea3b1c83c5828260a0f0df5509e1f1c9b27a6ca9
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-05-08 18:55:51 +0530

    Create Longest Airthmetic Subsequence.cpp

---
+++ Dynamic Programming/Longest Airthmetic Subsequence.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    int longestArithSeqLength(vector<int>& nums)
+    {
+        int n=nums.size();
+        if(n<3)
+        {
+            return 0;
+        }
+        
+        int ans=INT_MIN;
+        int dp[1001][1001]={0};
+        
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int diff=nums[j]-nums[i]+500;   /* To make negative common difference postive as negative index in dp array was not possible*/
+                if(dp[i][diff]!=0)
+                {
+                   dp[j][diff]=1+dp[i][diff]; 
+                }
+                else
+                {
+                    dp[j][diff]=2;
+                }
+                ans=max(ans,dp[j][diff]);
+            }
+        }
+        return ans;
+    }
+};
