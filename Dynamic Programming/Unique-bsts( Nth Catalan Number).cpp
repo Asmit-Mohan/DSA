d8b6d894ca6e117e301017909dc9e307a1758143
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-05-08 18:01:06 +0530

    Update Unique-bsts( Nth Catalan Number).cpp

--- Dynamic Programming/Unique-bsts( Nth Catalan Number).cpp
+++ Dynamic Programming/Unique-bsts( Nth Catalan Number).cpp
@@ -1,3 +1,36 @@
+/* Binomial Coffcient Approach */ /*Intution :- Nth Catalan number is equal to (2ncn)/(n+1) */
+
+class Solution
+{
+public:
+unsigned long int binomialCoefficient(int n,int i)  /* Calculation of 2ncn */
+{
+        unsigned long int ans = 1;
+        if(i==0||i==n)
+        {
+            return 1;
+        }
+        if(i>n-i)
+        {
+            i=n-i;
+        }
+        for(int j=0;j<i;j++)
+        {
+            ans=ans*(n-j);
+            ans=ans/(j+1);
+        }
+        return ans;
+}
+
+    int numTrees(int n) 
+    {
+        unsigned long int c = binomialCoefficient(2*n,n);
+        return c/(n+1);
+    }
+};
+
+/* DP Approach */
+
 class Solution
 {
     public:
