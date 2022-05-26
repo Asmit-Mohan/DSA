5aa83fb33393331a76b0589627a450347ebe761f
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-05-26 22:08:59 +0530

    Create count n by k occurences.cpp

---
+++ Searching/count n by k occurences.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    int countOccurence(int arr[], int n, int k)
+    {
+       unordered_map<int,int> mp;
+       int ans = 0;
+       int target = n/k;
+       
+       for(int i = 0; i < n; i++)
+       {
+           mp[arr[i]]++;
+       }
+       for(auto it: mp)
+       {
+           if(it.second > target)
+           {
+               ans++;
+           }
+       }
+       return ans;
+    }
+};
