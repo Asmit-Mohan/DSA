fdc0987e201d444d08b480a9bd250748c96fb8a9
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-05-27 15:10:23 +0530

    Update Array to BST.cpp

--- Binary Search Tree/Array to BST.cpp
+++ Binary Search Tree/Array to BST.cpp
@@ -22,3 +22,27 @@ public:
         return v;
     }
 };
+
+class Solution
+{
+    public:
+    TreeNode* solve(vector<int>& nums, int low, int high)
+    {
+        if(low>=high)
+        {
+            return NULL; 
+        }
+        
+        int mid=(low+high)/2;
+        
+        TreeNode* root=new TreeNode(nums[mid]);
+        root->left=solve(nums,low,mid);
+        root->right=solve(nums,mid+1,high);
+        return root;
+    }
+    
+    TreeNode* sortedArrayToBST(vector<int>& nums)
+    {
+        return solve(nums,0,nums.size());
+    }
+};
