22aed1a79b042278d54e02aa6b64012b7cdabe40
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-05-28 08:43:08 +0530

    Update Longest increasing subsequence.cpp

--- Dynamic Programming/Longest increasing subsequence.cpp
+++ Dynamic Programming/Longest increasing subsequence.cpp
@@ -35,7 +35,64 @@ public:
     }
 };
 
-/*Approach 2 Time :- O(N*LOGN) Space :- O(N) */
+/* Approach 2 Variation of LCS */
+
+class Solution
+{
+public:
+    int LCS(vector<int>& nums,vector<int>& nums2)
+    {
+        int m=nums.size();
+        int n=nums2.size();
+        int dp[m+1][n+1];
+        
+        for(int i=0;i<m+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+         for(int i=1;i<m+1;i++)
+         {
+            for(int j=1;j<n+1;j++)
+            {
+                if(nums[i-1]==nums2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    
+    int lengthOfLIS(vector<int>& nums)
+    {
+        vector<int>nums2;
+        set<int>s;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            s.insert(nums[i]);
+        }
+        for(auto x : s)
+        {
+            nums2.push_back(x);
+        }
+    
+        return LCS(nums,nums2);
+    }
+};
+
+/*Approach 3 Time :- O(N*LOGN) Space :- O(N) */
 
 class Solution
 {
