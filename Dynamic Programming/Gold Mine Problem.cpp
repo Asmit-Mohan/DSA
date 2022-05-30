b9bf3daafe4b949314ffda700cf7dd04c7235ddf
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-05-30 08:28:53 +0530

    Create Gold Mine Problem.cpp

---
+++ Dynamic Programming/Gold Mine Problem.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+    int maxGold(int row, int col, vector<vector<int>>matrix)
+    {
+        if(row == 1)
+        {
+            int sum = 0;
+            for(int j = 0; j < col; j++)
+            {
+                sum = sum + matrix[0][j];
+            }
+            return sum;
+        }
+        
+        for(int j = 1; j < col; j++)
+        {
+            for(int i = 0; i < row; i++)
+            {
+                if(i == 0)
+                {
+                    matrix[i][j] += max(matrix[i][j - 1], matrix[i + 1][j - 1]);
+                }
+                else if(i == row - 1)
+                {
+                    matrix[i][j] += max(matrix[i][j - 1], matrix[i - 1][j - 1]);
+                }
+                else
+                {
+                    matrix[i][j] += max(matrix[i][j - 1], max(matrix[i - 1][j - 1], matrix[i + 1][j - 1]));
+                }
+            }
+        }
+        
+        int mx = 0;
+        for(int i = 0; i < row; i++)
+        {
+             mx = max(mx, matrix[i][col - 1]);
+        }
+        return mx;
+    }
+};
