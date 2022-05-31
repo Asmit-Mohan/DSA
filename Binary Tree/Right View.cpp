ac143c5686313077ccbfe5e7b0336553b235a8e1
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-05-31 16:36:27 +0530

    Update Right View.cpp

--- Binary Tree/Right View.cpp
+++ Binary Tree/Right View.cpp
@@ -1,3 +1,4 @@
+/* Approach 1 */
 class Solution
 {
     public:
@@ -34,5 +35,32 @@ class Solution
          }
          return v;
         }
+    }
+};
+
+/* Approach 2 */
+
+class Solution
+{
+public:
+    void recursion(TreeNode *root, int level, vector<int> &res)
+    {
+        if(root==NULL)
+        {
+            return ;
+        }
+        if(res.size()==level)
+        {
+            res.push_back(root->val);
+        }
+        recursion(root->right, level+1, res);
+        recursion(root->left, level+1, res);
+    }
+    
+    vector<int> rightSideView(TreeNode *root)
+    {
+        vector<int> res;
+        recursion(root, 0, res);
+        return res;
     }
 };
