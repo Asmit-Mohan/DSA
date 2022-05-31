95615f9fc944dd30da9f0c3d88e3cc8241869f56
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-05-31 16:34:53 +0530

    Update Left View.cpp

--- Binary Tree/Left View.cpp
+++ Binary Tree/Left View.cpp
@@ -1,3 +1,4 @@
+/* Approach 1 Iterative */
 vector<int> leftView(Node *root)
 {
         vector<int> v;
@@ -32,3 +33,30 @@ vector<int> leftView(Node *root)
          return v;
         }
 }
+
+/* Approach 2 Recursive */ 
+
+class Solution
+{
+public:
+    void recursion(TreeNode *root, int level, vector<int> &res)
+    {
+        if(root==NULL)
+        {
+            return ;
+        }
+        if(res.size()==level)
+        {
+           res.push_back(root->val);
+        }
+        recursion(root->left,  level+1, res);
+        recursion(root->right, level+1, res);
+    }
+    
+    vector<int> leftSideView(TreeNode *root)
+    {
+        vector<int> res;
+        recursion(root, 0, res);
+        return res;
+    }
+};
