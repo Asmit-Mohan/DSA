d0229b410d56c45af922046788ff9d93b3ea39f5
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-05-31 16:46:19 +0530

    Create Lowest Common Ancestors.cpp

---
+++ Binary Tree/Lowest Common Ancestors.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+{
+    if(root==NULL)
+    {
+        return NULL;
+    }
+    if(root->val == p->val || root->val == q->val)
+    {
+        return root;
+    }
+    TreeNode* l =  lowestCommonAncestor(root->left,p,q);     //inorder
+    TreeNode* r =  lowestCommonAncestor(root->right,p,q);   // inorder
+
+    if(l==NULL && r!=NULL )
+    {
+        return r;
+    }
+    else if(r==NULL && l!=NULL)
+    {
+        return l;
+    }
+    else if(l==NULL && r==NULL )
+    {
+        return NULL;
+    }
+    else
+    {
+        return root;
+    }
+}
+};
