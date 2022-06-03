469d029289fc482f452e0a9ae9d453efe516f211
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-03 08:21:59 +0530

    Update Postorder Traversal.cpp

--- Binary Tree/Postorder Traversal.cpp
+++ Binary Tree/Postorder Traversal.cpp
@@ -1,3 +1,4 @@
+/* Approach 1 Recursion */
 void post(Node* root,vector<int>&v)
 {
     if(root==NULL)
@@ -18,3 +19,76 @@ vector <int> postOrder(Node* root)
     post(root,v);
     return v;
 }
+
+/* Approach 2 Iterative Using 2 Stack*/
+class Solution
+{
+public:
+    vector<int> postorderTraversal(TreeNode* root)
+    {
+        vector<int>v;
+        if(root==NULL)
+        {
+            return v;
+        }
+        else
+        {
+            stack<TreeNode*>s1,s2;
+            s1.push(root);
+            while(!s1.empty())
+            {
+                TreeNode* temp=s1.top();
+                s1.pop();
+                s2.push(temp);
+                if(temp->left)
+                {
+                    s1.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    s1.push(temp->right);
+                }
+            }
+            while(!s2.empty())
+            {
+                v.push_back(s2.top()->val);
+                s2.pop();
+            }
+        }
+        return v;      
+    }
+};
+
+/* Approach 3 Using 1 Stack */ 
+
+class Solution
+{
+public:
+    vector<int> postorderTraversal(TreeNode* root)
+    {
+        vector<int> ans;
+		if(root==NULL)
+        {
+            return ans;
+        }
+		TreeNode* curr;
+		stack<TreeNode*>s;
+		s.push(root);
+		while(!s.empty())
+        {
+			curr = s.top();
+			s.pop();
+			ans.push_back(curr->val);
+			if(curr->left)
+            {
+                s.push(curr->left);
+            }
+            if(curr->right)
+            {
+                s.push(curr->right);
+            }
+		}
+		reverse(ans.begin(),ans.end());
+		return ans;
+    }
+};
