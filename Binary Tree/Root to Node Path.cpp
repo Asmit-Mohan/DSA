470597b1637980f7461175d68375861f95316e50
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-03 12:41:52 +0530

    Create Root to Node Path.cpp

---
+++ Binary Tree/Root to Node Path.cpp
@@ -0,0 +1,30 @@
+bool findPath(TreeNode* root,int target,vector<int>&ans)
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+    ans.push_back(root->val);
+    if(root->val==target)
+    {
+        return 1;
+    }
+   
+    if(findPath(root->left,target,ans)||findPath(root->right,target,ans))
+    {
+        return 1;
+    }
+    ans.pop_back();
+    return 0;
+}
+
+vector<int> Solution::solve(TreeNode* root, int target)
+{
+    vector<int>ans;
+    if(root==NULL)
+    {
+        return ans;
+    }
+    findPath(root,target,ans);
+    return ans;
+}
