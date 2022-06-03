4419e28e6f48be6c72e1761fa059afcdfd8fd42b
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-03 07:24:23 +0530

    Update Preorder Traversal.cpp

--- Binary Tree/Preorder Traversal.cpp
+++ Binary Tree/Preorder Traversal.cpp
@@ -1,3 +1,5 @@
+/* Approach 1 Recursion */
+
 void solve(Node* root,vector<int>&v)
 {
      if(root==NULL)
@@ -18,3 +20,38 @@ vector <int> preorder(Node* root)
     solve(root,v);
     return v;
 }
+
+/* Approach 2 Iterative Stack */
+
+class Solution
+{
+public:
+    vector<int> preorderTraversal(TreeNode* root)
+    {
+        vector<int>v;
+        if(root==NULL)
+        {
+            return v;
+        }
+        else
+        {
+        stack<TreeNode*>s;
+        s.push(root);
+        while(!s.empty())
+        {
+            TreeNode* temp=s.top();
+            s.pop();
+            v.push_back(temp->val);
+            if(temp->right)
+            {
+                s.push(temp->right);
+            }
+            if(temp->left)
+            {
+                s.push(temp->left);
+            }
+        }
+        }
+        return v;
+    }
+};
