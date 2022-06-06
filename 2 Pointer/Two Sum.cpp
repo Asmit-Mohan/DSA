7c1c987d75721f76e0622cb134b361cb8bda13b9
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-06 22:40:39 +0530

    Update Two Sum.cpp

--- 2 Pointer/Two Sum.cpp
+++ 2 Pointer/Two Sum.cpp
@@ -3,54 +3,47 @@ class Solution
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-        int n=nums.size();
-        vector<int>temp(n);
-        set<int>ans;
-        for(int i=0;i<n;i++)
-        {
-            temp[i]=nums[i];
-        }
-        sort(nums.begin(),nums.end());
-        int a,b;
-        int l=0;
-        int r=n-1;
-        while(l<r)
-        {
-            if(nums[l]+nums[r]==target)
-            {
-                a=nums[l];
-                b=nums[r];
-                break;
-            }
-            else if(nums[l]+nums[r]>target)
-            {
-                r--;
-            }
-            else
-            {
-                l++;
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(temp[i]==a)
-            {
-                ans.insert(i);
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(temp[i]==b)
-            {
-                ans.insert(i);
-            }
-        }
-        vector<int>res;
-        set<int>::iterator itr;
-        for (itr = ans.begin(); itr != ans.end(); itr++)
-        {
-            res.push_back(*itr);
-        }
-        return res;
+          vector<int>ans;
+          if(nums.size()==2)
+          {
+               if(nums[0]+nums[1]==target)
+               {
+                   ans.push_back(0);
+                   ans.push_back(1);
+               }
+               return ans;
+          }
+        
+          vector<pair<int,int>>v;
+          for(int i=0;i<nums.size();i++)
+          {
+              v.push_back({nums[i],i});
+          }
+        
+          sort(v.begin(),v.end());
+          int l=0;
+          int r=nums.size()-1;
+          
+          while(l<r)
+          {
+              int sum=v[l].first+v[r].first;
+              
+              if(sum==target)
+              {
+                  ans.clear();
+                  ans.push_back(v[l].second);
+                  ans.push_back(v[r].second);
+                  break;
+              }
+              else if(sum>target)
+              {
+                  r--;
+              }
+              else
+              {
+                  l++;
+              }
+          }
+          return ans;
     }
 };
