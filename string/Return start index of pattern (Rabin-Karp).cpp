51137dd6a24efbeecd0799c236dd00c0ccabe9f8
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-06 16:55:40 +0530

    Update Return start index of pattern (Rabin-Karp).cpp

--- string/Return start index of pattern (Rabin-Karp).cpp
+++ string/Return start index of pattern (Rabin-Karp).cpp
@@ -40,11 +40,11 @@ class Solution
         }
 };
 
-/*Approach 2 (Rabin Karp) Time :- O(|text|-|pat|+1) --> Average case O(|text|*|pat|) --> Worst case*/
+/* Approach 2 (Rabin Karp) Time :- O(|text|-|pat|+1) --> Average case O(|text|*|pat|) --> Worst case*/
 
-/*Logic :- Using Rolling Hash Function to avoid spurious hit*/
-/*When the hash value of the pattern matches with the hash value of a window of the text but the window is not the actual pattern then it is called a spurious hit or collision.*/
-/*Mod with prime to avoid overflow, the larger the prime is, the fewer collisions it would cause.*/
+/* Logic :- Using Rolling Hash Function to avoid spurious hit*/
+/* When the hash value of the pattern matches with the hash value of a window of the text but the window is not the actual pattern then it is called a spurious hit or collision.*/
+/* Mod with prime to avoid overflow, the larger the prime is, the fewer collisions it would cause.*/
 
 class Solution
 {
@@ -59,12 +59,14 @@ class Solution
     int t = 0; // hash value for txt
     int h = 1;
  
+    // The value of h would be pow(d,M-1)%mod
     
     for (i = 0; i < M - 1; i++)
     {
         h = (h * d) % mod;
     }
-  
+    
+    /* Calculate the hash value of pattern and firstwindow of text */ 
     for (i = 0; i < M; i++)
     {
         p = (d * p + pat[i]) % mod;
@@ -73,6 +75,7 @@ class Solution
     
        for (i = 0; i <= N - M; i++)
        {
+       		/* If Hash Value Matches then there is a possibility of answer then check it */
             if ( p == t )
             {  
                 bool flag = true;
@@ -89,22 +92,22 @@ class Solution
                     ans.push_back(i+1);
                 }
             }
-      
+            
+    		/* Calculate hash value for next window of txt: Remove leading digit, add trailing digit */
             if ( i < N-M )
             {
                 t = (d*(t - txt[i]*h) + txt[i+M])%mod;
-                if (t < 0)
+                if (t < 0)  /* We might get negative value of t, converting it to positive */
                 {
                     t = (t + mod);
                 }
             }
        }
-       
     }
     vector <int> search(string pat, string txt)
     {
          vector<int>ans;
-         int mod=11; /*Can be any prime number, but larger the prime minimum will be the collision*/
+         int mod=11;        /*Can be any prime number, but larger the prime minimum will be the collision*/
          int d=256;
          solve(txt,pat,mod,d,ans);
          if(ans.size()==0)
@@ -114,3 +117,9 @@ class Solution
          return ans;
     }
 };
+
+/*Reference Links :- */
+
+// https://www.geeksforgeeks.org/rabin-karp-algorithm-for-pattern-searching/
+// https://www.programiz.com/dsa/rabin-karp-algorithm
+// https://www.youtube.com/watch?v=qQ8vS2btsxI
