b0bf048cf3c7d7447be6ae939caf494f8c8e6186
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-09 01:54:38 +0530

    Create Column name from a given column number.cpp

---
+++ string/Column name from a given column number.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+string colName (long long int n)
+{
+    string result = "";
+    while(n)
+    {
+        char c = 'A' + (n - 1) %26;
+        result = c + result;
+        n = (n - 1)/26;
+    }
+    return result;
+    }
+};
