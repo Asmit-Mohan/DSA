c3550411063165a0ed51acf18389f87477cd2968
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-09 22:18:32 +0530

    Create  Serialize and Deserialize Binary Tree.cpp

---
+++ Binary Tree/Serialize and Deserialize Binary Tree.cpp
@@ -0,0 +1,80 @@
+class Codec
+{
+public:
+    string serialize(TreeNode* root)
+    {
+        string tree="";
+        if(root==NULL)
+        {
+            return tree;
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* front=q.front();
+            q.pop();
+            if(front==NULL)
+            {
+                tree+=("#,");
+            }
+            else
+            {
+                tree+=(to_string(front->val)+',');
+            }
+            if(front!=NULL)
+            {
+                q.push(front->left);
+                q.push(front->right);
+            }
+        }
+        cout<<tree<<endl;
+        return tree;
+    }
+    
+    TreeNode* deserialize(string data)
+    {
+        if(data.length()==0)
+        {
+            return NULL;
+        }
+        stringstream s(data);
+        string str;
+        queue<TreeNode*>q;
+        
+        getline(s,str,',');
+        TreeNode* root = new TreeNode(stoi(str));
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            TreeNode* front=q.front();
+            q.pop();
+            
+            getline(s,str,',');
+            if(str=="#")
+            {
+                front->left=NULL;
+            }
+            else
+            {
+                TreeNode* temp = new TreeNode(stoi(str));
+                front->left=temp;
+                q.push(temp);
+            }
+            
+            getline(s,str,',');
+            if(str=="#")
+            {
+                front->right=NULL;
+            }
+            else
+            {
+                TreeNode* temp = new TreeNode(stoi(str));
+                front->right=temp;
+                q.push(temp);
+            }
+        }
+        return root;
+    }
+};
