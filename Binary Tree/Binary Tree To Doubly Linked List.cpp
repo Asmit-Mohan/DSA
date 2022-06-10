5b01c1263154c9c701589186bba0cf604c56deaa
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-10 19:14:17 +0530

    Create Binary Tree To Doubly Linked List.cpp

---
+++ Binary Tree/Binary Tree To Doubly Linked List.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+    public:
+    Node* prev=NULL;
+    Node* head=NULL;
+    
+    Node * bToDLL(Node *root)
+    {
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        
+        /* Inorder Technique */
+        bToDLL(root->left);
+        
+        if(prev==NULL)
+        {
+            head=root;
+        }
+        else
+        {
+            root->left=prev;
+            prev->right=root;
+        }
+        prev=root;
+        
+        bToDLL(root->right);
+        return head;
+    }
+};
