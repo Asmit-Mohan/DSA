4bfc4f113c79c02e3ca68399b5b24fecaf9709df
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-10 19:33:58 +0530

    Create Maximum sum of non adjacent nodes.cpp

---
+++ Binary Tree/Maximum sum of non adjacent nodes.cpp
@@ -0,0 +1,34 @@
+class Solution  --> House Robber Approach --> Code Library
+{
+    public:
+    unordered_map<Node*,int>dp;
+    
+    int getMaxSum(Node *root) 
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        if(dp[root])
+        {
+            return dp[root];
+        }
+        
+        int include = root->data;
+        
+        if(root->left)
+        {
+            include+=getMaxSum(root->left->left);
+            include+=getMaxSum(root->left->right);
+        }
+        if(root->right)
+        {
+            include+=getMaxSum(root->right->left);
+            include+=getMaxSum(root->right->right);
+        }
+        
+        int exclude = getMaxSum(root->left) + getMaxSum(root->right);
+        
+        return dp[root] = max(include,exclude);
+    }
+};
