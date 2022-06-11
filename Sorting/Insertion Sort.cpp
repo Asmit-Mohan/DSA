b411dce9c515992f7bbb9b86ef4272623f3c5b4c
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-11 16:16:03 +0530

    Update Insertion Sort.cpp

--- Sorting/Insertion Sort.cpp
+++ Sorting/Insertion Sort.cpp
@@ -4,12 +4,14 @@ class Solution
     void insert(int arr[], int i)
     {
         int j=i-1;
+        int current = arr[i];
+        
         while(j>=0&&arr[j]>arr[i]) 
         {
             arr[j+1]=arr[j];
             j--;
         }
-        arr[j+1]=arr[i];
+        arr[j+1]=current;
     }
   
     public:
