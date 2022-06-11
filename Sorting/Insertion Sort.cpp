ac7889f67d8e31146ac5cfaf68b335bf2dc0992a
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-11 16:03:08 +0530

    Update Insertion Sort.cpp

--- Sorting/Insertion Sort.cpp
+++ Sorting/Insertion Sort.cpp
@@ -1,32 +1,3 @@
-/*Approach 1*/
-
-class Solution
-{
-    public:
-    void insert(int arr[], int i)
-    {
-        for(int j=0;j<i;j++)
-        {
-            if(arr[j]>arr[i])
-            {
-                int temp=arr[j];
-                arr[j]=arr[i];
-                arr[i]=temp;
-            }
-        }
-    }
-    public:
-    void insertionSort(int arr[], int n)
-    {
-        for(int i=0;i<n;i++)
-        {
-            insert(arr,i);
-        }
-    }
-};
-
-/*Approach 2*/
-
 class Solution
 {
     public:
