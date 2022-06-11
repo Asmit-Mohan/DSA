63518345c82f04ae234309bcbc772cda91c830da
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-11 15:51:44 +0530

    Create Bubble sort on linked list.cpp

---
+++ Sorting/Bubble sort on linked list.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    ListNode* sortList(ListNode* head)
+    {
+        if(head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+        ListNode* curr = head;
+        
+        while(curr->next!=NULL)
+        {
+            ListNode* temp = curr->next;
+            while(temp!=NULL)
+            {
+                if(temp->val<curr->val)
+                {
+                    swap(temp->val,curr->val);
+                }
+                temp=temp->next;
+            }
+            curr=curr->next;
+        }
+        return head;
+    }
+};
