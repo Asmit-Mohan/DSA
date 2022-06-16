f9c75cdf2b9c68b3cb7db8de49a6c60a6bac4625
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-16 17:11:03 +0530

    Create Count Subarray with sum less than k.cpp

---
+++ Sliding Window/Count Subarray with sum less than k.cpp
@@ -0,0 +1,22 @@
+int Solution::solve(vector<int> &A, int B)
+{
+    int i=0,j=0;
+    int count=0;
+    int sum=0;
+    
+    while(j<A.size())
+    {
+        if(sum+A[j]<B)
+        {
+            count+=(j-i+1);
+            sum+=A[j];
+            j++;
+        }
+        else
+        {
+            sum-=A[i];
+            i++;
+        }
+    }
+    return count;
+}
