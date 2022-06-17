5c0c24aca8b3eade6718f7722b984070d7bfc203
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-17 19:30:37 +0530

    Create Count subarray with k odd numbers.cpp

---
+++ Sliding Window/Count subarray with k odd numbers.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    int numberOfSubarrays(vector<int>& nums, int k)
+    {
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        int ans = 0;
+        int odd = 0;
+        
+        while(j<nums.size())
+        {
+            if(nums[j]%2==1)
+            {
+                odd++;
+            }
+            
+            while(odd>k)
+            {
+                if(nums[i]%2)
+                {
+                    odd--;
+                    count = 0;
+                }
+                i++;
+            }
+            
+            while(odd == k && nums[i]%2 == 0)
+            {
+                i++;
+                count++;
+            }
+            
+            if(odd == k)
+            {
+                ans=ans+count+1;
+            }
+            
+            j++;
+        }
+        return ans;
+    }
+};
