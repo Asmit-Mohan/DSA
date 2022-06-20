f5937ecfd744e2fbf6c7cf70d949afbd3107e428
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-06-20 23:45:42 +0530

    Update First Missing Positive Integer.cpp

--- Array/First Missing Positive Integer.cpp
+++ Array/First Missing Positive Integer.cpp
@@ -1,3 +1,4 @@
+/* Naive Approach */
 class Solution
 {
 public:
@@ -27,5 +28,44 @@ public:
             k++;
         }
         return ans;
+    }
+};
+
+/* Space Optimised Approach */
+
+class Solution
+{
+public:
+    int firstMissingPositive(vector<int>& arr)
+    {
+        int n=arr.size();
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]<=0||arr[i]>n)
+            {
+                arr[i]=n+1;
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(abs(arr[i])>n)
+            {
+                continue;
+            }
+            else if(arr[abs(arr[i])-1]>0)
+            {
+                arr[abs(arr[i])-1]=-arr[abs(arr[i])-1];
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>0)
+            {
+                return i+1;
+            }
+        }
+        return n+1;
     }
 };
