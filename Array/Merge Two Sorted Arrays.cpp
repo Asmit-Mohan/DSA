517ec42f756600d3be15e2998598a8bff9bf0b8b
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-02 15:03:41 +0530

    Update Merge Two Sorted Arrays.cpp

--- Array/Merge Two Sorted Arrays.cpp
+++ Array/Merge Two Sorted Arrays.cpp
@@ -42,24 +42,26 @@ public:
 class Solution
 {
 public:
-	void merge(int arr1[], int arr2[], int n, int m)
-	{
-	    int i=0;
-	    int j=0;
-	    int k=n-1;
-	    
-	    while(i<n&&j<m)
-	    {
-	        if(arr1[i]>arr2[j])
-	        {
-	            swap(arr1[k--],arr2[j++]);
-	        }
-	        else
-	        {
-	            i++;
-	        }
-	    }
-	    sort(arr1,arr1+n);
-	    sort(arr2,arr2+m);
-	}
+    void merge(long long arr1[], long long arr2[], int n, int m) 
+    { 
+        int i=n-1;
+        int j=0;
+        
+        while(i>=0&&j<m)
+        {
+            if(arr1[i]>arr2[j])
+            {
+                swap(arr1[i],arr2[j]);
+                i--;
+                j++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        sort(arr1,arr1+n);
+        sort(arr2,arr2+m);
+    } 
 };
