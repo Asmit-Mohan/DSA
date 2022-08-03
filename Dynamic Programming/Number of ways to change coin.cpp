0ddc73483c773fda88747cbaf0072c8197aae4b6
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-03 22:26:58 +0530

    Update Number of ways to change coin.cpp

--- Dynamic Programming/Number of ways to change coin.cpp
+++ Dynamic Programming/Number of ways to change coin.cpp
@@ -1,3 +1,37 @@
+/* Approach 1 Time :- O(N*N) Space:- O(N*N) + O(N) */
+
+class Solution
+{
+  public:
+    long long int solve(int arr[],int n,int w,vector<vector<long long>>&dp)
+    {
+        if(n==0)
+        {
+            return dp[n][w]=0;
+        }
+        if(w==0)
+        {
+            return dp[n][w]=1;
+        }
+        if(dp[n][w]!=-1)
+        {
+            return dp[n][w];
+        }
+        if(arr[n-1]<=w)
+        {
+            return dp[n][w]=solve(arr,n,w-arr[n-1],dp)+solve(arr,n-1,w,dp);
+        }
+        return dp[n][w]=solve(arr,n-1,w,dp);
+    }
+    
+    long long int count(int wt[], int n, int w)
+    {
+        vector<vector<long long >>dp(n+1,vector<long long>(w+1,-1));
+        return solve(wt,n,w,dp);
+    }
+};
+
+/* Approach 2 Time :- O(N*N) Space:- O(N*N) */
 class Solution
 {
   public:
