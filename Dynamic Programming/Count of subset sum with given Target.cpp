fe7630a4b4f36473b891822bfbf6fec50f8c09a5
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-03 16:48:41 +0530

    Update Count of subset sum with given Target.cpp

--- Dynamic Programming/Count of subset sum with given Target.cpp
+++ Dynamic Programming/Count of subset sum with given Target.cpp
@@ -1,3 +1,5 @@
+/* Tabulation Approach Time :- O(N^2) Space:- O(N^2) */
+
 class Solution
 {
        public:
@@ -37,4 +39,43 @@ class Solution
         }
         return dp[n][w]%mod;
      }
+};
+
+/* Memorisation Approach Time :- O(N^2) Space:- O(N^2) + O(N) */
+
+class Solution
+{
+	 public:
+	 int mod = pow(10,9)+7;
+	 int solve(int arr[], int sum, int n, vector<vector<int>>&dp)
+        { 
+               if(sum==0)
+               {
+                  return 1;
+               }      
+               if(n==0)
+               {
+                  return 0;
+               }
+               if(dp[n][sum]!=-1)
+               {
+                  return dp[n][sum];
+               }
+               if(arr[n-1]<=sum)
+               {  
+                 dp[n][sum] = (solve(arr, sum-arr[n-1],n-1,dp) + solve(arr,sum,n-1,dp))%mod;
+               }
+               else
+               {
+                 dp[n][sum] = solve(arr,sum,n-1,dp)%mod;
+               }
+               return dp[n][sum];
+       }
+   
+	int perfectSum(int arr[], int n, int sum)
+	{
+              vector<vector<int>>dp(n+1, vector<int>(sum+1,-1));
+              sort(arr,arr+n,greater<int>());
+              return solve(arr,sum,n,dp);
+	}
 };
