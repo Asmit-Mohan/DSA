09f0dab1a450357e7b1b833948dfc4dae3f127bc
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-03 15:16:55 +0530

    Update Subset Sum.cpp

--- Dynamic Programming/Subset Sum.cpp
+++ Dynamic Programming/Subset Sum.cpp
@@ -1,3 +1,5 @@
+/* Tabulation Method --> Time :- O(N^2) and Space :- O(N^2) */
+
 class Solution
 {   
 public:
@@ -34,4 +36,40 @@ public:
        }
        return dp[n][w];
     }
+};
+
+/* Memorisation Time :- O(N^2) and Space :- O(N^2) + O(N) Auxialiry Stack */
+
+class Solution
+{   
+public:
+   int solve(vector<int>arr, int sum, int n, vector<vector<int>>&dp)
+   {
+        if(sum==0)
+        {
+           return 1;
+        }      
+        if(n==0)
+        {
+           return 0;
+        }
+        if(dp[n][sum]!=-1)
+        {
+           return dp[n][sum];
+        }
+        if(arr[n-1]<=sum)
+        {  
+          dp[n][sum] = solve(arr, sum-arr[n-1],n-1,dp) || solve(arr,sum,n-1,dp);
+        }
+        else
+        {
+          dp[n][sum] = solve(arr,sum,n-1,dp);
+        }
+        return dp[n][sum];
+   }
+   bool isSubsetSum(vector<int>arr, int sum)
+   {
+       vector<vector<int>>dp(arr.size()+1, vector<int>(sum+1,-1));
+       return solve(arr,sum,arr.size(),dp);
+   }
 };
