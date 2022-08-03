419ddc3bf6e8fd71eedde1396214077c35e9b8b7
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-03 23:27:03 +0530

    Update LCS.cpp

--- Dynamic Programming/LCS.cpp
+++ Dynamic Programming/LCS.cpp
@@ -1,3 +1,42 @@
+/* Approach 1 Time :- O(M*N) Space :- O(M*N) + o(N) */
+
+class Solution
+{
+public:
+    
+    int dp[1001][1001];
+    int solve(string s1,string s2,int m,int n)
+    {
+        if(m==0||n==0)
+        {
+            return dp[m][n]=0;
+        }
+        if(dp[m][n]!=-1)
+        {
+            return dp[m][n];
+        }
+        if(s1[m-1]==s2[n-1])
+        {
+            return dp[m][n]=1+solve(s1,s2,m-1,n-1);
+        }
+        else
+        {
+            return dp[m][n]=max(solve(s1,s2,m-1,n),solve(s1,s2,m,n-1));
+        }
+        return dp[m][n];
+    }
+    
+    int longestCommonSubsequence(string s1, string s2)
+    {
+         int m=s1.size();
+         int n=s2.size();
+         memset(dp,-1,sizeof(dp));
+         return solve(s1,s2,m,n);
+    }
+};
+
+/* Approach 2 Time :- O(M*N) Space :- O(M*N) + o(N) */
+
 class Solution
 {
     public:
