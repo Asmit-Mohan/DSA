47814d7355309c0e8bef5a4eb0ef2d49ce2739fa
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-03 19:29:55 +0530

    Update Maximize the cut segment.cpp

--- Dynamic Programming/Maximize the cut segment.cpp
+++ Dynamic Programming/Maximize the cut segment.cpp
@@ -1,3 +1,5 @@
+/* Approach 1 Time :- O(4*W) Space :-  O(4*W) */
+
 class Solution
 {
     public:
@@ -29,5 +31,46 @@ class Solution
             }
         }
         return dp[n][w]>0?dp[n][w]:0;
+    }
+};
+
+/* Approach 2 Time :- O(4*W) Space :-  O(4*W) + O(N) Recursive Stack */
+
+class Solution
+{
+    public:
+    int solve(int w, int wt[], int n, vector<vector<int>> &dp)
+    {
+        if(n==0)
+        {
+            return dp[n][w] = INT_MIN;
+        }
+        
+        if(w==0)
+        {
+            return dp[n][w] = 0;
+        }
+        
+        if(dp[n][w] != -1)
+        {
+            return dp[n][w];
+        }
+        
+        if(wt[n-1] > w)
+        {
+            return dp[n][w] = solve(w, wt, n-1, dp);
+        }
+        
+        return dp[n][w] = max(1 + solve(w-wt[n-1] , wt, n, dp),solve(w, wt, n-1, dp));
+    }
+    
+    int maximizeTheCuts(int w, int x, int y, int z)
+    {
+        int n=3;
+        int arr[4]={x,y,z};
+        
+        vector<vector<int>> dp(n+1, vector<int>(w+1, -1));
+        int ans = solve(w, arr, n, dp);
+        return ans<=0?0:ans;
     }
 };
