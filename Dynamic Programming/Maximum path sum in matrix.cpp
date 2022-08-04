a0daaa4c69463f282049c5613bc881ddd2d5ead6
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-04 13:21:06 +0530

    Update Maximum path sum in matrix.cpp

--- Dynamic Programming/Maximum path sum in matrix.cpp
+++ Dynamic Programming/Maximum path sum in matrix.cpp
@@ -1,3 +1,5 @@
+/* Approach 1 Time :- O(N*N) Space :- O(N*N) */
+
 class Solution
 {
 public:
@@ -26,4 +28,36 @@ public:
         }
         return res;
     }
+};
+
+/* Approach 2 Time :- O(N*N) Space :- O(N*N) + O(N) */
+
+class Solution
+{
+public:
+int dp[501][501];
+int solve(vector<vector<int>>& matrix,int n,int x, int y)
+{
+   if(x>=n||y>=n||x<0||y<0)
+   {
+       return 0;
+   }
+   if(dp[x][y]!=-1)
+   {
+       return dp[x][y];
+   }
+   return dp[x][y]=matrix[x][y]+max({solve(matrix,n,x+1,y+1),solve(matrix,n,x+1,y-1),solve(matrix,n,x+1,y)});
+}
+
+   int maximumPath(int N, vector<vector<int>> Matrix)
+   {
+       memset(dp,-1,sizeof(dp));
+       int ans=0;
+       for(int i=0;i<Matrix[0].size();i++)
+       {
+           int temp=solve(Matrix,N,0,i);
+           ans=max(ans,temp);
+       }
+       return ans;
+   }
 };
