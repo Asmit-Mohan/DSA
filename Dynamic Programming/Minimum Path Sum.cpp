b0b2e8b1335e92d89be8c4ab388cb9cde289bb04
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-04 18:08:17 +0530

    Update Minimum Path Sum.cpp

--- Dynamic Programming/Minimum Path Sum.cpp
+++ Dynamic Programming/Minimum Path Sum.cpp
@@ -1,3 +1,5 @@
+/* Approach 1 Time :- O(M*N) Space :- O(M*N) */ 
+
 class Solution
 {
 public:
@@ -26,4 +28,35 @@ public:
         }
         return dp[m-1][n-1];
     }
+};
+
+/* Approach 2 Time :- O(M*N) Space :- O(M*N) + O(N) */
+
+class Solution 
+{
+public:
+	int solve(vector<vector<int>>&grid, int i, int j, vector<vector<int>>&dp)
+    {
+		if(i<0 || j<0)
+        {
+            return INT_MAX;    /* For Comparision Purpose */
+        }
+		if(i==0 && j==0)
+        {
+            return grid[i][j];
+        }
+		if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+		return dp[i][j] = grid[i][j] + min(solve(grid, i-1, j, dp), solve(grid, i, j-1, dp));   
+	}
+
+	int minPathSum(vector<vector<int>>& grid)
+    {
+		int n = grid.size();
+		int m = grid[0].size();
+		vector<vector<int>>dp(n, vector<int>(m, -1));
+		return solve(grid, n-1, m-1, dp);
+	}
 };
