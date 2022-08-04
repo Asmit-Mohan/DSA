28961500f549a02b456870cf6baa77761bb8fa83
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-04 19:43:31 +0530

    Update Unique Path 2.cpp

--- Dynamic Programming/Unique Path 2.cpp
+++ Dynamic Programming/Unique Path 2.cpp
@@ -1,3 +1,43 @@
+/* Approach 1 Time :- O(N*N) Space :- O(N*N) + O(N) */
+
+class Solution
+{
+public: 
+    int dp[102][102];
+    int solve(vector<vector<int>>& grid,int i,int j)
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        if(i>=m || j>=n )
+        {
+            return 0;
+        }
+        if(grid[i][j]==1 )
+        {
+            return 0;
+        }
+        if(i==m-1 && j==n-1 )
+        {
+            return 1;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int temp = solve(grid,i+1,j) + solve(grid,i,j+1);
+        return dp[i][j]=temp;
+    }
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& grid)
+    {
+        memset(dp,-1,sizeof dp);
+        return solve(grid,0,0);    
+    }
+};
+
+/* Approach 2 Time :- O(N*N) Space :- O(N*N) */
+
 class Solution
 {
 public:
