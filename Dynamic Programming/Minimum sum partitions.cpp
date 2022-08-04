f625df52752d86e42bf3589e29aab50a34290b6f
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-05 01:13:45 +0530

    Update Minimum sum partitions.cpp

--- Dynamic Programming/Minimum sum partitions.cpp
+++ Dynamic Programming/Minimum sum partitions.cpp
@@ -1,53 +1,46 @@
+/* Time :- O(N*SUM) Space :- O(N*SUM) */
+
 class Solution
 {
   public:
-     int minDifference(int arr[], int n) 
-     { 
-	int sum=0;
-	for(int i=0;i<n;i++)
+	int minDifference(int arr[], int n)
+	{ 
+        int sum = 0; 
+        sort(arr, arr+n);
+        for(int i=0; i<n; i++)
         {
-           sum=sum+arr[i];
+            sum = sum + arr[i];
         }
-        int t[n+1][sum+1];
+        bool dp[n+1][sum+1];
+        memset(dp, false, sizeof(dp));
+        for (int i=0; i<n+1; i++)
+        {
+            for (int j=0; j<sum+1; j++)
+            {
+                if(j==0)
+                {
+                    dp[i][j] = true;
+                }
+                else if(i==0)
+                {
+                    dp[i][j] = false;
+                }
+                else if(arr[i-1]<=j)
+                {
+                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
         
-	for(int i=0;i<=n;i++)
+        int ans = INT_MAX;
+        for (int i=0; i<=(sum/2); i++)
         {
-            for(int j=0;j<=sum;j++)
+            if (dp[n][i])
             {
-                if(i==0)
-                {
-                    t[i][j]=0;
-                }
-                else if(j==0)
-                {
-                    t[i][j]=1;
-                }
-            }
-        }
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=sum;j++)
-            {
-                if(arr[i-1]<=j)
-                {
-                    t[i][j]=t[i - 1][j - arr[i-1]]||t[i-1][j];
-                }
-                else
-                {
-                    t[i][j]=t[i-1][j];
-                }
-            }
-        }
-        int ans=INT_MAX;
-        for(int i=0;i<=sum/2;i++)
-        {
-            int s1=i;
-            int s2=sum-i;
-            if(t[n][i]==1&&ans>abs(s1-s2))
-            {
-                ans=abs(s1-s2);
+                ans = min(ans, (sum-2*i));
             }
         }
         return ans;
-    }
+    } 
 };
