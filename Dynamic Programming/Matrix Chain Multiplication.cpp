1390b5575c44e1e98af816360b226e4f9dc7e32a
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-06 15:00:45 +0530

    Update Matrix Chain Multiplication.cpp

--- Dynamic Programming/Matrix Chain Multiplication.cpp
+++ Dynamic Programming/Matrix Chain Multiplication.cpp
@@ -1,3 +1,5 @@
+/* Time Complexity :- O(N*N*N) Space :- (N*N) + O(N) Recursive Stack */
+
 class Solution
 {
 public:
@@ -24,5 +26,31 @@ public:
     {
         memset(dp,-1,sizeof(dp));
         return solve(arr,1,n-1);
+    }
+};
+
+/* Time Complexity :- O(N*N*N) Space :- (N*N) */
+
+class Solution
+{
+public:
+    int dp[101][101];
+    int matrixMultiplication(int N, int arr[])
+    {
+       memset(dp,0,sizeof(dp));
+       for(int i=N-1;i>=1;i--)
+       {
+         for(int j=i+1;j<=N-1;j++)
+           {
+               int ans=INT_MAX; 
+               for(int k=i;k<j;k++)
+               {
+                   dp[i][j]=dp[i][k] + dp[k+1][j] + arr[i-1]*arr[k]*arr[j]; 
+                   ans=min(ans,dp[i][j]); 
+               }
+               dp[i][j]=ans; 
+           }
+       }
+       return dp[1][N-1];
     }
 };
