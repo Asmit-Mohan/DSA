2ea6480167c9b1a650abd8dc622663b598609a93
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-06 15:47:41 +0530

    Update Palindromic patitioning.cpp

--- Dynamic Programming/Palindromic patitioning.cpp
+++ Dynamic Programming/Palindromic patitioning.cpp
@@ -1,130 +1,114 @@
-/* GFG Accepted Approach Leetcode TLE */
+/* Memorization Approach Time Complexity is :- O(N*N*N) Space :- O(N*N) */
 
 class Solution
 {
 public:
-int solve(string str,int i,int j)
-{
-    if(i>=j)
-    {
-        return 0;
-    }
-    if(dp[i][j]!=-1)
-    {
-        return dp[i][j];
-    }
-    if(ispalin(str,i,j))
-    {
-        return 0;
-    }
-    if(str.size()==0||str.size()==1)
-    {
-        return 1;
-    }
-    int ans=INT_MAX;
-    for(int k=i;k<j;k++)
-    {
-        int left;
-        int right;
-        if(dp[i][k]!=-1)
-        {
-            left=dp[i][k];
-        }
-        else
-        {
-            left=solve(str,i,k);
-        }
-        
-        if(dp[k+1][j]!=-1)
-        {
-            right=dp[k+1][j];
-        }
-        else
-        {
-            right=solve(str,k+1,j);
-        }
-        
-        int temp=1+left+right;
-        ans=min(ans,temp);
-    }
-    return dp[i][j]=ans;
-}
-    int palindromicPartition(string str)
-    {
-        memset(dp,-1,sizeof(dp));
-        return  solve(str,0,str.length()-1);
-    }
-};
-
-/* Leetcode and GFG Accepted Approach */
-
-class Solution
-{
-public:
+ 
 int dp[2001][2001];
-bool isPal(string &str, int i, int j)
+bool palin(int i,int j,string &str)
 {
-    while(i < j)
+    while(i<j)
     {
-        if(str[i] != str[j])
+        if(str[i]!=str[j])
         {
             return 0;
         }
         i++;
         j--;
-    }    
+    }
     return 1;
 }
     
-    int solve(string &str, int i, int j)
+    int solve(int i,int j,string &str)
     {
-        int ans = INT_MAX;
-        int right;
-        
-        if(i >= j)
+        if(i>=j)
         {
-            return dp[i][j]=0;
+            return 0;
         }
         
-        if(isPal(str, i, j))
+        if(palin(i,j,str))
         {
-            return dp[i][j]=0;
+          return 0;  
         }
         
-        if(dp[i][j] != -1)
+        if(dp[i][j]!=-1)
         {
             return dp[i][j];
         }
         
-        for(int k = i; k < j; k++)
-        {  
-            if(isPal(str,i,k))  /* Left part will be calculated here */
+        int ans=INT_MAX;
+        
+        for(int k=i;k<j;k++)
+        {
+            int right;
+            if(palin(i,k,str)) /* Left Part Solved Here To Optimize */
             {
-                if(dp[k+1][j] != -1)
+                if(dp[k+1][j]!=-1)
                 {
-                     right = dp[k+1][j];
+                    right=dp[k+1][j];
                 }
                 else
                 {
-                    right = solve(str,k+1,j);
+                    right=solve(k+1,j,str);
                 }
-                
-                dp[k + 1][j] = right;
-                int temp =  right + 1;
-                ans = min(ans,temp);   
             }
+            int temp=1+right;
+            ans=min(ans,temp);
         }
-        return dp[i][j] = ans;
+        return dp[i][j]=ans;
     }
     
-    int minCut(string s) 
+    int minCut(string s)
     {
         if(s.length()==1)
         {
             return 0;
         }
-        
+        if(palin(0,s.length()-1,s))
+        {
+            return 0;
+        }
         memset(dp,-1,sizeof(dp));
-        return solve(s,0,s.size()-1);
+        return solve(0,s.length()-1,s);
+    }
+};
+
+/* Tabulation Approach Time Complexity is :- O(N*N*N) Space :- O(N) */
+
+class Solution
+{
+public:
+bool isPalin(int l,int r,string &str)
+{
+    while(l<r)
+    {
+        if(str[l]!=str[r])
+        {
+            return 0;
+        }
+        l++;
+        r--;
+    }
+    return 1;
+}
+    
+    int minCut(string s)
+    {
+        int n=s.size();
+        vector<int> dp(n+1,0);
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            int ans=INT_MAX;
+            for(int j=i;j<n;j++)
+            {
+                if(isPalin(i,j,s))
+                {
+                    ans=min(ans,1+dp[j+1]);
+                }
+            }
+            dp[i]=ans;
+        }
+        return dp[0]-1;
     }
 };
