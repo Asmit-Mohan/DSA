12855d6d5afb02b70b32da1a9a37eb58e6bb3a2b
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-08 00:43:27 +0530

    Update Total Decoding Messages.cpp

--- Dynamic Programming/Total Decoding Messages.cpp
+++ Dynamic Programming/Total Decoding Messages.cpp
@@ -1,3 +1,5 @@
+/* Time :- O(N) Space :- O(N) + O(N) */
+
 class Solution
 {
 	public:
@@ -34,4 +36,35 @@ class Solution
 	    memset(dp,-1,sizeof(dp));
 	    return solve(str,pos);
     	}
+};
+
+/* Time :- O(N) Space :- O(N) --> LC Accepted GFG Wrong Answer */
+
+class Solution
+{
+public:
+    int numDecodings(string s)
+    {
+        int n=s.length();
+        if(s[0]=='0')
+        {
+            return 0;
+        }
+        
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            if(s[i-1]-'0'>0)
+            {
+                dp[i]=dp[i-1];
+            }
+            if(s[i-2]=='1' || (s[i-2]-'0'==2 && s[i-1]-'0'<=6))
+            {
+                dp[i]+=dp[i-2];
+            }   
+        }
+        return dp[n];
+    }
 };
