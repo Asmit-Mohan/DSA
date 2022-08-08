8c93b62b94f26d7fd1bd9a37cf5501381f7f312b
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-08 12:18:38 +0530

    Update Kth largest element in a stream.cpp

--- Heap/Kth largest element in a stream.cpp
+++ Heap/Kth largest element in a stream.cpp
@@ -1,24 +1,30 @@
+/* Time :- O(Nlogk) Space :- O(K) */
+
 class Solution
 {
   public:
-    vector<int> kthLargest(int k, int nums[], int n)
+    vector<int> kthLargest(int k, int arr[], int n)
     {
-         priority_queue <int, vector<int>, greater<int> > minh;
-         vector<int>v;
-         for(int i=0;i<n;i++)
-         {
-             minh.push(nums[i]);
-             if(minh.size()>k)
-             {
-                 minh.pop();
-             }
-             if(minh.size()<k)
-             {
-                v.push_back(-1);
-                continue;
-             }
-            v.push_back(minh.top());
-         }
-        return v;     
+        vector<int>ans;
+        priority_queue<int, vector<int>, greater<int>>pq;
+        
+        for(int i=0; i<n; i++)
+        {
+            pq.push(arr[i]);
+            if(pq.size() < k)
+            {
+                ans.push_back(-1);
+            }
+            else if(pq.size() == k)
+            {
+                ans.push_back(pq.top());
+            }
+            else
+            {
+                pq.pop();
+                ans.push_back(pq.top());
+            }
+        }
+        return ans;   
     }
 };
