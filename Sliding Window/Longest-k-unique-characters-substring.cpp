ccdca141f8059e57afb6c6b34320b854a2cb517d
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-08 14:44:43 +0530

    Update Longest-k-unique-characters-substring.cpp

--- Sliding Window/Longest-k-unique-characters-substring.cpp
+++ Sliding Window/Longest-k-unique-characters-substring.cpp
@@ -1,42 +1,39 @@
+/* Time :- O(N) Space :- O(K) */
+
 class Solution
 {
   public:
-    int longestKSubstr(string s, int k)
+    int longestKSubstr(string arr, int k)
     {
-       int i=0;
-       int j=0;
-       int n=s.length();
-       unordered_map<char,int>mp;
-       int ans=-1;
-       
-       while(j<n)
-       {
-          mp[s[j]]++;
-          if(mp.size()<k)
-          {
-              j++;
-          }
-          
-          if(mp.size()==k)
-          {
-             ans=max(ans,j-i+1);
-             j++;
-          }
-         
-           if(mp.size()>k)
-           {
-               while(mp.size()>k)
-               {
-                   mp[s[i]]--;
-                   if(mp[s[i]]==0)
-                   {
-                       mp.erase(s[i]);
-                   }
-                   i++;
-               }
-               j++;
-           }
-       }
-       return ans;
+        int i=0;
+        int j=0;
+        int ans=INT_MIN;
+        unordered_map<int,int>mp;
+        
+        while(j<arr.length())
+        {
+            mp[arr[j]]++;
+            
+            if(mp.size()==k)
+            {
+                int window=j-i+1;
+                ans=max(ans,window);
+            }
+            
+            if(mp.size()>k)
+            {
+                while(mp.size()>k)
+                {
+                    mp[arr[i]]--;
+                    if(mp[arr[i]]==0)
+                    {
+                        mp.erase(arr[i]);
+                    }
+                    i++;
+                }
+            }
+            j++;
+        }
+        return ans==INT_MIN?-1:ans;
     }
 };
