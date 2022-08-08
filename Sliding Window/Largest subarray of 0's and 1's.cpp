ff3303efab91b4b644674a530d40e39057dd1e47
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-08 14:40:22 +0530

    Update Largest subarray of 0's and 1's.cpp

--- Sliding Window/Largest subarray of 0's and 1's.cpp
+++ Sliding Window/Largest subarray of 0's and 1's.cpp
@@ -1,48 +1,53 @@
+/* Time :- O(N) Space :- O(N) */ 
+
 class Solution
 {
-  public:
-    int maxLen(int A[], int n)
+public:
+    int findMaxLength(vector<int>& nums)
     {
-        int z=0;
-        int o=0;
-        for(int i=0;i<n;i++)
-        {
-            if(A[i]==0)
-            {
-                z++;
-                A[i]=-1;
-            }
-            else
-            {
-                o++;
-            }
-        }
+       int zero=0;
+       int one=0;
         
-        if(o==z)
-        {
-            return n;
-        }
-        /*Now the problem is largest subarray with sum==0*/
-        int sum=0;
-        int ans=INT_MIN;
-        int j=0;
-        unordered_map<int,int> mp;
-        mp[0]=0;
+       for(int i=0;i<nums.size();i++)
+       {
+           if(nums[i]==0)
+           {
+               zero++;
+               nums[i]=-1;
+           }
+           else
+           {
+               one++;
+           }
+       }
+       
+       if(one==zero)
+       {
+          return nums.size();   
+       }
         
-        while(j<n)
-        {
-            sum=sum+A[j];
-            if(mp.find(sum)!=mp.end())  
-            {
-                ans=max(ans,j-mp[sum]+1);
-            }
-            else
-            {
-                 mp[sum]=j+1;
-            }
-            j++;
-        }
-        return ans==INT_MIN?0:ans;
+       int i=0;
+       int j=0;
+       int ans=INT_MIN;
+       int sum=0;
+       unordered_map<int,int>mp; 
+       mp[0]=0;
+        
+       while(j<nums.size())
+       {
+           sum=sum+nums[j];
            
+           if(mp.find(sum)!=mp.end())
+           {
+               ans=max(ans,j-mp[sum]+1);
+           }
+           else
+           {
+               mp[sum]=j+1;
+           }
+           
+           j++;
+       }
+       return ans==INT_MIN?0:ans;
     }
 };
