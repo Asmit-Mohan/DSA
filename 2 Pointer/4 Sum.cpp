e91b3620c29c3e7cfac359d8d044c329922055fa
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-09 16:54:42 +0530

    Update 4 Sum.cpp

--- 2 Pointer/4 Sum.cpp
+++ 2 Pointer/4 Sum.cpp
@@ -1,60 +1,60 @@
 class Solution
 {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target)
+    vector<vector<int>> fourSum(vector<int>& nums, int target) 
     {
-        const int n = nums.size();
-        if (n < 4)
-        {
-            return {};
-        }
-        sort( nums.begin(), nums.end());
-        vector<vector<int>> result;
-        for (int i = 0; i < n - 2; ++i)
-        {
-            if (i > 0 && nums[i] == nums[i - 1])
-            {
-                continue;
-            }
-            for (int j = i + 1; j < n - 1; ++j)
-            {
-                if (j > i + 1 && nums[j] == nums[j - 1])
-                {
-                    continue;
-                }
-                
-                int l = j + 1;
-                int r = n - 1;
-                int t = target - nums[i] - nums[j];
-                while (l < r)
-                {
-                int sum = nums[l] + nums[r];
-                if (sum == t)
-                {
-                        vector<int>temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[j]);
-                        temp.push_back(nums[l]);
-                        temp.push_back(nums[r]);
-                        
-                        result.push_back(temp);
-                        l++;
-                        while (l < r && nums[l] == nums[l - 1])
-                        {
-                            l++;
-                        }
-                    }
-                    else if (sum < t)
-                    {
-                        l++;
-                    }
-                    else
-                    {
-                        r--;
-                    }
-                }
-            }
-        }
-        return result;
+        
+     vector<vector<int>>ans;
+     int n = nums.size();
+     sort(nums.begin() , nums.end());
+     
+     for(int i = 0 ; i <n;i++)
+     {
+         long long int temp1 = target - nums[i];
+         
+         for(int j = i+1 ; j<n;j++)
+         {
+             int l = j+1;
+             int r = n-1;
+             long long int temp2 = temp1 - nums[j];
+             
+             while(l<r)
+             {
+                 long long int sum = nums[l] + nums[r];
+                 
+                 if(sum == temp2)
+                 {
+                     ans.push_back({nums[i] , nums[j] , nums[l] , nums[r]});
+                     
+                 }
+                 if(temp2 > sum)
+                 {
+                     
+                     while(l+1 < n and nums[l] == nums[l+1])
+                     {
+                         l++;
+                     }
+                     l++;
+                 }
+                 else
+                 {
+                     while(r>1 and nums[r] == nums[r-1])
+                     {
+                         r--;
+                     }
+                     r--;
+                 }
+             }
+             while(j+1 < n and nums[j] == nums[j+1])
+             {
+                 j++;
+             }
+         }
+         while(i+1 < n and nums[i] == nums[i+1])
+         {
+             i++;
+         }
+      }
+      return ans;
     }
 };
