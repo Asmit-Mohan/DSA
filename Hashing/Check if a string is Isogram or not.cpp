e604d27b3098ebe00ce1fe73547687f49ddcaa16
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-09 22:13:38 +0530

    Update Check if a string is Isogram or not.cpp

--- Hashing/Check if a string is Isogram or not.cpp
+++ Hashing/Check if a string is Isogram or not.cpp
@@ -1,32 +1,19 @@
+/* Time :- O(N) Space :- O(26) */
+
 class Solution
 {
     public:
     bool isIsogram(string s)
     {
-    int flag=0;
-    vector<int> hash(26,0);
-    for(int i=0;i<s.length();i++)
-    {
-        hash[s[i]-'a']++;
-    }
-    for(int i=0;i<26;i++)
-      {
-          if(hash[i]==0 || hash[i]==1)
-          {
-              
-          }
-         else
-          {
-             flag=1;    
-          }
-      }
-      if(flag==0)
-      {
-          return 1;
-      }
-      else
-      {
-          return 0;
-      }
+        int hash[26]={0};
+        for(int i=0;i<s.length();i++)
+        {
+            hash[s[i]-'a']++;
+            if(hash[s[i]-'a']>1)
+            {
+                return 0;
+            }
+        }
+        return 1;
     }
 };
