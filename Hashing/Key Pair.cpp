210b67455a7f54422502270990ef0939b2aa842d
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-09 22:38:43 +0530

    Update Key Pair.cpp

--- Hashing/Key Pair.cpp
+++ Hashing/Key Pair.cpp
@@ -1,9 +1,11 @@
+/* Time :- O(N) Space :- O(N) */
+
 class Solution
 {
 public:
 	bool hasArrayTwoCandidates(int arr[], int n, int x)
 	{
-	    set<int>s;
+	    unordered_set<int>s;
 	    for(int i=0;i<n;i++)
 	    {
 	        if(s.find(x-arr[i])==s.end())
