1d1e26b482bc2b6e2ba16e2033aec9d907ce4402
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-10 14:47:46 +0530

    Update Quick Sort.cpp

--- Sorting/Quick Sort.cpp
+++ Sorting/Quick Sort.cpp
@@ -1,36 +1,54 @@
+/* Time :- O(Nlogn) [Average Case and Best Case] :- O(N*N) [Worst case complexity] Space :- O(1) */
+
+/* Worst case when the partition process always picks the greatest or smallest element as the pivot when sorted in ascending or descending order */ 
+
+class Solution
+{
+    public:
     void quickSort(int arr[], int low, int high)
     {
-        if(low<high)
-        {
-            int pivot_idx=partition(arr,low,high);
-            quickSort(arr,low,pivot_idx-1);
-            quickSort(arr,pivot_idx+1,high);
-        }
+       if(low<high)
+       {
+           int pivot=partition(arr,low,high);
+           quickSort(arr,low,pivot-1);
+           quickSort(arr,pivot+1,high);
+       }
     }
-
-
+    
+    public:
     int partition (int arr[], int low, int high)
     {
-        int pivot=arr[low];
-        int l=low+1;
-        int r=high;
-        while(l<=r)
-        {
-            while(arr[l]<=pivot)
-            {
-                l++;
-            }
-            while(arr[r]>pivot)
-            {
-                r--;
-            }
-            if(r>l)
-            {
-                swap(arr[l],arr[r]);
-                l++;
-                r--;
-            }
-        }
-        swap(arr[low],arr[r]);
-        return r;
+       int pivot=arr[low];
+       int l=low+1;
+       int r=high;
+       
+       while(l<=r)
+       {
+           while(pivot>=arr[l])
+           {
+               l++;
+           }
+           while(pivot<arr[r])
+           {
+               r--;
+           }
+           if(l<r)
+           {
+               swap(arr[l],arr[r]);
+               l++;
+               r--;
+           }
+       }
+       swap(arr[r],arr[low]);
+       return r;
     }
+};
+
+/* Key points :- 
+
+1. It is not stable algorithm.
+2. It is an in-place sorting algorithm.
+3. It is preferred over merge sort [Not in case of linked list] as space is O(1) only.
+4. More optimised version is randomised quick sort.
+
+*/
