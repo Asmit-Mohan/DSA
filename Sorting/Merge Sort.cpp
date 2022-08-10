c91b94d9fd264241162b3b2d7f2c3af789158b5c
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-10 15:28:04 +0530

    Update Merge Sort.cpp

--- Sorting/Merge Sort.cpp
+++ Sorting/Merge Sort.cpp
@@ -1,63 +1,61 @@
+/* Time :- O(Nlogn) [All Cases] Space :- O(N) */ 
+
 class Solution
 {
     public:
-    void merge(int arr[], int l, int mid, int r)
+    void merge(int arr[], int low, int mid, int high)
     {
-        int i=l;
+        int i=low;
         int j=mid+1;
-        int k=l;
-        int temp[1000000];
+        int idx=low;
         
-        while(i<=mid&&j<=r)
+        int temp[high+1];
+        
+        while(i<=mid&&j<=high)
         {
-            if(arr[i]<=arr[j])
+            if(arr[i]>arr[j])
             {
-                temp[k]=arr[i];
-                i++;
+                temp[idx++]=arr[j++];
             }
             else
             {
-                temp[k]=arr[j];
-                j++;
+                temp[idx++]=arr[i++];
             }
-            k++;
         }
-       
-            if(i>mid)
-            {
-                while(j<=r)
-                {
-                    temp[k]=arr[j];
-                    j++;
-                    k++;
-                }
-            }
-            
-            if(j>r)
-            {
-                while(i<=mid)
-                {
-                    temp[k]=arr[i];
-                    i++;
-                    k++;
-                }
-            }
         
-        for(int i=l;i<=r;i++)
+        while(i<=mid)
+        {
+            temp[idx++]=arr[i++];
+        }
+        while(j<=high)
+        {
+            temp[idx++]=arr[j++];
+        }
+        
+        for(int i=low;i<=high;i++)
         {
             arr[i]=temp[i];
         }
     }
-  
+    
     public:
-    void mergeSort(int arr[], int l, int r)
+    void mergeSort(int arr[], int low, int high)
     {
-        if(l<r)
+        if(low<high)
         {
-            int mid=(l+r)/2;
-            mergeSort(arr,l,mid);
-            mergeSort(arr,mid+1,r);
-            merge(arr,l,mid,r);
+            int mid=low+(high-low)/2;
+            mergeSort(arr,low,mid);
+            mergeSort(arr,mid+1,high);
+            merge(arr,low,mid,high);
         }
     }
 };
+
+/* Key Points :-
+
+1. T(n) = 2T(n/2) + θ(n) [ All cases ]
+2. In Place :- Nope
+3. Stable :- Yes
+4. Preferred over quick sort in case of linked list since mid element can  be get in O(1) Space and no furthur space is required for merging.
+
+*/
