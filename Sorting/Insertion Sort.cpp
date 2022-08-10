f92cb3834771381e1b64898fe6aeb80c03e0a146
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-10 12:17:42 +0530

    Update Insertion Sort.cpp

--- Sorting/Insertion Sort.cpp
+++ Sorting/Insertion Sort.cpp
@@ -1,3 +1,5 @@
+/* Time :- O(N*N) [Average and Worst case] :- O(N) [Best Case] Space :- O(1) */
+
 class Solution
 {
     public:
@@ -6,7 +8,7 @@ class Solution
         int j=i-1;
         int current = arr[i];
         
-        while(j>=0&&arr[j]>arr[i]) 
+        while(j>=0&&arr[j]>current) 
         {
             arr[j+1]=arr[j];
             j--;
@@ -23,3 +25,24 @@ class Solution
         }
     }
 };
+
+/* Recursive Approach O(N*N) [Average and Worst case] :- O(N) [Best Case] Space :- O(N) [ Auxiliary Stack Space ] */
+
+void insertionSortRecursive(int arr[], int n)
+{
+    if (n < 2)
+    {
+        return;
+    }
+    insertionSortRecursive( arr, n-1 );
+    int last = arr[n-1];
+    int j = n-2;
+    while (j >= 0 && arr[j] > last)
+    {
+        arr[j+1] = arr[j];
+        j--;
+    }
+    arr[j+1] = last;
+}
+
+/* Key Points :- (1) It is appropriate for data sets which are already partially sorted (2) In-place Algorithm (3) Stable Algorithm */
