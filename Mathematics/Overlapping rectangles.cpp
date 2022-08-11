870ca6a082ae1044b813bf906485848a950bb9c4
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-11 15:37:07 +0530

    Update Overlapping rectangles.cpp

--- Mathematics/Overlapping rectangles.cpp
+++ Mathematics/Overlapping rectangles.cpp
@@ -1,3 +1,5 @@
+/* GFG */
+
 class Solution
 {
   public:
@@ -16,4 +18,26 @@ class Solution
             return true;
         }
     }
+};
+ 
+/* Leetcode */
+
+class Solution
+{
+   public:
+   bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) 
+   {
+       if(rec1[0]>=rec2[2] || rec2[0]>=rec1[2])
+       {
+           return false;
+       }
+       else if(rec2[1]>=rec1[3] || rec1[1]>=rec2[3]) 
+       {
+           return false;
+       }
+       else
+       {
+           return true;
+       }
+   }
 };
