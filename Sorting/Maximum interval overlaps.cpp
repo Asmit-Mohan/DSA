99ad4ce2f4528b1299e69c48331b0c714ece52a2
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-12 16:09:47 +0530

    Update Maximum interval overlaps.cpp

--- Sorting/Maximum interval overlaps.cpp
+++ Sorting/Maximum interval overlaps.cpp
@@ -1,38 +1,40 @@
+/* Time :- O(Nlogn) Space :- O(1) */
+
 class Solution
 {
 	public:
 	vector<int> findMaxGuests(int start[], int end[], int n)
 	{
-        sort(start,start+n);
-        sort(end,end+n);
-        
-        int curr_guest=1;
-        int max_guest=1;
-        int req_time = start[0];
-        int i=1;
-        int j=0;
-        
-        while(i<n&&j<n)
-        {
-            if(start[i]<=end[j])
-            {
-                curr_guest++;
-                if(curr_guest>max_guest)
-                {
-                    max_guest=curr_guest;
-                    req_time=start[i];
-                }
-                i++;
-            }
-            else
-            {
-                curr_guest--;
-                j++;
-            }
-        }
-        vector<int>ans;
-        ans.push_back(max_guest);
-        ans.push_back(req_time);
-        return ans;
+		sort(start,start+n);
+		sort(end,end+n);
+
+		int curr_guest=1;
+		int max_guest=1;
+		int req_time = start[0];
+		int i=1;
+		int j=0;
+
+		while(i<n&&j<n)
+		{
+		    if(start[i]<=end[j])
+		    {
+			curr_guest++;
+			if(curr_guest>max_guest)
+			{
+			    max_guest=curr_guest;
+			    req_time=start[i];
+			}
+			i++;
+		    }
+		    else
+		    {
+			curr_guest--;
+			j++;
+		    }
+		}
+		vector<int>ans;
+		ans.push_back(max_guest);
+		ans.push_back(req_time);
+		return ans;
 	}
 };
