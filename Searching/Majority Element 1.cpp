980526a3dfbe78267baf84ca568232120c78bac7
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-12 19:04:32 +0530

    Update Majority Element 1.cpp

--- Searching/Majority Element 1.cpp
+++ Searching/Majority Element 1.cpp
@@ -1,42 +1,29 @@
+/* Approach 1 Using Hashmap Time :- O(N) Space :- O(N) */
+
+/* Approach 2 Using Voting Algorithms Time :- O(N) Space :- O(1) */
+
 class Solution
 {
-  public:
-     
-    int majorityElement(int arr[], int n)
+public:
+    int majorityElement(vector<int>& nums)
     {
-        int candidate=arr[0];
-        int count=1;
-        for(int i=1;i<n;i++)
-        {
-            if(arr[i]==candidate)
-            {
-                count++;
-            }
-            else
-            {
-                count--;
-                if(count==0)
-                {
-                    count=1;
-                    candidate=arr[i];
-                }
-            }
-        }
-        count=0;
-        for(int i=0;i<n;i++)
-        {
-            if(candidate==arr[i])
-            {
-                count++;
-            }
-        }
-        if(count>n/2)
-        {
-            return candidate;
-        }
-        else
-        {
-            return -1;
-        }
+          int count=0;
+          int res;
+          for(int i=0;i<nums.size();i++)
+          {
+              if(count==0)
+              {
+                  res=nums[i];
+              }
+              if(nums[i]==res)
+              {
+                  count++;
+              }
+              else
+              {
+                  count--;
+              }
+          }
+        return res;
     }
 };
