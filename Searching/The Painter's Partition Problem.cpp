0186e601abbae28b97e6a1903e68d35e471ab596
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-14 00:57:12 +0530

    Update The Painter's Partition Problem.cpp

--- Searching/The Painter's Partition Problem.cpp
+++ Searching/The Painter's Partition Problem.cpp
@@ -1,54 +1,58 @@
+/* Time :- O(Nlogm) Space :- O(1) */
+
 class Solution
 {
    public:
-   int isvalid(int arr[],int n,int k,int mid)
+   int isvalid(int arr[],int n,int painterAvailable,int maximumArea)
     {
-        int curr=1;
-        long long sum=0;
+        int reqPainter=1;
+        long long currArea=0;
+        
         for(int i=0;i<n;i++)
         {
-            sum=sum+arr[i];
-            if(sum>mid)
+            currArea=currArea+arr[i];
+            if(currArea>maximumArea)
             {
-                curr++;
-                sum=arr[i];
+                reqPainter++;
+                currArea=arr[i];
             }
-            if(curr>k)
+            if(reqPainter>painterAvailable)
             {
                return 0;
             }
         }
         return 1;
     }
+    
     long long minTime(int arr[], int n, int k)
     {
-         if(n>=k)
-         {
-             long long l=*max_element(arr,arr+n);
-             long long r=0;
-             for(int i=0;i<n;i++)
-             {
-                 r=r+arr[i];
-             }
-             long long res=-1;
-             while(l<=r)
-             {
-                 int mid=l+((r-l)/2);
-                 if(isvalid(arr,n,k,mid))
-                 {
-                     res=mid;
-                     r=mid-1; // To find more optimised solution
-                 }
-                 else
-                 {
-                     l=mid+1;
-                 }
-             }
-             return res;
-         }
-         else
-         {
-            return *max_element(arr,arr+n);
-         }
+        if(k>n)
+        {
+           return *max_element(arr,arr+n);
+        }
+        
+        long long r=0;
+        for(int i=0;i<n;i++)
+        {
+            r=r+arr[i];
+        }
+        
+        long long res=-1;
+        long long l=*max_element(arr,arr+n);
+        
+        while(l<=r)
+        {
+            int mid=l+((r-l)/2);
+            if(isvalid(arr,n,k,mid))
+            {
+                res=mid;
+                r=mid-1; 
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return res;
     }
 };
