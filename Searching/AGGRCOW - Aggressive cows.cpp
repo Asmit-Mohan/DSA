45d86dde8af89133a36ceb16ae83a59551ac3c54
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-13 23:13:19 +0530

    Update AGGRCOW - Aggressive cows.cpp

--- Searching/AGGRCOW - Aggressive cows.cpp
+++ Searching/AGGRCOW - Aggressive cows.cpp
@@ -1,51 +1,42 @@
-#include <bits/stdc++.h>
-#define ll long long
-using namespace std;
-bool solve(ll dist,ll arr[],ll n,ll cow)
+class Solution
 {
-	ll count=1;
-	ll lastPlaced=arr[0];
-	for(ll i=1;i<n;i++)
+public:
+    bool solve(int dist,vector<int>arr,int n,int cow)
+    {
+        int count=1;
+        int lastPlaced=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            if(abs(arr[i]-lastPlaced)>=dist)
+            {
+                count++;
+                lastPlaced=arr[i];
+            }
+        }
+        return count>=cow?1:0;
+    }
+    
+    int maxDistance(vector<int>& arr, int cow)
+    {
+        int n=arr.size();
+        int ans=INT_MIN;
+        sort(arr.begin(),arr.end());
+        
+	int low=1;
+	int high=arr[n-1]-arr[0];
+	while(low<=high)
 	{
-		if(arr[i]-lastPlaced>=dist)
-		{
-			count++;
-			lastPlaced=arr[i];
-		}
+	   int mid=low+(high-low)/2;
+	   if(solve(mid,arr,n,c))
+	   {
+	      ans=max(ans,mid);
+	      low=mid+1;
+	   }
+	   else
+	   {
+	      high=mid-1;
+	   }
 	}
-	return count>=cow?1:0;
-}
-int main()
-{
-	ll t;
-	cin>>t;
-	while(t--)
-	{
-	    ll n,c;
-	    ll ans=INT_MIN;
-	    cin>>n>>c;
-	    ll arr[n];
-	    for(ll i=0;i<n;i++)
-	    {
-	    	cin>>arr[i];
-	    }
-	    sort(arr,arr+n);
-	    ll low=arr[0];
-	    ll high=arr[n-1]-arr[0];
-	    while(low<=high)
-	    {
-	    	ll mid=low+(high-low)/2;
-	    	if(solve(mid,arr,n,c))
-	    	{
-	    		ans=max(ans,mid);
-	    		low=mid+1;
-	    	}
-	    	else
-	    	{
-	    		high=mid-1;
-	    	}
-	    }
-	    cout<<ans<<endl;
-	}
-	return 0;
-}
+       return ans;
+    }
+};
