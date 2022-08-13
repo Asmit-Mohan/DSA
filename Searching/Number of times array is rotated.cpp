54f2a4e14ac70dd2c551e98187fead9a1435ac5c
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-13 16:50:20 +0530

    Update Number of times array is rotated.cpp

--- Searching/Number of times array is rotated.cpp
+++ Searching/Number of times array is rotated.cpp
@@ -1,24 +1,26 @@
+/* Time :- O(logn) Space :- O(1) */
+
+/*Number of times array is rotated equals to index of minimum element*/
+
 class Solution
 {
 public:	
-
-  /*Number of times array is rotated equals to index of minimum element*/
-	int findKRotation(int a[], int n)
-	{
-	  int l=0;
-    int r=n-1;
-    while(l<r)
-    {
-        int mid=l+(r-l)/2;
-        if(a[mid]>a[r])
-        {
-           l=mid+1;  //It means r is near rotated sorted point
-        }
-        else
-        {
-           r=mid; //r is greater then all element next to r will be grater then move to mid
-        }
-    }
-    return l;
-	  }
+   int findKRotation(int a[], int n)
+   {
+      int l=0;
+      int r=n-1;
+      while(l<r)
+      {
+         int mid=l+(r-l)/2;
+         if(a[mid]>a[r])
+         {
+             l=mid+1;      //It means r is near rotated sorted point
+         }
+         else
+         {
+             r=mid;      //r is greater then all element next to r will be grater then move to mid
+         }
+     }
+     return l;
+   }
 };
