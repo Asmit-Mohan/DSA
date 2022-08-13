726d28e1fc51e4dde56b2ebe0e4d6aba984acf9c
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-13 14:43:02 +0530

    Update Kth smallest element in a sorted matrix.cpp

--- Searching/Kth smallest element in a sorted matrix.cpp
+++ Searching/Kth smallest element in a sorted matrix.cpp
@@ -19,45 +19,52 @@ int kthSmallest(int mat[MAX][MAX], int n, int k)
 
 /* Approach 2 Time :- O(N*logn) Space :- O(1) */
 
-int solve(int val,int mat[][MAX], int n)
+class Solution
 {
-    int start=0;
-    int end=n-1;
-    int count=0;
-    
-    while(start<n&&end>=0)
+public: 
+    int solve(int val,vector<vector<int>>& mat)
     {
-        if(mat[end][start]>val)
+        int n=mat.size();
+        int low=0;
+        int high=n-1;
+        int ans=0;
+        
+        while(low<n&&high>=0)
         {
-            end--;
+            if(mat[low][high]>val)
+            {
+                high--;
+            }
+            else
+            {
+                low++;
+                ans=ans+(high+1);
+            }
         }
-        else
-        {
-            count=count+end+1;
-            start++;
-        }
+        return ans;
     }
-    return count;
-}
-
-int kthSmallest(int mat[MAX][MAX], int n, int k)
-{
-    int low=mat[0][0];
-    int high=mat[n-1][n-1];
-    int ans;
     
-    while(low<=high)
+    int kthSmallest(vector<vector<int>>& mat, int k)
     {
-        int mid=(low+high)>>1;
-        if(solve(mid,mat,n)<k)
+        int n=mat.size();
+        int low=mat[0][0];
+        int high=mat[n-1][n-1];
+        int res=0;
+        
+        while(low<=high)
         {
-            low=mid+1;
+            int mid=low+(high-low)/2;
+            int ans=solve(mid,mat);
+            if(ans<k)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                res=mid;
+                high=mid-1;
+            }
         }
-        else 
-        {
-           ans=mid;
-           high=mid-1;
-        }
+        return res;
     }
-    return ans;
-}
+};
