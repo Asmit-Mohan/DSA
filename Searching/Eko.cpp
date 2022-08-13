39c626e54737790989f42af97052f73bf0ec9f02
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-13 16:38:01 +0530

    Update Eko.cpp

--- Searching/Eko.cpp
+++ Searching/Eko.cpp
@@ -1,50 +1,41 @@
-#include <bits/stdc++.h>
-#define ll long long
-using namespace std;
-ll solve(ll h,ll arr[],ll n)
+class Solution
 {
-	ll sum=0;
-	for(ll i=0;i<n;i++)
-	{
-		if(arr[i]>=h)
-		{
-			sum=sum+(arr[i]-h);
-		}
-	}
-	return sum;
-}
-int main()
-{
-	ll n,m;
-	cin>>n>>m;
-	ll arr[n];
-	for(ll i=0;i<n;i++)
-	{
-		cin>>arr[i];
-	}
-	ll low=0;
-	ll high=*max_element(arr,arr+n);
-	ll ans=INT_MIN;
-	
-	while(low<=high)
-	{
-		ll mid=low+(high-low)/2;
-		ll temp=solve(mid,arr,n);
-		if(temp==m)
-		{
-			ans=mid;
-			break;
-		}
-		else if(temp>m)
-		{
-			ans=max(ans,mid);
-			low=mid+1;
-		}
-		else
-		{
-			high=mid-1;
-		}
-	}
-	cout<<ans<<endl;
-	return 0;
-}
+    public:
+    int solve(int h,int arr[],int n)
+    {
+    	int sum=0;
+    	for(int i=0;i<n;i++)
+    	{
+    	    if(arr[i]>h)
+    	    {
+    		sum=sum+(arr[i]-h);
+    	    }
+    	}
+    	return sum;
+    }
+
+    int find_height(int arr[], int n, int m)
+    {
+        int low=0;
+    	int high=*max_element(arr,arr+n);
+     
+    	while(low<=high)
+    	{
+    	     int mid=low+(high-low)/2;
+    	     int temp=solve(mid,arr,n);
+    	     if(temp==m)
+    	     {
+    		return mid;
+    	     }
+    	     else if(temp>m)
+    	     {
+    		low=mid+1;
+    	     }
+    	     else
+    	     {
+    		high=mid-1;
+    	     }
+    	}
+        return -1;
+      }
+};
