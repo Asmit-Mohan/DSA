716ba85dfdab6040c0f3acadcd31a23230144de0
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-15 19:45:57 +0530

    Update Column name from a given column number.cpp

--- string/Column name from a given column number.cpp
+++ string/Column name from a given column number.cpp
@@ -1,15 +1,17 @@
+/* Time :- O(logn) Space :- O(1) */
+
 class Solution
 {
 public:
-string colName (long long int n)
-{
-    string result = "";
-    while(n)
+    string colName (long long int n)
     {
-        char c = 'A' + (n - 1) %26;
-        result = c + result;
-        n = (n - 1)/26;
-    }
-    return result;
+        string result = "";
+        while(n)
+        {
+            char c = 'A' + (n - 1) %26;
+            result = c + result;
+            n = (n - 1)/26;
+        }
+        return result;
     }
 };
