587e83c0b2a09cecd9b9836991505bb01dce4dde
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-15 19:07:55 +0530

    Update Remove duplicate charecters.cpp

--- string/Remove duplicate charecters.cpp
+++ string/Remove duplicate charecters.cpp
@@ -1,4 +1,4 @@
-/* Time :- O(N) Space :- O(N) */
+/* GFG Order Preserved as in given input string [Not Lexicographical order] Time :- O(N) Space :- O(N) */
 
 class Solution
 {
@@ -15,6 +15,40 @@ public:
 	         mp[x]=1;
 	     }
 	 }
+        return ans;
+    }
+};
+
+/* Leetcode :- Lexicographical Ordering */
+
+class Solution
+{
+public:
+    string removeDuplicateLetters(string s)
+    {
+        int hash[26]={0};
+        int visited[26]={0};
+        string ans="";
+        
+        for(auto x : s)
+        {
+            hash[x-'a']++;
+        }
+        
+        for(auto it : s)
+        {
+            hash[it-'a']--;
+            if(visited[it-'a']==0)
+            {
+                while(ans.length()>0&&ans.back()>it&&hash[ans.back()-'a']>0)
+                {
+                    visited[ans.back()-'a']=0;
+                    ans.pop_back();
+                }
+                ans.push_back(it);
+                visited[it-'a']=1;
+            }
+        }
         return ans;
     }
 };
