885f5d8a85d4884a481c6602c7cbc9fe67dbfe31
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-16 11:17:15 +0530

    Update Reverse individual words.cpp

--- STL/Reverse individual words.cpp
+++ STL/Reverse individual words.cpp
@@ -1,16 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
+/* Time :- O(N) Space :- O(N) */
+
+class Solution
 {
-	string str="Geeks for Geeks A Computer Science portal for Geeks";
-	string res;
-	stringstream ss(str);
-	string word;
-	while(ss>>word)
-	{
-		reverse(word.begin(),word.end());
-		res=res+word+" ";
-	}
-	cout<<res;
-	return 0;
-}
+  public:
+    string reverseWords (string str)
+    {
+        string ans="";
+        string temp="";
+        for(char it : str)
+        {
+            if(it=='.')
+            {
+                ans=ans+temp+".";
+                temp="";
+            }
+            else
+            {
+                temp=it+temp;
+            }
+        }
+        ans=ans+temp;
+        return ans;
+    }
+};
