1ab7f293d41e1b29dbf0002c7f1fb3b764487a10
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-16 11:05:53 +0530

    Update Minimum indexed charecter.cpp

--- STL/Minimum indexed charecter.cpp
+++ STL/Minimum indexed charecter.cpp
@@ -1,27 +1,28 @@
+/* Time :- O(n) Space :- O(n) */
+
 class Solution
 {
   public:
     int minIndexChar(string str, string patt)
     {
-        map<char,int>mp;
-        map<char, int>::iterator it ;
+        unordered_map<char,int>mp;  
         for(int i=0;i<str.length();i++)
         {
-            it = mp.find(str[i]);
-            if(it == mp.end())
+            if(mp.find(str[i]) == mp.end())
             {
                  mp[str[i]]=i;    
             }
         }
+        
         int idx=INT_MAX;
         for(int i=0;i<patt.size();i++)
         {
-            it = mp.find(patt[i]);
-            if(it != mp.end())
+            if(mp.find(patt[i]) != mp.end())
             {
                 idx=min(idx,it->second);  
             }
         }
+        
         if(idx==INT_MAX)
         {
             return -1;
@@ -31,4 +32,4 @@ class Solution
             return idx;
         }
     }
-}
+};
