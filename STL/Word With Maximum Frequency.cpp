d0a16570cd1513e3f10886390075844e82b52c73
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-16 10:36:23 +0530

    Update Word With Maximum Frequency.cpp

--- STL/Word With Maximum Frequency.cpp
+++ STL/Word With Maximum Frequency.cpp
@@ -1,16 +1,21 @@
+/* Time :- O(nlogn) Space :- O(n) */
+
 string maximumFrequency(string s)
 {
     stringstream str(s);
     string word;
     map<string,int>mp;
     int fre=0;
+    
     while (str>>word)
     {
         mp[word]++;
         fre = max(fre, mp[word]);
     }
+    
     stringstream st(s);
     string mx;
+    
     while(st >> word)
     {
         if(mp[word] == fre)
@@ -19,6 +24,7 @@ string maximumFrequency(string s)
             break;
         }
     }
+    
     mx=mx+" "+to_string(fre);
     return mx;
 }
