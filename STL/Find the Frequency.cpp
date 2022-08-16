a0516a8bdccb8c135a3eb2a039596dd446102401
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-16 11:00:46 +0530

    Update Find the Frequency.cpp

--- STL/Find the Frequency.cpp
+++ STL/Find the Frequency.cpp
@@ -1,11 +1,14 @@
+/* Time :- O(n) Space :- O(n) */
+
 int findFrequency(vector<int> v, int x)
 {
-    map<int,int>mp;
+    unordered_map<int,int>mp;
     for(int i=0;i<v.size();i++)
     {
         mp[v[i]]++;
     }
-    map<int,int>::iterator it;
+    
+    unordered_map<int,int>::iterator it;
     for(it=mp.begin();it!=mp.end();it++)
     {
         if(it->first==x)
