c1a6c3a623c3cf5c6ac8ca1b20ae5a6a1dbea196
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-16 16:24:23 +0530

    Update Rain Water Trapping.cpp

--- Stack/Rain Water Trapping.cpp
+++ Stack/Rain Water Trapping.cpp
@@ -1,30 +1,37 @@
+/* Time :- O(N) Space :- O(N) */
+
 class Solution
 {
-    public:
-    int trappingWater(int arr[], int n)
+public:
+ int trap(vector<int>& height)
+ {
+    int len=height.size();
+    int mxl[len];
+    int mxr[len];
+    mxl[0]=height[0];
+    
+    for(int i=1;i<len;i++)
     {
-        int mxl[n];
-        int mxr[n];
-        mxl[0]=arr[0];
-        for(int i=1;i<n;i++)
-        {
-            mxl[i]=max(mxl[i-1],arr[i]);
-        }
-        mxr[n-1]=arr[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            mxr[i]=max(mxr[i+1],arr[i]);
-        }
-        int water[n];
-        for(int i=0;i<n;i++)
-        {
-            water[i]=min(mxl[i],mxr[i])-arr[i];
-        }
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-           sum=sum+water[i];
-        }
-        return sum;
+        mxl[i]=max(mxl[i-1],height[i]);
     }
+    
+    mxr[len-1]=height[len-1];
+    for(int i=len-2;i>=0;i--)
+    {
+        mxr[i]=max(mxr[i+1],height[i]);
+    }
+    
+    int water[len];
+    for(int i=0;i<len;i++)
+    {
+        water[i]=min(mxl[i],mxr[i])-height[i];
+    }
+    
+    int sum=0;
+    for(int i=0;i<len;i++)
+    {
+        sum=sum+water[i];
+    }
+    return sum;
+  }
 };
