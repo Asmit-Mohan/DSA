83981ab90ae2c621aaa1b233542b9628c5e25d47
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-16 12:42:45 +0530

    Update Print Bracket Number.cpp

--- Stack/Print Bracket Number.cpp
+++ Stack/Print Bracket Number.cpp
@@ -1,24 +1,27 @@
+/* Time :- O(N) Space :- O(N) */
+
 class Solution
 {
 public:	
-	vector<int> barcketNumbers(string S)
-	{
-       int count=0;
-       stack<int>s;
-       vector<int>v;
-       for(int i=0;i<S.length();i++)
-       {
-           if(S[i]=='(')
-           {
-               v.push_back(++count);
-               s.push(count);
-           }
-           else
-           {
-               v.push_back(s.top());
-               s.pop();
-           }
-       }
-       return v;
-	}
+   vector<int> barcketNumbers(string S)
+   {
+      vector<int>v;
+      int count=0;
+      stack<int>s;
+        
+      for(int i=0;i<S.length();i++)
+      {
+         if(S[i]=='(')
+         {
+             v.push_back(++count);
+             s.push(count);
+         }
+         else if(S[i]==')')
+         {
+             v.push_back(s.top());
+             s.pop();
+         }
+      }
+      return v;
+    }
 };
