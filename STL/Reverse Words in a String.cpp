5b391976d22414e70a147ff4af2eeaaa51e4b89a
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-16 11:15:08 +0530

    Update Reverse Words in a String.cpp

--- STL/Reverse Words in a String.cpp
+++ STL/Reverse Words in a String.cpp
@@ -1,3 +1,39 @@
+/* Approach 1 Time :- O(N) Space :- O(N) */
+
+class Solution
+{
+public:
+    string reverseWords(string s)
+    {
+        stack<string> st;     
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            string temp;
+            if(s[i] == ' ')
+            {
+                continue;
+            }
+            while(i < s.length() && s[i] != ' ')
+            {
+                temp += s[i];
+                i++;
+            }
+            st.push(temp);
+        }
+        
+        string res;
+        while(!st.empty())
+        {
+            res += st.top() + " ";
+            st.pop();
+        }
+        res.pop_back();
+        return res;
+    }
+};
+
+/* Approach 2 Time :- More than O(N) Space :- O(N) */
+
 class Solution
 {
 public:
