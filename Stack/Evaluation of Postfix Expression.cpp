356811afe9e65a8d8be0258bcbeb841a2720cb43
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-16 12:39:22 +0530

    Update Evaluation of Postfix Expression.cpp

--- Stack/Evaluation of Postfix Expression.cpp
+++ Stack/Evaluation of Postfix Expression.cpp
@@ -1,40 +1,47 @@
+/* Time :- O(N) Space :- O(N) */
+
 class Solution
 {
-public:
-int evaluatePostfix(string S)
-{
-stack<int>s;
-char c;
-for(int i=0;i<S.size();i++)
-{
-if(isdigit(S[i]))   
-{
-s.push(S[i]-'0');  //Char to int conversion
-}
-else
-{
-c=S[i];
-int va1=s.top();
-s.pop();
-int va2=s.top();
-s.pop();
-switch(c)
-{
-case '+':
-s.push(va2+va1);
-break;
-case '-':
-s.push(va2-va1);
-break;
-case '*':
-s.push(va2*va1);
-break;
-case '/':
-s.push(va2/va1);
-break;
-}
-}
-}
-return s.top();
-}
+    public:
+    int evaluatePostfix(string S)
+    {
+        stack<int>s;
+        char op;
+        
+        for(int i=0;i<S.length();i++)
+        {
+            if(isdigit(S[i]))
+            {
+                s.push(S[i]-'0');
+            }
+            else
+            {
+                op=S[i];
+                int val1=s.top();
+                s.pop();
+                int val2=s.top();
+                s.pop();
+                
+                switch(op)
+                {
+                    case '+':
+                    s.push(val1+val2);
+                    break;
+                    
+                    case '-':
+                    s.push(-val1+val2);
+                    break;
+                    
+                    case '*':
+                    s.push(val1*val2);
+                    break;
+                    
+                    case '/':
+                    s.push(val2/val1);
+                    break;
+                }
+            }
+        }
+        return s.top();
+    }
 };
