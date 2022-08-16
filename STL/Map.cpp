5a37944e4b9706ec9d46ef88b1e8683499f2e127
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-16 10:42:49 +0530

    Update Map.cpp

--- STL/Map.cpp
+++ STL/Map.cpp
@@ -1,3 +1,5 @@
+/* Time :- O(nlogn) Space :- O(1) */
+
 map<int,int> mapInsert(int arr[],int n)
 {
     map<int,int>mp;
@@ -7,6 +9,9 @@ map<int,int> mapInsert(int arr[],int n)
     }
     return mp;
 }
+
+/* Time :- O(n) Space :- O(1) */
+
 void mapDisplay(map<int,int>mp)
 {
     for (auto itr = mp.begin(); itr != mp.end(); ++itr)
@@ -14,6 +19,9 @@ void mapDisplay(map<int,int>mp)
         cout << itr->first<<" "<< itr->second << '\n';
     }
 }
+
+/* Time :- O(logn) Space :- O(1) */
+
 void mapErase(map<int,int>&mp,int x)
 {
     if(mp.erase(x))
