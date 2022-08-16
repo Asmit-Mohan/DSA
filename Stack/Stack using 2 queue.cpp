ad2f898cbf5bec308647722132bc12e0133c8a17
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-16 14:11:42 +0530

    Update Stack using 2 queue.cpp

--- Stack/Stack using 2 queue.cpp
+++ Stack/Stack using 2 queue.cpp
@@ -1,54 +1,5 @@
-#include<bits/stdc++.h>
-using namespace std;
+/* Time :- O(1) for push() and O(N) for pop() && Space :- O(1) for both method */
 
-class QueueStack
-{
-private:
-    queue<int> q1;
-    queue<int> q2;
-public:
-    void push(int);
-    int pop();
-};
-
-
-int main()
-{
-    int T;
-    cin>>T;
-    while(T--)
-    {
-        QueueStack *qs = new QueueStack();
-
-        int Q;
-        cin>>Q;
-        while(Q--){
-        int QueryType=0;
-        cin>>QueryType;
-        if(QueryType==1)
-        {
-            int a;
-            cin>>a;
-            qs->push(a);
-        }else if(QueryType==2){
-            cout<<qs->pop()<<" ";
-
-        }
-        }
-        cout<<endl;
-    }
-}
-/* The structure of the class is
-class QueueStack
-{
-private:
-    queue<int> q1;
-    queue<int> q2;
-public:
-    void push(int);
-    int pop();
-};
-*/
 void QueueStack :: push(int x)
 {
     q1.push(x);
@@ -63,24 +14,26 @@ int QueueStack :: pop()
         int temp;
         int count=0;
         if(q2.empty())
-        return -1;
+        {
+            return -1;
+        }
         else
         {
-        while(!q2.empty())
-        {
-            q1.push(q2.front());
-            q2.pop();
-            count++;
-        }
-        count--;
-        while(count--)
-        {
-            
-             q2.push(q1.front());
-             q1.pop();
-        }
-        temp=q1.front();
-        q1.pop();
+            while(!q2.empty())
+            {
+                q1.push(q2.front());
+                q2.pop();
+                count++;
+            }
+            count--;
+            while(count--)
+            {
+
+                 q2.push(q1.front());
+                 q1.pop();
+            }
+            temp=q1.front();
+            q1.pop();
         }
         return temp;     
 }
