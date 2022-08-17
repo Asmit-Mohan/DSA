0fd8c8dbb893504412d614823a65c8aeb628d843
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-17 15:42:18 +0530

    Update Reverse a queue.cpp

--- Queue/Reverse a queue.cpp
+++ Queue/Reverse a queue.cpp
@@ -1,11 +1,30 @@
+/* Time :- O(N) Space :- O(N) [ Auxiliary Stack Space ] */
+
 void reverse(queue < int > & q)
 {
-  if(q.size()==1)
-	{
-		return;
-	}
-	int temp=q.front();
-	q.pop();
-	reverse(q);
-	q.push(temp);
+   if(q.size()==1)
+   {
+      return;
+   }
+   int temp=q.front();
+   q.pop();	
+   reverse(q);
+   q.push(temp);
+}
+
+/* Time :- O(N) Space :- O(N) Iterative Method */
+
+void reverse(queue < int > & q)
+{
+    int n = q.size();
+    vector < int > nums(n);
+    for (int i = n - 1; i >= 0; --i)
+    {
+	nums[i] = q.front();
+        q.pop();
+    }
+    for (int i = 0; i < n; i++)
+    {
+       q.push(nums[i]);
+    }
 }
