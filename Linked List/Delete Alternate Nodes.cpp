d5c80f23a27289d709f0f9ebe59afe9925f2a0b7
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-17 16:08:29 +0530

    Update Delete Alternate Nodes.cpp

--- Linked List/Delete Alternate Nodes.cpp
+++ Linked List/Delete Alternate Nodes.cpp
@@ -1,3 +1,7 @@
+/* Time :- O(N) Space :- O(1) */
+
+/* Approach 1 */
+
 void deleteAlt(struct Node *head)
 {
    if(head==NULL)
@@ -18,4 +22,28 @@ void deleteAlt(struct Node *head)
            deleteAlt(head->next);
        }
    }
+}
+
+/* Approach 2 */
+
+void deleteAlt(struct Node *head)
+{
+   if(head->next==NULL)
+   {
+       return;
+   }
+   
+   if(head->next->next==NULL)
+   {
+       head->next=NULL;
+   }
+   
+   struct Node* temp = head;
+ 
+   while(temp->next!=NULL&&temp->next->next!=NULL)
+   {
+        temp->next=temp->next->next;
+        temp=temp->next;
+   }
+   temp->next=NULL;
 }
