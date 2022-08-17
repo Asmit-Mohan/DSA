a795e7001148dc25cfae645bbe94a48caebceb8d
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-17 19:27:51 +0530

    Update Sorted-insert-for-circular-linked-list.cpp

--- Linked List/Sorted-insert-for-circular-linked-list.cpp
+++ Linked List/Sorted-insert-for-circular-linked-list.cpp
@@ -1,29 +1,32 @@
+/* Time :- O(N) Space :- O(1) */
+
 class Solution
 {
-public:
-Node *sortedInsert(Node* head, int data)
-{
-      Node *ins=new Node(data);
-      Node *curr=head;
-      if(data<head->data)
-      {
-          while(curr->next!=head)
-          {
-              curr=curr->next;
-          }
-          ins->next=head;
-          curr->next=ins;
-          return ins;
-      }
-      else
-      {
-          while(curr->next->data<data)
-          {
-              curr=curr->next;
-          }
-          ins->next=curr->next;
-          curr->next=ins;
-          return head;
-      }
-}
+    public:
+    Node *sortedInsert(Node* head, int data)
+    {
+        Node* temp = new Node(data);
+        Node* curr=head;
+        
+        if(data<head->data)
+        {
+            while(curr->next!=head)
+            {
+                curr=curr->next;
+            }
+            curr->next=temp;
+            temp->next=head;
+            return temp;
+        }
+        else
+        {
+            while(curr->next->data<data)
+            {
+                curr=curr->next;
+            }
+            temp->next=curr->next;
+            curr->next=temp;
+            return head;
+        }
+    }
 };
