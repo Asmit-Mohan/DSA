52d6bae44bfee9f47d196ba72004e3884334868f
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-17 17:03:47 +0530

    Update Check Linked List is Palindrome or not.cpp

--- Linked List/Check Linked List is Palindrome or not.cpp
+++ Linked List/Check Linked List is Palindrome or not.cpp
@@ -1,29 +1,34 @@
+/* Time :- O(N) Space :- O(1) */
+
 class Solution
 {
 public:
-    ListNode* reverse(ListNode* head)
+    ListNode* reverse(ListNode* head)        
     {
-        if(head==NULL||head->next==NULL)
-        {
-            return head;
-        }
+         if(head==NULL||head->next==NULL)
+         {
+             return head;
+         }
          ListNode* ans=reverse(head->next);
          ListNode* nextNode=head->next;
          nextNode->next=head;
          head->next=NULL;
          return ans;
     }
+    
     bool isPalindrome(ListNode* head)
     {
         ListNode* slow=head;
         ListNode* fast=head;
         
-        while(fast!=NULL&&fast->next!=NULL)
+        while(fast!=NULL&&fast->next!=NULL)   /* O(N) */
         {
             slow=slow->next;
             fast=fast->next->next;
         }
-        slow=reverse(slow);
+        
+        slow=reverse(slow);                /* O(N) */
+        
         while(slow!=NULL)
         {
             if(head->val!=slow->val)
