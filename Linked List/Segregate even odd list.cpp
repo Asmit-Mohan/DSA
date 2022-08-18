8710ba3632bdf5fc6f98e98cca275dd881b1b2e3
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-18 16:25:35 +0530

    Update Segregate even odd list.cpp

--- Linked List/Segregate even odd list.cpp
+++ Linked List/Segregate even odd list.cpp
@@ -1,4 +1,5 @@
-/* Time :- O(N) and Space O(N) */
+/* Approach 1 Time :- O(N) and Space O(N) */
+
 class Solution
 {
 public:
@@ -30,5 +31,32 @@ public:
              ptr=ptr->next;
          }
          return ans->next;
+    }
+};
+
+/* Approach 2 Time :- O(N) Space :- O(1) */
+
+class Solution 
+{
+public:
+    ListNode* oddEvenList(ListNode* head)
+    {
+        if(head==NULL || head->next==NULL)
+        {
+          return head;
+        }
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *even_head = head->next;
+        
+        while(even && even->next)
+        {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = even_head;
+        return head;
     }
 };
