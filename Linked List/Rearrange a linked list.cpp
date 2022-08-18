5ac203264b8bfd062dce650467871ac9edce8494
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-18 14:18:01 +0530

    Update Rearrange a linked list.cpp

--- Linked List/Rearrange a linked list.cpp
+++ Linked List/Rearrange a linked list.cpp
@@ -1,17 +1,17 @@
-/* Approach:-1 */
+/* Time :- O(N) Space :- O(1) */
 
-class Solution
+class Solution 
 {
-    public:
-    void rearrangeEvenOdd(Node *head)
+public:
+    ListNode* oddEvenList(ListNode* head)
     {
-      if(!head)
-      {
-          return;
-      }
-      Node *odd = head;
-      Node *even = head->next;
-      Node *even_head = head->next;
+        if(head==NULL || head->next==NULL)
+        {
+          return head;
+        }
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *even_head = head->next;
         
         while(even && even->next)
         {
@@ -20,38 +20,7 @@ class Solution
             odd = odd->next;
             even = even->next;
         }
-        odd->next = even_head;        
-    }
-};
-
-/*Approach:-2 Logic (Divide into two linked list of odd and even then put odd->next=even ie. merging) */
-class Solution
-{
-public:
-    ListNode* oddEvenList(ListNode* head)
-    {
-        if(!head)
-        {
-        	return head;
-        }
-        ListNode *end = head;
-        int counter = 0;
-        while(end->next)
-        {    
-        	end = end->next;
-            counter++;
-        }
-        
-        counter = counter&1?(counter/2)+1:counter/2;
-        ListNode *temp = head;
-        while(counter--)
-        {
-            end->next = temp->next;
-            temp->next = temp->next->next;
-            end->next->next = NULL;
-            temp = temp->next;
-            end = end->next;
-        }
+        odd->next = even_head;
         return head;
     }
 };
