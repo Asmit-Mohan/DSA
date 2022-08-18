d7475c6dfdb26c958ce5cfa70c500e4bf1ad410c
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-18 22:56:00 +0530

    Update Reverse Linked List 2.cpp

--- Linked List/Reverse Linked List 2.cpp
+++ Linked List/Reverse Linked List 2.cpp
@@ -1,35 +1,39 @@
+/* Time :- O(N) Space :- O(1) */
+
 class Solution
 {
 public:
-    ListNode* reverseBetween(ListNode* head, int left, int right)
+    ListNode* reverseKGroup(ListNode* head, int k)
     {
-        ListNode* dummy = new ListNode(0);
-        dummy->next=head;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
         
-        ListNode* leftPrev=dummy;
-        ListNode* curr=head;
-        
-        int temp = left-1;
-        while(temp--)   /* Moving current at the starting point of reverse */
+        while(k--)
         {
-           leftPrev=curr;
-           curr=curr->next;
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
         }
-        
-        ListNode* prev=NULL;
-        int size=right-left+1;
-        
-        while(size--)  /* Performing operation req times by reversing the links */
-        {
-            ListNode* flag = curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=flag;
-        }
-        
-        leftPrev->next->next=curr; 
-        leftPrev->next=prev;
-        
-        return dummy->next;
+        head->next = curr;
+        return prev;
+    }
+    
+    ListNode* reverseBetween(ListNode* head, int left, int right)
+    {   
+	   if(left == 1)
+       {
+           return reverseKGroup(head, right-left+1);
+       }
+	   
+       int count = 2; 
+       ListNode* curr = head;
+       while(left>count)
+       {
+            count++;
+            curr = curr->next;
+       }
+       curr->next = reverseKGroup(curr->next, right-left+1);
+       return head;
     }
 };
