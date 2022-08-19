d87f1f2bb8350aead9c154825926f662f056d5d9
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-19 10:04:38 +0530

    Update Permutation with spaces.cpp

--- Recursion/Permutation with spaces.cpp
+++ Recursion/Permutation with spaces.cpp
@@ -1,26 +1,37 @@
- void result(string S,string op,vector<string>&ans)
+/* Time :- O(2^n) Space :- O(1) */
+
+class Solution
+{
+public:
+    void solve(string s,string op,vector<string>&ans)
     {
-        if(S.length()==0)
+        if(s.length()==0)
         {
-            ans.push_back(op);
-            return;
+           ans.push_back(op);
+           return;
         }
+        
         string op1=op;
         string op2=op;
-        op1.push_back(' ');
-        op1.push_back(S[0]);
-        op2.push_back(S[0]);
-        S.erase(S.begin() + 0);
-        result(S,op1,ans);
-        result(S,op2,ans);
-        return;
+        
+        op1.push_back(s[0]);
+        op2.push_back(' ');
+        op2.push_back(s[0]);
+        
+        s.erase(s.begin()+0);
+        
+        solve(s,op1,ans);
+        solve(s,op2,ans);
     }
+    
     vector<string> permutation(string S)
     {
-       vector<string>ans;
-       string op="";
-       op.push_back(S[0]);
-       S.erase(S.begin() + 0);
-       result(S,op,ans);
-       return ans;
+        vector<string>ans;
+        string op="";
+        op.push_back(S[0]);
+        S.erase(S.begin()+0);
+        solve(S,op,ans);
+        sort(ans.begin(),ans.end());
+        return ans;
     }
+};
