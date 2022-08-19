db2c3014e553b6c57cdfd2a0ba690cc8736bc7af
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-19 18:49:28 +0530

    Update Permutation 2.cpp

--- Backtracking/Permutation 2.cpp
+++ Backtracking/Permutation 2.cpp
@@ -1,37 +1,40 @@
+/* Time :- O(N!*N) Space :- O(N) [Temp Vector] + O(N) [Visited array] + O(N) [ASS] */
+
 class Solution
 {
 public:
-void solve(vector<vector<int>> &ans, vector<int> &nums, vector<int> &temp,vector<bool>&flag)
-{
-        if(temp.size() == nums.size())
+        void solve(vector<vector<int>> &ans, vector<int> &nums, vector<int> &temp,vector<bool>&flag)
         {
-            ans.push_back(temp);
-            return;
+                if(temp.size() == nums.size())
+                {
+                    ans.push_back(temp);
+                    return;
+                }
+                for(int i = 0; i < nums.size(); i++)
+                {
+                    /*That means if it equal to previous one and previous one is used then current one is duplicate of it so skip it*/
+                    
+                    if(i>0&&nums[i]==nums[i-1]&&flag[i-1]==true) 
+                    {
+                        continue;
+                    }
+                    if(flag[i] == false)
+                    {
+                        flag[i] = true;
+                        temp.push_back(nums[i]);
+                        solve(ans, nums, temp, flag);
+                        flag[i] = false;
+                        temp.pop_back();
+                    }
+                }
         }
-        for(int i = 0; i < nums.size(); i++)
+        vector<vector<int>> permuteUnique(vector<int>& nums)
         {
-            /*That means if it equal to previous one and previous one is used then current one is duplicate of it so skip it*/
-            if(i>0&&nums[i]==nums[i-1]&&flag[i-1]==true) 
-            {
-                continue;
-            }
-            if(flag[i] == false)
-            {
-                flag[i] = true;
-                temp.push_back(nums[i]);
+                vector<vector<int>> ans;
+                vector<int> temp;
+                vector<bool> flag(nums.size(), false);
+                sort(nums.begin(),nums.end());
                 solve(ans, nums, temp, flag);
-                flag[i] = false;
-                temp.pop_back();
-            }
+                return ans; 
         }
-}
-vector<vector<int>> permuteUnique(vector<int>& nums)
-{
-        vector<vector<int>> ans;
-        vector<int> temp;
-        vector<bool> flag(nums.size(), false);
-        sort(nums.begin(),nums.end());
-        solve(ans, nums, temp, flag);
-        return ans; 
-}
 };
