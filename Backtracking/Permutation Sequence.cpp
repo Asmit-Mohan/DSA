ed7b2e7cc9f178791cda16da68170ee6733f9e85
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-19 19:14:34 +0530

    Update Permutation Sequence.cpp

--- Backtracking/Permutation Sequence.cpp
+++ Backtracking/Permutation Sequence.cpp
@@ -1,8 +1,10 @@
+/* Brute Force Approach Time :- O(N*N!) Space :- (N) */
+
 class Solution
 {
 public:
     int count=0;
-    void backtrack(string &in,string &op,int k,string &temp,vector<bool>& visited)
+    void solve(string &in,string &op,int k,string &temp,vector<bool>& visited)
     {
         if(temp.size()==in.length())
         {
@@ -20,7 +22,7 @@ public:
             {
                 visited[i]=true;
                 temp.push_back(in[i]);  
-                backtrack(in,op,k,temp,visited);
+                solve(in,op,k,temp,visited);
                 temp.pop_back();  
                 visited[i]=false;
             }
@@ -36,7 +38,41 @@ public:
             s.push_back(i+'0');
         }
         vector<bool> visited(n,false);
-        backtrack(s,ans,k,temp,visited);
+        solve(s,ans,k,temp,visited);
         return ans;
+    }
+};
+
+/* Optimal Solution Mathematics Factorial Based Approach Time :- O(N*N) Space :- O(N) */
+
+class Solution
+{
+public:
+    string getPermutation(int n, int k)
+    {
+        int fact = 1; 
+        vector<int> numbers; 
+        for(int i = 1;i<n;i++)
+        {
+            fact = fact * i; 
+            numbers.push_back(i); 
+        }
+        
+        numbers.push_back(n); 
+        string ans = ""; 
+        k = k - 1; 
+        
+        while(true)
+        {
+            ans = ans + to_string(numbers[k / fact]); 
+            numbers.erase(numbers.begin() + k / fact); 
+            if(numbers.size() == 0)
+            {
+                break; 
+            }
+            k = k % fact; 
+            fact = fact / numbers.size();
+        }
+        return ans; 
     }
 };
