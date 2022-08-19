2d0f95e43b87e3d059499acdda01ac14228f1a2c
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-19 16:44:28 +0530

    Update Josephus Problem.cpp

--- Recursion/Josephus Problem.cpp
+++ Recursion/Josephus Problem.cpp
@@ -1,3 +1,57 @@
+/* Approach 1 :-  Time :- O(N)  Space :- O(N) */
+
+class Solution
+{
+public:
+    int findTheWinner(int n, int k)
+    {
+        if(n==1)
+        {
+            return 1;
+        }
+        else
+        {
+            return (findTheWinner(n-1,k)+k-1)%n+1;
+        }
+    }
+};
+
+/* Approach 2 :- Time :- O(N*K) Space :- O(N) */
+
+class Solution
+{
+public:
+    int findTheWinner(int n, int k)
+    {
+        queue<int>q;
+        for(int i=1;i<=n;i++)
+        {
+            q.push(i);
+        }
+        
+        int i=0;
+        while(q.size()!=1)
+        {
+            i++;
+            int temp=q.front();
+            if(i==k)
+            {
+                q.pop();
+                i=0;
+            }
+            else
+            {
+                q.pop();
+                q.push(temp);
+            }
+        
+        }
+        return q.front();
+    }
+};
+
+/* Approach 3 :-  Time :- O(N*N)  Space :- O(N) */
+
 class Solution
 {
 public:
