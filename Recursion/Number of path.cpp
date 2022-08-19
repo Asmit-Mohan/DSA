39834ed5b8e0adf7757c36c2a6be83ec44a3ff73
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-19 09:30:38 +0530

    Update Number of path.cpp

--- Recursion/Number of path.cpp
+++ Recursion/Number of path.cpp
@@ -1,3 +1,5 @@
+/* Approach 1 --> Recursion Time :- O(2^n) Space :- O(1) */
+
 Long Long  numberOfPaths(int m, int n)
 {
     if(m==1||n==1)
@@ -5,4 +7,24 @@ Long Long  numberOfPaths(int m, int n)
         return 1;
     }
     return numberOfPaths(m-1,n)+numberOfPaths(m,n-1);
+}
+
+/* Efficient Approach 2 --> DP Time :- O(m*n) Space :- O(m*n) */
+
+int dp[11][11]={0};
+
+long long  numberOfPaths(int m, int n)
+{
+    if(m==1||n==1)
+    {
+        return dp[m][n] = 1;
+    }
+    else if(dp[m][n]!=0)
+    {
+        return dp[m][n];
+    }
+    else
+    {
+        return dp[m][n] = numberOfPaths(m,n-1) + numberOfPaths(m-1,n);
+    }
 }
