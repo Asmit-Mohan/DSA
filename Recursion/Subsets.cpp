9be9c11624b1e518ef0834d78d4f72325f71b015
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-19 12:04:44 +0530

    Update Subsets.cpp

--- Recursion/Subsets.cpp
+++ Recursion/Subsets.cpp
@@ -1,16 +1,20 @@
+/* Time :- O(2^N) Space :- O(N) */
+
+/* Solve function time complexity is O(1) and solve function is called O(2^n) times so overall time is --> O(2^n) */
+
 class Solution
 {
 public:
 void subsets1(vector<int>&nums,vector<vector<int>> &v , int i , vector<int>ans )
     {
-      if(i==nums.size())
-      {
-        v.push_back(ans);
-        return;
-      }
-      subsets1(nums , v , i+1 , ans);
-      ans.push_back(nums[i]);
-      subsets1(nums , v , i+1 , ans);
+          if(i==nums.size())
+          {
+            v.push_back(ans);
+            return;
+          }
+          subsets1(nums , v , i+1 , ans);
+          ans.push_back(nums[i]);
+          subsets1(nums , v , i+1 , ans);
       }
       vector<vector<int>> subsets(vector<int>& nums)
       {
