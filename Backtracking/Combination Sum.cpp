d25e2b2b9441949c1456bc9cb657280ec234a922
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-20 23:34:20 +0530

    Update Combination Sum.cpp

--- Backtracking/Combination Sum.cpp
+++ Backtracking/Combination Sum.cpp
@@ -1,3 +1,37 @@
+/* Approach 1 Time :- O[k*(2^T)] Space :- O(k*x) where T is target and k is average length of array whose sum is target */
+
+class Solution
+{
+    public: 
+    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int>&temp)
+    {
+        if(ind == arr.size())
+        {
+            if(target == 0)
+            {
+                ans.push_back(temp); 
+            }
+            return; 
+        } 
+        if(arr[ind] <= target)
+        {
+            temp.push_back(arr[ind]); 
+            solve(ind, target - arr[ind], arr, ans, temp); 
+            temp.pop_back(); 
+        }
+        solve(ind+1, target, arr, ans, temp); 
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target)
+    {
+        vector<vector<int>> ans; 
+        vector<int> temp; 
+        solve(0, target, candidates, ans, temp); 
+        return ans; 
+    }
+};
+
+/* Approach 2 */
+
 class Solution
 {
 public:
