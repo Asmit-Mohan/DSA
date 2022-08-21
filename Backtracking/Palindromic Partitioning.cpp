ad12778606e932ee6e4132bc27111cc7ea08a824
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-21 18:37:18 +0530

    Update Palindromic Partitioning.cpp

--- Backtracking/Palindromic Partitioning.cpp
+++ Backtracking/Palindromic Partitioning.cpp
@@ -1,13 +1,22 @@
+/* Time Complexity :- O( (2^n) *k*(n/2) )
+
+   Reason: O(2^n) to generate every substring and O(n/2)  to check if the substring generated is a palindrome.
+   O(k) is for inserting the palindromes in another data structure, where k  is the average length of the palindrome list.
+
+   Space Complexity: O(k * x) --> 
+   
+   Reason: The space complexity can vary depending upon the length of the answer. k is the average length of the list of palindromes and if we have x such list of palindromes in our final answer.
+   The depth of the recursion tree is n, so the auxiliary space required is equal to the O(n) */
+    
+
 class Solution
 {
 public:
-    bool isPalin(string s)
+    bool isPalindrome(string str, int l, int r)
     {
-        int l=0;
-        int r=s.length()-1;
-        while(l<r)
-        {
-            if(s[l]!=s[r])
+       while(l<=r)
+       {
+            if(str[l] != str[r])
             {
                 return false;
             }
@@ -16,30 +25,29 @@ public:
         }
         return true;
     }
-    void solve(string str,vector<string>temp,vector<vector<string>>&ans)
+    
+    void solve(int index, string s, vector<string> &temp, vector<vector<string>> &ans)
     {
-        if(str.length()==0)
+        if(index == s.size())
         {
             ans.push_back(temp);
             return;
         }
-        for(int i=0;i<str.length();i++)
+        for(int i = index; i < s.size(); ++i)
         {
-            string prefix=str.substr(0,i+1);
-            string rest=str.substr(i+1);
-            if(isPalin(prefix))
+            if(isPalindrome(s, index, i))
             {
-                temp.push_back(prefix);
-                solve(rest,temp,ans); 
+                temp.push_back(s.substr(index, i - index + 1));
+                solve(i+1, s, temp, ans);
                 temp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s)
     {
-        vector<vector<string>>ans;
-        vector<string>temp;
-        solve(s,temp,ans);
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(0, s, temp, ans);
         return ans;
     }
 };
