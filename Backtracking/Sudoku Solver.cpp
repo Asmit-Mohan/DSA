e97b6fd5b70013df351dd41e82227220a39c0e97
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-21 19:01:06 +0530

    Update Sudoku Solver.cpp

--- Backtracking/Sudoku Solver.cpp
+++ Backtracking/Sudoku Solver.cpp
@@ -1,3 +1,8 @@
+/*  Time Complexity  :- O[9^(n ^ 2)], in the worst case, for each cell in the n^2 board, we have 9 possible numbers.
+
+    Space Complexity :- O(1), since we are refilling the given board itself, there is no extra space required, so constant space complexity.
+*/
+
 class Solution
 {
 public:
