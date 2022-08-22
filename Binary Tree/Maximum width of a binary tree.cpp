c6c046a1f6ac31ea0a81b874e0318bc2de302549
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-22 21:47:06 +0530

    Update Maximum width of a binary tree.cpp

--- Binary Tree/Maximum width of a binary tree.cpp
+++ Binary Tree/Maximum width of a binary tree.cpp
@@ -1,3 +1,5 @@
+/* Time :- O(N) Space :- O(N) */
+
 class Solution
 {
 public:
@@ -9,14 +11,16 @@ public:
         
        while(!q.empty())
        {
-            int n=q.size();
-            unsigned int l=q.front().second;
-            unsigned int r;
+           int n=q.size();
+           unsigned int l=q.front().second;
+           unsigned int r;
+           
            while(n--)
            {
                TreeNode* temp=q.front().first;
                r=q.front().second;
                q.pop();
+               
                if(temp->left)
                {
                    q.push({temp->left,(2*r)+1});
@@ -26,7 +30,7 @@ public:
                    q.push({temp->right,(2*r)+2});
                }
            }
-            ans=max(ans,r-l+1);
+           ans=max(ans,r-l+1);
        }
        return ans;
     }
