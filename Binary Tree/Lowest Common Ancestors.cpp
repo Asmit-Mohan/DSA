52c2c212a3a5740b6174ba2d31102ff4facd7a76
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-22 21:09:58 +0530

    Update Lowest Common Ancestors.cpp

--- Binary Tree/Lowest Common Ancestors.cpp
+++ Binary Tree/Lowest Common Ancestors.cpp
@@ -1,34 +1,36 @@
+/* Time :- O(N) Space :- O(N) */
+
 class Solution
 {
 public:
-TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
-{
-    if(root==NULL)
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
     {
-        return NULL;
-    }
-    if(root->val == p->val || root->val == q->val)
-    {
-        return root;
-    }
-    TreeNode* l =  lowestCommonAncestor(root->left,p,q);     //inorder
-    TreeNode* r =  lowestCommonAncestor(root->right,p,q);   // inorder
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        if(root->val == p->val || root->val == q->val)
+        {
+            return root;
+        }
+        TreeNode* l =  lowestCommonAncestor(root->left,p,q);     //inorder
+        TreeNode* r =  lowestCommonAncestor(root->right,p,q);   // inorder
 
-    if(l==NULL && r!=NULL )
-    {
-        return r;
+        if(l==NULL && r!=NULL )
+        {
+            return r;
+        }
+        else if(r==NULL && l!=NULL)
+        {
+            return l;
+        }
+        else if(l==NULL && r==NULL )
+        {
+            return NULL;
+        }
+        else
+        {
+            return root;
+        }
     }
-    else if(r==NULL && l!=NULL)
-    {
-        return l;
-    }
-    else if(l==NULL && r==NULL )
-    {
-        return NULL;
-    }
-    else
-    {
-        return root;
-    }
-}
 };
