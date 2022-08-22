1e37c1c3d6fd63b0fd0789a5dc499cb6c411d877
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-22 13:55:38 +0530

    Update Zig Zag Tree Traversal.cpp

--- Binary Tree/Zig Zag Tree Traversal.cpp
+++ Binary Tree/Zig Zag Tree Traversal.cpp
@@ -1,10 +1,12 @@
+/* Approach 1 Time :- O(N) Space :- O(N) */
+
 class Solution
 {
 public:
 vector <int> zigZagTraversal(Node* root)
 {
     vector<int> v;
-    stack<Node*> s1,s2;  /* s1 will deal with even level and s2 will deal with odd level in binary tree */
+    stack<Node*> s1,s2;         /* s1 will deal with even level and s2 will deal with odd level in binary tree */
     if(!root)
     {
         return v;
@@ -45,4 +47,49 @@ vector <int> zigZagTraversal(Node* root)
       }
     return v;
    }
+};
+
+/* Approach 2 Time :- O(N) Space :- O(N) */
+
+class Solution
+{
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root)
+    {
+        vector<vector<int>> result;
+        if (root == NULL)
+        {
+            return result;
+        }
+
+        queue<TreeNode*>q;
+        q.push(root);
+        bool leftToRight = true;
+
+        while(!q.empty())
+        {
+            int size = q.size();
+            vector<int> row(size);
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                int index = (leftToRight) ? i : (size - 1 - i);
+                row[index] = node->val;
+                
+                if (node->left)
+                {
+                    q.push(node->left);
+                }
+                if (node->right)
+                {
+                    q.push(node->right);
+                }
+            }
+            
+            leftToRight = !leftToRight;
+            result.push_back(row);
+        }
+        return result;
+    }
 };
