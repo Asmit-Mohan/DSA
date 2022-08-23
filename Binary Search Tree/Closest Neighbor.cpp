c73571f5766c381f179a7af6dfd3540e5e78658c
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-23 22:48:03 +0530

    Update Closest Neighbor.cpp

--- Binary Search Tree/Closest Neighbor.cpp
+++ Binary Search Tree/Closest Neighbor.cpp
@@ -1,53 +1,52 @@
-/*Naive Approach*/
+/* Naive Approach Time :- O(N) Space :- O(N) */
 
-void solve(Node* root,vector<int>&v)
-{
-    if(root!=NULL)
-    {
-        solve(root->left,v);
-        v.push_back(root->key);
-        solve(root->right,v);
-    }
-}
-int findMaxForN(Node* root, int target)
-{
-    vector<int>v;
-    solve(root,v);
-    sort(v.begin(),v.end());
-    auto it = lower_bound(v.begin(),v.end(),target);
-        if((*it)== target)
-        {
-            return *(it);
-        }
-        else
-        {
-            return *(it - 1);
-        }
-}
-
-/*Efficient Approach*/
-void solve(Node* root,int &ans,int target)
+void solve(Node* root,int target,int &ans)
 {
     if(root==NULL)
     {
         return;
     }
-    if(target>=root->key)
+    solve(root->left,target,ans);
+    if(root->key<=target)
     {
         ans=max(ans,root->key);
     }
-    if(root->key>target)
-    {
-        solve(root->left,ans,target);
-    }
-    if(root->key<target)
-    {
-        solve(root->right,ans,target);
-    }
+    solve(root->right,target,ans);
 }
-int findMaxForN(Node* root, int target)
+
+int findMaxForN(Node* root, int N)
 {
-    int ans=INT_MIN;
-    solve(root,ans,target);
+    int ans = INT_MIN;
+    solve(root,N,ans);
     return ans;
+}
+
+/* Efficient Approach Time :- O(H) Space :- O(H) where H can be replaced with logn since height of BST if logn */
+
+int findMaxForN(Node* root, int N)
+{
+    if (root == NULL)
+    {
+        return -1;
+    }
+    if (root->key == N)
+    {
+        return N;
+    }
+    else if (root->key<N)
+    {
+        int k = findMaxforN(root->right, N);
+        if (k == -1)
+        {
+            return root->key;
+        }
+        else
+        {
+            return k;
+        }
+    }
+    else if (root->key > N)
+    {
+        return findMaxforN(root->left, N);  
+    }   
 }
