60ce8e6c391cbcf18e60ad09954c0394835ea197
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-23 12:14:53 +0530

    Update Count complete tree nodes.cpp

--- Binary Tree/Count complete tree nodes.cpp
+++ Binary Tree/Count complete tree nodes.cpp
@@ -1,3 +1,38 @@
+/* Approach 1 --> Tree Traversal */
+
+/* 
+
+Time Complexity: O(N) --> Reason: We are traversing for every node of the tree
+
+Space Complexity: O(logN) --> Reason: Space is needed for the recursion stack. As it is a complete tree, the height of that stack will always be logN.
+    
+*/
+
+void traverse(node * curr, int& count)
+{
+   if (curr == NULL)
+   {
+      return;
+   }
+   count++;
+   traverse(curr -> left, count);
+   traverse(curr -> right, count);
+}
+
+/* Approach 2 Using Height Technique */
+
+/* 
+
+Time Complexity: O(log^2 N).
+
+Reason: To find the leftHeight and right Height we need only logN time and in the worst case we will encounter the second case(leftHeight!=rightHeight) for at max logN times, so total time complexity will be O(log N * logN)
+
+Space Complexity: O(logN)
+
+Reason: Space is needed for the recursion stack when calculating height. As it is a complete tree, the height will always be logN.
+
+*/
+
 class Solution 
 {
 public:
