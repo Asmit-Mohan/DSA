79d2d31e460ed623a6df577d4e4cf97f239a09a5
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-24 18:48:54 +0530

    Update Normal BST to Balanced BST.cpp

--- Binary Search Tree/Normal BST to Balanced BST.cpp
+++ Binary Search Tree/Normal BST to Balanced BST.cpp
@@ -1,29 +1,37 @@
-void inorder(Node *root, vector<int> &arr)
-{
-    if(root != NULL)
-    {
-        inorder(root->left, arr);
-        arr.push_back(root->data);
-        inorder(root->right, arr);
-    }
-}
+/* Time :- O(N) Space :- O(N) */
 
-Node *createBST(vector<int> arr, int l, int r) //It means if we make bst from sorted array it will be balanced. 
+class Solution
 {
-    if(l > r)
+public:
+    void inorder(TreeNode* root,vector<int>&v)
     {
-        return NULL;
+        if(root==NULL)
+        {
+            return;
+        }
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
     }
-    int mid = l + (r - l)/2;
-    Node *root = new Node(arr[mid]);
-    root->left = createBST(arr, l, mid-1);
-    root->right = createBST(arr, mid+1, r); 
-    return root;
-}
-
-Node* buildBalancedTree(Node* root)
-{
-	vector<int> arr;
-	inorder(root, arr);
-	return createBST(arr, 0, arr.size()-1);
-}
+    
+    TreeNode* solve(int low,int high,vector<int>&arr)
+    {
+        if(low>high)
+        {
+            return NULL;
+        }
+        
+        int mid = low+(high-low)/2;
+        TreeNode* root = new TreeNode(arr[mid]);
+        root->left =  solve(low,mid-1,arr);
+        root->right = solve(mid+1,high,arr);   
+        return root;
+    }
+    
+    TreeNode* balanceBST(TreeNode* root)
+    {
+        vector<int>v;
+        inorder(root,v);
+        return solve(0,v.size()-1,v);
+    }
+};
