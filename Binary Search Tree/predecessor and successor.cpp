c8b2c1597f8d80c7cdbb8b463572b80deb89cf98
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-25 23:59:15 +0530

    Update predecessor and successor.cpp

--- Binary Search Tree/predecessor and successor.cpp
+++ Binary Search Tree/predecessor and successor.cpp
@@ -1,22 +1,26 @@
-void findPreSuc(Node* root, Node*& pre, Node*& suc, int target)
+class Solution
 {
-    if(root==NULL)
+Public:
+    void findPreSuc(Node* root, Node*& pre, Node*& suc, int target)
     {
-        return;
+        if(root==NULL)
+        {
+            return;
+        }
+        else if(root->key>target)
+        {
+            suc=root;
+            findPreSuc(root->left,pre,suc,target);
+        }
+        else if(root->key<target)
+        {
+            pre=root;
+            findPreSuc(root->right,pre,suc,target);
+        }
+        else
+        {
+            findPreSuc(root->left,pre,suc,target);
+            findPreSuc(root->right,pre,suc,target);
+        }
     }
-    else if(root->key>target)
-    {
-        suc=root;
-        findPreSuc(root->left,pre,suc,target);
-    }
-    else if(root->key<target)
-    {
-        pre=root;
-        findPreSuc(root->right,pre,suc,target);
-    }
-    else
-    {
-        findPreSuc(root->left,pre,suc,target);
-        findPreSuc(root->right,pre,suc,target);
-    }
-}
+};
