9868b6e1887508c0dbcf3ea398238686ac3f8393
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-26 00:15:37 +0530

    Update inorder-successor-in-bst .cpp

--- Binary Search Tree/inorder-successor-in-bst .cpp
+++ Binary Search Tree/inorder-successor-in-bst .cpp
@@ -1,5 +1,4 @@
-
-/* Approach 1 */
+/* Approach 1 Time :- O(H) Space :- O(1) */
 
 class Solution
 {
@@ -10,49 +9,49 @@ Node *prev = NULL;
 Node *cur = root;
 while(cur!=NULL)
 {
-		if(cur->data > x->data)
-		{
-			prev = cur;
-			cur = cur->left;
-		}
-		else
-		{
-			cur = cur->right;
-		}
+     if(cur->data > x->data)
+     {
+	  prev = cur;
+          cur = cur->left;
+     }
+     else
+     {
+	  cur = cur->right;
+     }
 }
 return prev;
 }
 };
 
-/* Approach 2 */
+/* Approach 2 Time :- O(H) Space :- O(1) */
 
 class Solution
 {
-public:
-int flag=1;
-
-void solve(Node* root,Node* &ans,Node* target)
-{
-    if(root!=NULL)
+  public:
+    
+    void solve(Node* root,Node* &ans,int target)
     {
-        solve(root->left,ans,target);
-        if(flag==0)
-        {
-            ans=root;
-            flag=1;
-            return;
-        }
-        if(root->data==target->data)
-        {
-            flag=0;
-        }
-        solve(root->right,ans,target);
+       if(root==NULL)
+       {
+           return;
+       }
+       
+       if(root->data>target)
+       {
+           ans=root;
+           solve(root->left,ans,target);
+       }
+       
+       if(root->data<=target)
+       {
+           solve(root->right,ans,target);
+       }
     }
-}
-
-Node * inOrderSuccessor(Node *root, Node *x)
-{
-    Node* ans=NULL;
-    solve(root,ans,x);
-    return ans;
-}
+    
+    Node * inOrderSuccessor(Node *root, Node *x)
+    {
+        Node* ans=NULL;
+        solve(root,ans,x->data);
+        return ans;
+    }
+};
