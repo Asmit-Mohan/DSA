f0d5d218b7b6bcd9442e8e0754dd1cd0216f3abb
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-26 21:24:14 +0530

    Update Ceil BST.cpp

--- Binary Search Tree/Ceil BST.cpp
+++ Binary Search Tree/Ceil BST.cpp
@@ -1,3 +1,5 @@
+/* Approach 1 Time :- O(N) Space :- O(N) */
+
 void solve(BinaryTreeNode<int> * root,int &ans,int X)
 {
     if(root!=NULL)
@@ -16,4 +18,29 @@ int findCeil(BinaryTreeNode<int> *node, int x)
     int ans=INT_MAX;
     solve(node,ans,x);
     return ans==INT_MAX?-1:ans;
+}
+
+/* Approach 2 Time :- O(H) Space :- O(1) */
+
+ int findCeil(BinaryTreeNode<int> *root, int key)
+ {
+	int ceil = -1; 
+    while (root)
+    {
+        if (root->data == key)
+        {
+            ceil = root->data;
+            return ceil;
+        }
+        if (key > root->data)
+        {
+            root = root->right;
+        }
+        else
+        {
+            ceil = root->data; 
+            root = root->left;
+        }
+    }
+    return ceil; 
 }
