ad7547e0f2f5283065dce1fc6969e2d82c1a5cc3
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-26 11:39:04 +0530

    Update Floor BST.cpp

--- Binary Search Tree/Floor BST.cpp
+++ Binary Search Tree/Floor BST.cpp
@@ -1,3 +1,5 @@
+/* Approach 1 Time :- O(N) Space :- O(1) */
+
 void solve(TreeNode<int> * root,int &ans,int X)
 {
     if(root!=NULL)
@@ -15,4 +17,30 @@ int floorInBST(TreeNode<int> * root, int X)
    int ans=INT_MIN;
    solve(root,ans,X);
    return ans;
+}
+
+/* Approach 2 Time :- O(H) Space :- O(1) */
+
+int floorInBST(TreeNode<int> * root, int key)
+{
+    int floor = -1; 
+    while (root)
+    {
+        if (root->val == key)
+        {
+            floor = root->val;
+            return floor;
+        }
+ 
+        if (key > root->val)
+        {
+            floor = root->val;
+            root = root->right;
+        }
+        else
+        {
+            root = root->left;
+        }
+    }
+    return floor;
 }
