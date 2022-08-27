028553be93e76d767972b5be0a19217ac8572b0f
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-27 11:39:33 +0530

    Update Mother Vertex.cpp

--- Graph/Mother Vertex.cpp
+++ Graph/Mother Vertex.cpp
@@ -1,41 +1,46 @@
+/* Time :- O(V+E) Space :- O(V) + O(V) */
+
 class Solution 
 {
     public:
-    void dfs(int node, vector<int> &vis, vector<int> adj[])  
-    {
-        vis[node] = 1; 
-        for(auto it : adj[node])
-        {
-            if(vis[it]==0)
-            {
-                dfs(it, vis, adj);
-            }
-        }
-    }
+	void dfs(int node, vector<int> &vis, vector<int> adj[])  
+	{
+	    vis[node] = 1; 
+	    for(auto it : adj[node])
+	    {
+		if(vis[it]==0)
+		{
+		    dfs(it, vis, adj);
+		}
+	    }
+	}
 	int findMotherVertex(int V, vector<int>adj[])
 	{
-	   vector<int> vis(V,0); 
-	   int last_dfs;
-       for(int i=0;i<V;i++)
-       {
-         if(vis[i]==0)
-         {
-             dfs(i,vis,adj); 
-             last_dfs=i;
-         }
-      }
-      for(int i=0;i<V;i++)
-      {
-          vis[i]=0;
-      }
-      dfs(last_dfs,vis,adj);
-      for(int i=0;i<V;i++)
-      {
-          if(vis[i]==0)
-          {
-              return -1;
-          }
-      }
-      return last_dfs;
+	   int ans;
+	   vector<int>vis(V,0);
+	   
+	   for(int i=0;i<V;i++)
+	   {
+	       if(vis[i]==0)
+	       {
+	           dfs(i,vis,adj);
+	           ans=i;
+	       }
+	   }
+	   
+	   for(int i=0;i<V;i++)
+	   {
+	       vis[i]=0;
+	   }
+	   
+	   dfs(ans,vis,adj);
+	   for(int i=0;i<V;i++)
+	   {
+	       if(vis[i]==0)
+	       {
+	           return -1;
+	       }
+	   }
+	   return ans;
 	}
 };
