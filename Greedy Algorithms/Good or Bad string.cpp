af2c740a4e7e109052ab5f2674b88242c0d88174
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-28 11:04:47 +0530

    Update Good or Bad string.cpp

--- Greedy Algorithms/Good or Bad string.cpp
+++ Greedy Algorithms/Good or Bad string.cpp
@@ -1,3 +1,5 @@
+/* Time :- O(N) Space :- O(1) */
+
 class Solution
 {
   public:
@@ -5,40 +7,29 @@ class Solution
     {
         int vow=0;
         int cons=0;
-        int flag=1;
+        
         for(int i=0;i<S.length();i++)
         {
             if(S[i]=='a'||S[i]=='e'||S[i]=='i'||S[i]=='o'||S[i]=='u')
             {
                 cons=0;
                 vow++;
-                if(vow>5)
-                {
-                    flag=0;
-                    break;
-                }
             }
             else if(S[i]=='?')
             {
                 cons++;
                 vow++;
-                if(vow>5||cons>3)
-                {
-                    flag=0;
-                    break;
-                }
             }
             else
             {
                 cons++;
                 vow=0;
-                if(cons>3)
-                {
-                    flag=0;
-                    break;
-                }
+            }
+            if(cons>3||vow>5)
+            {
+                return 0;
             }
         }
-        return flag;
+        return 1;
     }
 };
