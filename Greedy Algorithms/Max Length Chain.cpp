768f3b2e24d910474d03cd44d6ee039ccd3cec48
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-29 11:18:37 +0530

    Update Max Length Chain.cpp

--- Greedy Algorithms/Max Length Chain.cpp
+++ Greedy Algorithms/Max Length Chain.cpp
@@ -1,32 +1,60 @@
-int comp(pair<int,int>a, pair<int,int>b)
-{
-    return a.second<b.second;
-}
+/* Time :- O(Nlogn) Space :- O(N) */
 
-int maxChainLen(struct val p[],int n)
+class Solution
 {
-    vector<pair<int,int>>v;
-    for(int i=0;i<n;i++)
+public:
+    static bool comp(pair<int,int>a,pair<int,int>b)
     {
-        v.push_back({p[i].first,p[i].second});
+        return a.second<b.second;    
     }
-    sort(v.begin(),v.end(),comp);
-    int i=0;
-    int l=1;
-    int ans=1;
-    int r=v.size()-1;
-    while(l<=r)
+    
+    int maxChainLen(struct val p[],int n)
     {
-        if(v[l].first>v[i].second)
+        vector<pair<int,int>>v;
+        for(int i=0;i<n;i++)
         {
-            ans++;
-            i=l;
-            l++;
+            v.push_back({p[i].first,p[i].second});
         }
-        else
+    
+        int ans=1;
+        sort(v.begin(),v.end(),comp);    
+        pair<int,int>temp = v[0];
+        
+        for(int i=1;i<n;i++)
         {
-            l++;
+            if(v[i].first>temp.second)
+            {
+               ans++;
+               temp = v[i];
+            }
         }
+        return ans;
     }
-    return ans;
-}
+};
+
+/* Time :- O(Nlogn) Space :- O(1) */
+
+class Solution
+{
+public:
+    bool static comp(val fst,val sec)
+    {
+        return fst.second<sec.second;
+    }
+    
+    int maxChainLen(struct val p[],int n)
+    {
+        sort(p,p+n,comp);
+        int ans=0;
+        int prev=-1e9;
+        for(int i=0;i<n;i++)
+        {
+            if(p[i].first>prev)
+            {
+                prev=p[i].second;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
