4f1eb5d7ac486f243526ae15fcdcb34d95b532c2
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-29 10:59:40 +0530

    Update Maximum sum after k negations.cpp

--- Greedy Algorithms/Maximum sum after k negations.cpp
+++ Greedy Algorithms/Maximum sum after k negations.cpp
@@ -1,32 +1,30 @@
+/* Time :- O(Nlogn) Space :- O(1) */
+
 class Solution
 {
     public:
     long long int maximizeSum(long long int arr[], int n, int k)
     {
-    sort(arr,arr+n);
-    long long  sum=0;
-    long long count=0;
-    
-    for(int i=0;i<n;i++)
-    {
-       if(arr[i]<0&&k>0)
-       {
-           arr[i]=-arr[i];
-           k--;
-       }
-       
-    }
-    for(int i=0;i<n;i++)
-    {
-        sum=sum+arr[i];
-    }
-    
-    long long mn=*min_element(arr,arr+n);
-   
-       if(k%2==1)
-       {
-           sum-=2*mn;
-       }
-       return sum;
+        long long int sum = 0;
+        sort(arr,arr+n);
+        
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<0&&k>0)
+            {
+                k--;
+                arr[i] = arr[i]*(-1);
+            }
+            sum=sum+arr[i];
+        }
+        
+        if(k%2==1)
+        {
+            return sum - 2*(*min_element(arr,arr+n));
+        }
+        else
+        {
+            return sum;    
+        }
     }
 };
