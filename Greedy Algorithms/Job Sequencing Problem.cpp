8a77fa021ec1142009353e42507ec231a5a7d45a
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-08-30 12:31:20 +0530

    Update Job Sequencing Problem.cpp

--- Greedy Algorithms/Job Sequencing Problem.cpp
+++ Greedy Algorithms/Job Sequencing Problem.cpp
@@ -1,38 +1,55 @@
-class Solution /* Striver Code */
+/* Time :- O(Nlogn) Space :- O[Max(arr[i].Dealine)] */
+
+class Solution 
 {
     public:
-    static bool comp (Job a,Job b)
+    static bool comp(Job a, Job b)
     {
         return a.profit>b.profit;
+    }
+    
+    bool isPossible(vector<int>&visited,int dead)
+    {
+        for(int i=1;i<dead;i++)
+        {
+            if(visited[i]==0)
+            {
+                visited[i]=1;
+                return 1;
+            }
+        }
+        return 0;
     }
     
     vector<int> JobScheduling(Job arr[], int n) 
     { 
         sort(arr,arr+n,comp);
-        int maxi=arr[0].dead;
+        
+        int num=0;
+        int total=0;
+        vector<int>ans;
+        vector<int>visited(101,0);
         
         for(int i=0;i<n;i++)
         {
-            maxi=max(maxi,arr[i].dead);
-        }
-        
-        int complete[maxi+1]={0};
-        int days=0;
-        int profit=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=arr[i].dead;j>0;j--)
+            if(visited[arr[i].dead]==0)
             {
-                if(complete[j]==0)
+                num++;
+                visited[arr[i].dead]=1;
+                total = total + arr[i].profit;
+            }
+            else
+            {
+                if(isPossible(visited,arr[i].dead))
                 {
-                    complete[j]=1;
-                    profit+=arr[i].profit;
-                    days++;
-                    break;
+                    num++;
+                    total = total + arr[i].profit;
                 }
             }
         }
-        return {days,profit};
+        
+        ans.push_back(num);
+        ans.push_back(total);
+        return ans;
     } 
 };
