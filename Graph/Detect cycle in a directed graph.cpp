59d246b50832a72094407f745def624c4f393f81
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-09-01 16:06:10 +0530

    Update Detect cycle in a directed graph.cpp

--- Graph/Detect cycle in a directed graph.cpp
+++ Graph/Detect cycle in a directed graph.cpp
@@ -1,3 +1,5 @@
+/* Approach 1 Time Complexity :- O(V+E) Space Complexity :- O(V) + O(V) */
+
 class Solution
 {
   public:
@@ -26,6 +28,7 @@ class Solution
     {
         vector<int>vis(V,0);
         vector<int>dfsVis(V,0);
+	    
         for(int i=0;i<V;i++)
         {
             if(vis[i]==0)
@@ -40,7 +43,7 @@ class Solution
     }
 };
 
-/*Approach 2 (BFS) (kahn's Algorithms) Time Complexity :- O(V+E) Space Complexity :- O(V) + O(V) */
+/* Approach 2 (BFS) (kahn's Algorithms) Time Complexity :- O(V+E) Space Complexity :- O(V) + O(V) */
 
 class Solution
 {
