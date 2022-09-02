647386d52ea0b38fbbdcd71b47525ecbbb0f234e
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-09-02 22:40:19 +0530

    Update Diameter of binary tree.cpp

--- Binary Tree/Diameter of binary tree.cpp
+++ Binary Tree/Diameter of binary tree.cpp
@@ -1,4 +1,4 @@
-/* Time :- O(N) Space :- O(H) */
+/* Approach 1 [Brute Force] Time :- O(N*N) Space :- O(N*H) */
 
 class Solution
 {
@@ -29,6 +29,37 @@ class Solution
         
         int ans=max(ans1,ans2);
         
+        return ans;
+    }
+};
+
+/* Optimal Approach Time :- O(N) Space :- O(H) */
+
+class Solution
+{
+  public:
+    int height(Node* root,int &ans)
+    {
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        int l=height(root->left,ans);
+        int r=height(root->right,ans);
+        
+        ans=max(ans,1+l+r);
+   
+        return 1+max(l,r);
+    }
+    int diameter(Node* root)
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        
+        int ans=INT_MIN;
+        height(root,ans);
         return ans;
     }
 };
