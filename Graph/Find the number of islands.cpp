8ef66bd7bcd164fc139924b6d0764130c23a31e1
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-09-02 11:15:49 +0530

    Update Find the number of islands.cpp

--- Graph/Find the number of islands.cpp
+++ Graph/Find the number of islands.cpp
@@ -1,3 +1,5 @@
+/* Approach 1 Time :- O(N*M) Space :- O(N*M) */
+
 class Solution
 {
 public:
@@ -45,4 +47,61 @@ public:
         }
         return count;
     }
+};
+
+
+/* Approach 2 Time :- O(N*M) Space :- O(1) */
+
+class Solution
+{
+public:
+void dfs(vector<vector<char>>& grid, int x, int y)
+{
+    if(x < 0 || y < 0 || x >= grid.size() || y >=grid[0].size()  || grid[x][y] == '0')
+    {
+        return;
+    }
+    
+    grid[x][y]='0';
+    
+    dfs(grid,x-1,y);
+    dfs(grid,x,y+1);
+    dfs(grid,x,y-1);
+    dfs(grid,x+1,y);
+    
+    dfs(grid,x+1,y-1);
+    dfs(grid,x-1,y-1);
+    dfs(grid,x+1,y+1);
+    dfs(grid,x-1,y+1);
+}
+
+int numIslands(vector<vector<char>>& grid)
+{
+    int row = grid.size();
+    int col = grid[0].size();
+    int isLands = 0;
+    
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            if(grid[i][j] == '1')
+            {
+                grid[i][j]='0';
+                isLands++;
+                
+                dfs(grid,i-1,j);
+                dfs(grid,i,j+1);
+                dfs(grid,i,j-1);
+                dfs(grid,i+1,j);
+            
+                dfs(grid,i+1,j-1);
+                dfs(grid,i-1,j-1);
+                dfs(grid,i+1,j+1);
+                dfs(grid,i-1,j+1);
+            }
+        }
+    }
+    return isLands;     
+}
 };
