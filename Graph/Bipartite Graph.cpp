f063827564bb904b123a5b5dedd8c01008ff78e5
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-09-02 14:31:15 +0530

    Update Bipartite Graph.cpp

--- Graph/Bipartite Graph.cpp
+++ Graph/Bipartite Graph.cpp
@@ -1,46 +1,53 @@
-/* BFS */
+/* BFS Time :- O(V+E) Space :- O(V) */
 
 class Solution
 {
 public:
-	bool isBipartite(int V, vector<int>adj[])
-	{
-	    vector<int>color(V,-1);
-	    queue<int>q;
-	    
-	    for(int i=0;i<V;i++)
-	    {
-	        if(color[i]==-1)
-	        {
-	            color[i]=0;
-	            q.push(i);
-	            while(!q.empty())
-	            {
-	                int node=q.front();
-	                q.pop();
-    	                for(auto it : adj[node])
-    	                {
-				if(color[it]==-1)
-				{
-				    color[it]=1-color[node];
-				    q.push(it);
-				}
-				else
-				{
-				    if(color[it]==color[node])
-				    {
-					return false;
-				    }
-				}
-    	                   }
-	               }
-	           }
-	      }
-	 return true;
-      }
+   bool solve(int color[],int i,vector<int>adj[])
+   {
+      queue<int>q;
+      q.push(i);
+      color[i]=1;
+      
+      while(!q.empty())
+      {
+        int node=q.front();
+        q.pop();
+        for(auto x : adj[node])
+        {
+            if(color[x]==-1)
+            {
+                color[x]=1-color[node];
+                q.push(x);
+            }
+            else if(color[x]==color[node])
+            {
+                return false;
+            }
+        }
+     }
+     return true;
+   }
+	
+    bool isBipartite(int V, vector<int>adj[])
+    {
+        int color[V];
+        memset(color,-1,sizeof color);
+        for(int i=0;i<V;i++)
+        {
+            if(color[i]==-1)
+            {
+                if(!solve(color,i,adj))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 };
 
-/* DFS */
+/* DFS Time :- O(V+E) Space :- O(V) */
 
 class Solution
 {
@@ -85,8 +92,5 @@ public:
        return true;
    }
 };
-
-/* Here we are not using visited array because colors is acting as visited array as, one any vertex is colored that means it is visisted so 
-   when we are checking colored or not that means we are checking it is visited or not */
   
 /* One Key point is that if a graph having a odd length cyclic then it is not Bipartite Graph */
