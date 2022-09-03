2f77a40d696463cd156d2397c002a51dd56b493e
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-09-03 12:22:52 +0530

    Update Course Schedule II.cpp

--- Graph/Course Schedule II.cpp
+++ Graph/Course Schedule II.cpp
@@ -1,4 +1,59 @@
-/* Time :- [DFS] O(V+E) Space :- O(V+E) */
+/* Approach 1 Kahn's Algorithms */
+
+class Solution
+{
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre)
+    {    
+        vector<vector<int>>adj(n);
+        for(auto it:pre)
+        {
+            adj[it[1]].push_back(it[0]);
+        }
+        
+        queue<int>q;
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++)
+        {
+            for(auto it:adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        
+        vector<int>res;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            res.push_back(node);
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+      
+        if(res.size()==n)
+        {
+            return res;
+        }
+        return {};
+    }
+};
+
+/* Approach 2 Time :- [DFS] O(V+E) Space :- O(V+E) */
 
 class Solution
 {
