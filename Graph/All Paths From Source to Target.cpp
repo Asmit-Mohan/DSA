a3c85671c40c5fa8fb859f6661941c69cc0e59ec
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-09-03 11:20:24 +0530

    Update All Paths From Source to Target.cpp

--- Graph/All Paths From Source to Target.cpp
+++ Graph/All Paths From Source to Target.cpp
@@ -2,42 +2,26 @@ class Solution
 {
 public:
     vector<vector<int>>ans;
-    
-    void dfs(int src,int des,vector<int>&result,vector<int>adj[])
+    void solve(int src,int des,vector<int>&path,vector<vector<int>>&graph)
     {
-        result.push_back(src);
-        
+        path.push_back(src); 
         if(src==des)
         {
-            ans.push_back(result);
+            ans.push_back(path);
             return;
         }
-        
-        for(auto x : adj[src])
+        for(auto it : graph[src])
         {
-           dfs(x,des,result,adj);
-           result.pop_back();
+            solve(it,des,path,graph);
+            path.pop_back();
         }
     }
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph)
     {
         int n=graph.size();
-        vector<int>adj[n];
         vector<int>path;
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<graph[i].size();j++)
-            {
-                if(graph[i].size()>0)
-                {
-                    adj[i].push_back(graph[i][j]);
-                }
-            }
-        }
-        
-        dfs(0,n-1,path,adj);
+        solve(0,n-1,path,graph);
         return ans;
     }
 };
