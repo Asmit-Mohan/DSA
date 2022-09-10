f3c26e65ad1a063d2e0fe7c63f3440ff13e07074
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-09-10 15:15:06 +0530

    Update Construct Binary Tree From Level Order Traversal.cpp

--- Binary Tree/Construct Binary Tree From Level Order Traversal.cpp
+++ Binary Tree/Construct Binary Tree From Level Order Traversal.cpp
@@ -1,3 +1,5 @@
+/* In Form of string */
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -91,6 +93,132 @@ int main()
 {
     string levelOrder[13] = {{"1"},{"2"},{"3"},{"4"},{"#"},{"6"},{"7"},{"#"},{"8"},{"9"},{"#"},{"#"},{"10"}};
     Node* root = solve(levelOrder,13);
+    vector<int>ans;
+    
+    preorder(root,ans);
+    cout<<"Preorder Traversal of Binary Tree :- "<<endl;
+    for(int i=0;i<ans.size();i++)
+    {
+    	cout<<ans[i]<<" ";
+    }
+    ans.clear();
+    cout<<endl;
+    
+    inorder(root,ans);
+    cout<<"Inorder Traversal of Binary Tree :- "<<endl;
+    for(int i=0;i<ans.size();i++)
+    {
+    	cout<<ans[i]<<" ";
+    }
+    ans.clear();
+    cout<<endl;
+    
+    postorder(root,ans);
+    cout<<"Postorder Traversal of Binary Tree :- "<<endl;
+    for(int i=0;i<ans.size();i++)
+    {
+    	cout<<ans[i]<<" ";
+    }
+    return 0;
+}
+
+
+/* In form of integer */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+   public:
+      int data;
+      Node *left, *right;
+      Node(int value)
+      {
+         data = value;
+         left = NULL;
+         right = NULL;
+      }
+};
+
+void preorder(Node* root,vector<int>&ans)
+{
+	if(root!=NULL)
+	{
+		ans.push_back(root->data);
+		preorder(root->left,ans);
+		preorder(root->right,ans);
+	}
+}
+
+void inorder(Node* root,vector<int>&ans)
+{
+	if(root!=NULL)
+	{
+		inorder(root->left,ans);
+		ans.push_back(root->data);
+		inorder(root->right,ans);
+	}
+}
+
+void postorder(Node* root,vector<int>&ans)
+{
+	if(root!=NULL)
+	{
+		postorder(root->left,ans);
+		postorder(root->right,ans);
+		ans.push_back(root->data);
+	}
+}
+
+Node* solve(int arr[],int n)
+{
+	int i=0;
+	Node* root = new Node(arr[i++]);
+	queue<Node*>q;
+	q.push(root);
+	
+	while(!q.empty())
+	{
+		Node* front = q.front();
+		q.pop();
+		
+		if(i<n)
+		{
+			if(arr[i]==-1)
+			{
+				front->left=NULL;
+			}
+			else
+			{
+				Node* temp = new Node(arr[i]);
+				front->left=temp;
+				q.push(front->left);
+			}
+			i++;
+		}
+		if(i<n)
+		{
+			if(arr[i]==-1)
+			{
+				front->right=NULL;
+			}
+			else
+			{
+				Node* temp = new Node(arr[i]);
+				front->right=temp;
+				q.push(front->right);
+			}
+			i++;
+		}
+	}
+	return root;
+}
+
+int main()
+{
+    int arr[13] = {1,2,3,4,-1,6,7,-1,8,9,-1,-1,10}; /* Level order traversal*/
+    Node* root = solve(arr,13);
     vector<int>ans;
     
     preorder(root,ans);
