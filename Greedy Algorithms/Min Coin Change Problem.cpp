4a36aa820b28b53e959cb3c8d794196939e8e927
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-09-23 11:20:31 +0530

    Create Min Coin Change Problem.cpp

---
+++ Greedy Algorithms/Min Coin Change Problem.cpp
@@ -0,0 +1,16 @@
+int findMinimumCoins(int V) 
+{
+    vector < int > ans;
+    int n = 9;
+    int coins[] = {1, 2, 5, 10, 20, 50, 100, 500, 1000};
+    
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (V >= coins[i])
+        {
+            V -= coins[i];
+            ans.push_back(coins[i]);
+        }
+    }
+    return ans.size();  
+}
