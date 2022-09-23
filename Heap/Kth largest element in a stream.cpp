db0c275167e1e733aa1c7b71d453b24d6e8bcd66
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-09-23 23:29:17 +0530

    Update Kth largest element in a stream.cpp

--- Heap/Kth largest element in a stream.cpp
+++ Heap/Kth largest element in a stream.cpp
@@ -1,5 +1,7 @@
 /* Time :- O(Nlogk) Space :- O(K) */
 
+/* GeeksForGeeks */
+
 class Solution
 {
   public:
@@ -26,5 +28,38 @@ class Solution
             }
         }
         return ans;   
+    }
+};
+
+/* Leetcode */
+
+class KthLargest
+{
+public:
+    
+    priority_queue<int,vector<int>,greater<int>>pq;
+    int track=INT_MIN;
+    
+    KthLargest(int k, vector<int>& nums)
+    {
+        track=k;
+        for(int i=0; i<nums.size(); i++)
+        {
+            m.push(nums[i]);
+            if(m.size()>k)
+            {
+                m.pop();
+            }
+        }
+    }
+    
+    int add(int val)
+    {
+        m.push(val);
+        if(m.size()>track)
+        {
+            m.pop();
+        }
+        return m.top();
     }
 };
