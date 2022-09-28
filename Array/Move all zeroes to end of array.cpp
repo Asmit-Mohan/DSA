6c4c268b7c0496e0737ca26a9b76b5b8c0c821bd
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-09-28 21:23:20 +0530

    Update Move all zeroes to end of array.cpp

--- Array/Move all zeroes to end of array.cpp
+++ Array/Move all zeroes to end of array.cpp
@@ -1,22 +1,28 @@
 class Solution
 {
 public:
-	void pushZerosToEnd(int arr[], int n)
-	{
-	    int i=0;
-	    int j=0;
-	    while(j<n)
-	    {
-	        if(arr[j]!=0)
-	        {
-	            swap(arr[i],arr[j]);
-	            i++;
-	            j++;
-	        }
-	        else
-	        {
-	            j++;
-	        }
-	    }
-	}
+    void moveZeroes(vector<int>& nums)
+    {
+        int i=0;
+        int j=0;
+        
+        while(i<nums.size()&&j<nums.size())
+        {
+            if(nums[i]==0&&nums[j]!=0)
+            {
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }
+            else if(nums[i]==0&&nums[j]==0)
+            {
+                j++;
+            }
+            else if(nums[i]!=0&&nums[j]!=0)
+            {
+                i++;
+                j++;
+            }
+        }
+    }
 };
