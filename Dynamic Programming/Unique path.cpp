071011709951fa49ab157bbcc40b1c3ecb14e4e6
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-09-29 11:11:08 +0530

    Update Unique path.cpp

--- Dynamic Programming/Unique path.cpp
+++ Dynamic Programming/Unique path.cpp
@@ -59,3 +59,21 @@ class Solution
         return dp[m-1][n-1]%mod;
     }
 };
+
+/* Approach 3 [ Most Optimised Approach ] */
+
+class Solution
+{
+public:
+    int uniquePaths(int m, int n)
+    {
+        long ans=1;
+        int N=m+n-2;
+        int r=m-1;
+        for(int i=1;i<=r;i++)
+        {
+            ans=ans*(N-r+i)/i;
+        }
+        return (int)ans;
+    }
+};
