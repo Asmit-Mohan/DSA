5aef593fd36d3757b74797c99bc86fcc863c84fd
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2022-11-03 00:39:08 +0530

    Update Stack using 2 queue.cpp

--- Stack/Stack using 2 queue.cpp
+++ Stack/Stack using 2 queue.cpp
@@ -1,39 +1,49 @@
 /* Time :- O(1) for push() and O(N) for pop() && Space :- O(1) for both method */
 
-void QueueStack :: push(int x)
+class Stack
 {
-    q1.push(x);
-    while(!q1.empty()) 
-    {
-        q2.push(q1.front());
-        q1.pop();
-    }
-}
-int QueueStack :: pop()
-{
-        int temp;
-        int count=0;
-        if(q2.empty())
-        {
-            return -1;
-        }
-        else
-        {
-            while(!q2.empty())
-            {
-                q1.push(q2.front());
-                q2.pop();
-                count++;
-            }
-            count--;
-            while(count--)
-            {
+	queue<int> q1, q2;
+	public:
+	
+	void push(int x)
+	{
+		q2.push(x);
+		while (!q1.empty())
+		{
+			q2.push(q1.front());
+			q1.pop();
+		}
+		queue<int> q = q1;
+		q1 = q2;
+		q2 = q;
+	}
 
-                 q2.push(q1.front());
-                 q1.pop();
-            }
-            temp=q1.front();
-            q1.pop();
-        }
-        return temp;     
-}
+	void pop()
+	{
+		if (q1.empty())
+		{
+			return;
+		}
+		else
+		{
+			q1.pop();
+		}
+	}
+
+	int top()
+	{
+		if (q1.empty())
+		{
+			return -1;
+		}
+		else
+		{
+			return q1.front();
+		}
+	}
+
+	int size()
+	{
+		return q1.size();
+	}
+};
