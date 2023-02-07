23b4fa4a1b93559cbe36a6fa3ab68cc87605436e
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2023-02-08 00:59:51 +0530

    Update Product array puzzle.cpp

--- Array/Product array puzzle.cpp
+++ Array/Product array puzzle.cpp
@@ -41,6 +41,34 @@ class Solution
     }
 }
 
+/* Time :- O(N) Space :- O(N) */
+
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int>& nums)
+    {
+        vector<int>left(nums.size(),1);
+        vector<int>right(nums.size(),1);
+        vector<int>ans;
+        int n=nums.size();
+        
+        for(int i=1;i<n;i++)
+        {
+            left[i]=left[i-1]*nums[i-1];
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=right[i+1]*nums[i+1];
+        }
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back(left[i]*right[i]);
+        }
+        return ans;
+    }
+};
+
 /* Time :- O(N) Space :- O(1) */
 
 class Solution
