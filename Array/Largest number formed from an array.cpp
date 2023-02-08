a23e9d53b5cefc21cb92ea064d58c9fdb4a16811
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2023-02-08 19:03:23 +0530

    Update Largest number formed from an array.cpp

--- Array/Largest number formed from an array.cpp
+++ Array/Largest number formed from an array.cpp
@@ -1,44 +1,38 @@
 class Solution
 {
 public:
-    
-    static int comp(string a,string b)
+    bool static comp(string &a,string &b)
     {
-        string ab=a.append(b);
-        string ba=b.append(a);
-        return ab.compare(ba)>0?1:0;
+        string ab = a+b;
+        string ba = b+a;
+        return ab>ba;
     }
-    
-	string printLargest(vector<string> &arr)
-	{
-	    int count=0;
-	    string ans="";
-	    vector<string>temp;
+
+    string largestNumber(vector<int>& nums)
+    {
+        string ans="";
+        int count=0;
+        vector<string>temp;
 	    
-	    for(int i=0;i<arr.size();i++)
-	    {
-	        if(arr[i]=="0")
-	        {
-	            count++;
-	        }
-	        temp.push_back(arr[i]);
-	    }
-	    
-	    sort(temp.begin(),temp.end(),comp);
-	    if(count==arr.size())
-	    {
-	        while(count--)
-	        {
-	            ans+=to_string(0);
-	        }
-	    }
-	    else
-	    {
-	        for(int i=0;i<temp.size();i++)
-	        {
-	           ans+=temp[i];
-	        }
-	    }
-	    return ans;
-	}
+        for(auto x : nums)
+        {
+            if(x==0)
+            {
+                count++;
+            }
+            temp.push_back(to_string(x));
+        }
+
+        if(count==nums.size())
+        {
+            return "0";
+        }
+        sort(temp.begin(),temp.end(),comp);
+
+        for(auto x : temp)
+        {
+            ans+=x;
+        }
+        return ans;        
+    }
 };
