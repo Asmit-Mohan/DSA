c811decc7516b580e21d30a1c388dfa8ba087db9
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2023-03-29 14:28:44 +0530

    Update Maximum Product Subarray.cpp

--- Array/Maximum Product Subarray.cpp
+++ Array/Maximum Product Subarray.cpp
@@ -1,3 +1,31 @@
+/* Approach 1 */
+
+class Solution
+{
+public:
+    int maxProduct(vector<int>&nums)
+    {
+        int ans=nums[0];
+        int mx=ans;
+        int mn=ans;
+        
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]<0)
+            {
+               swap(mx,mn);
+            }
+
+            mx=max(nums[i],mx*nums[i]);
+            mn=min(nums[i],mn*nums[i]);
+            ans=max(ans,mx);
+        }
+        return ans;    
+    }
+};
+
+/* Approach 2 */
+
 class Solution
 {
 public:
