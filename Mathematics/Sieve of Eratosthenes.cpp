fb2b8af1aae8208c0b24868a02fca99f55c2b6e2
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2023-05-21 11:57:28 +0530

    Update Sieve of Eratosthenes.cpp

--- Mathematics/Sieve of Eratosthenes.cpp
+++ Mathematics/Sieve of Eratosthenes.cpp
@@ -1,30 +1,34 @@
+/* Time :- O(NloglogN) Space :- O(N) */
+   
 class Solution
 {
 public:
     vector<int> sieveOfEratosthenes(int N)
     {
-        bool arr[N+1];
-        memset(arr, true, sizeof(arr));
-        arr[0]=false;
-        arr[1]=false;
-        for(int i=2;i<N;i++)
-        {
-            if(arr[i]==true)
-            {
-                for(int j=i*i;j<=N;j=j+i)
-                {
-                    arr[j]=false;
-                }
-            }
-        }
-        vector<int>v;
-        for(int i=2;i<=N;i++)
-        {
-            if(arr[i]==true)
-            {
-                v.push_back(i);
-            }
-        }
-        return v;
+       vector<int>ans;
+       vector<bool>hash(N+1,true);
+       
+       hash[0]=false;
+       hash[1]=false;
+       
+       for(int i=2;i<=N;i++)
+       {
+           if(hash[i])
+           {
+               for(int j=i*i;j<=N;j=j+i)
+               {
+                   hash[j]=false;
+               }
+           }
+       }
+       
+       for(int i=0;i<=N;i++)
+       {
+           if(hash[i])
+           {
+               ans.push_back(i);
+           }
+       }
+       return ans;
     }
 };
