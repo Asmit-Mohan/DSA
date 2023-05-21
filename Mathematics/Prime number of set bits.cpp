7285f296dc26ea6bcc75e4a3e0418810001d3ddf
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2023-05-21 12:05:40 +0530

    Update Prime number of set bits.cpp

--- Mathematics/Prime number of set bits.cpp
+++ Mathematics/Prime number of set bits.cpp
@@ -1,32 +1,77 @@
+/* Approch 1 */
+
 class Solution
 {
 public:
-bool isprime(int n)
-{
-    if (n<2)
+    bool isprime(int n)
     {
-        return false;
-    }
-    for(int i=2;i*i<=n;i++)
-    {
-        if(n%i==0)
+        if (n<2)
         {
             return false;
         }
-    }
-    return true;
-}
-int primeSetBits(int L, int R)
-{
-    int c=0;
-    for(int i=L;i<=R;i++)
-    {
-        int p=__builtin_popcount(i);
-        if(isprime(p))
+        for(int i=2;i*i<=n;i++)
         {
-            c++; 
+            if(n%i==0)
+            {
+                return false;
+            }
         }
+        return true;
     }
-    return c;
-}
+    int primeSetBits(int L, int R)
+    {
+        int c=0;
+        for(int i=L;i<=R;i++)
+        {
+            int p=__builtin_popcount(i);
+            if(isprime(p))
+            {
+                c++; 
+            }
+        }
+        return c;
+    }
+};
+
+
+/* Approach 2 */
+
+class Solution
+{
+public:
+    bool isprime(int n)
+    {
+        if (n<2)
+        {
+            return false;
+        }
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    int primeSetBits(int L, int R)
+    {
+        int c=0;
+        int dp[R+1];
+        
+        dp[0]=0;
+        for(int i=1;i<=R;i++)
+        {
+            dp[i]=dp[i/2]+i%2;
+        }
+        
+        for(int i=L;i<=R;i++)
+        {
+            if(isprime(dp[i]))
+            {
+                c++; 
+            }
+        }
+        return c;
+    }
 };
