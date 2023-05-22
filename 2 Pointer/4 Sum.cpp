c8eba2a011a144a179e60fb4e9a8bf6091641b3e
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2023-05-22 08:54:53 +0530

    Update 4 Sum.cpp

--- 2 Pointer/4 Sum.cpp
+++ 2 Pointer/4 Sum.cpp
@@ -3,6 +3,68 @@
 class Solution
 {
 public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target)
+    {
+        int n=nums.size();
+        if (n < 4)
+        {
+            return {};
+        }
+        sort(nums.begin(),nums.end());
+        set<vector<int>>s;
+        vector<vector<int>>ans;
+
+        for(int i=0;i<n-2;i++)
+        {
+            if(i>0&&nums[i]==nums[i-1])
+            {
+                continue;
+            }
+            for(int j=i+1;j<n-1;j++)
+            {
+                if(j>(i+1)&&nums[j]==nums[j-1])
+                {
+                    continue;
+                }
+                int l=j+1;
+                int r=n-1;   
+                while(l<r)
+                {
+                    long long temp1 = (long long)nums[i]+nums[j];
+                    long long temp2 = (long long)nums[l]+nums[r];
+                    long long temp = temp1 + temp2;
+
+                    if(temp==target)
+                    {
+                        s.insert({nums[i],nums[j],nums[l],nums[r]});
+                        l++;
+                        r--;
+                    }   
+                    else if(temp>target)
+                    {
+                        r--;
+                    }
+                    else
+                    {
+                        l++;
+                    }
+                }
+            }   
+        }
+
+        for(auto it : s)
+        {
+            ans.push_back(it);
+        }
+        return ans;    
+    }
+};
+
+/* Approach 2 */
+
+class Solution
+{
+public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) 
     {
         vector<vector<int>>ans;
@@ -55,70 +117,5 @@ public:
             }
         }
         return ans;
-    }
-};
-
-/* Approach 2 */
-
-/* Time :- O(N*N*N) Space :- O(1) --> No extra space instead of answer vector */
-
-class Solution
-{
-public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) 
-    {
-        
-     vector<vector<int>>ans;
-     int n = nums.size();
-     sort(nums.begin() , nums.end());
-     
-     for(int i = 0 ; i <n;i++)
-     {
-         long long int temp1 = target - nums[i];
-         
-         for(int j = i+1 ; j<n;j++)
-         {
-             int l = j+1;
-             int r = n-1;
-             long long int temp2 = temp1 - nums[j];
-             
-             while(l<r)
-             {
-                 long long int sum = nums[l] + nums[r];
-                 
-                 if(sum == temp2)
-                 {
-                     ans.push_back({nums[i] , nums[j] , nums[l] , nums[r]});
-                     
-                 }
-                 if(temp2 > sum)
-                 {
-                     
-                     while(l+1 < n and nums[l] == nums[l+1])
-                     {
-                         l++;
-                     }
-                     l++;
-                 }
-                 else
-                 {
-                     while(r>1 and nums[r] == nums[r-1])
-                     {
-                         r--;
-                     }
-                     r--;
-                 }
-             }
-             while(j+1 < n and nums[j] == nums[j+1])
-             {
-                 j++;
-             }
-         }
-         while(i+1 < n and nums[i] == nums[i+1])
-         {
-             i++;
-         }
-      }
-      return ans;
     }
 };
