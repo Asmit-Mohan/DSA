1024bab5bf620a96608c288b0f443bf8e00afa12
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2023-05-22 07:10:52 +0530

    Update 3 Sum Closest.cpp

--- 2 Pointer/3 Sum Closest.cpp
+++ 2 Pointer/3 Sum Closest.cpp
@@ -5,39 +5,37 @@ class Solution
 public:
     int threeSumClosest(vector<int>& nums, int target)
     {
-         int n=nums.size();
-         int ans=nums[0]+nums[1]+nums[2];
-        
-         if(nums.size()==3)
-         {
-             return nums[0]+nums[1]+nums[2];
-         }
-         sort(nums.begin(),nums.end());
-         for(int i=0;i<n-1;i++)
-         {
-             int l=i+1;
-             int r=n-1;
-             while(l<r)
-             {
-                  int sum=nums[i]+nums[l]+nums[r];
-                  if(abs(target-sum)<abs(target-ans))
-                  {
-                      ans=sum;
-                  }
-                  if(abs(target-sum)==abs(target-ans))
-                  {
-                      ans=min(ans,sum);
-                  }
-                  if(target>sum)
-                  {
-                       l++;
-                  }
-                  else
-                  {
-                      r--;
-                  }
-             }
-         }
-        return ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int ans=INT_MAX;
+        int res=-1;
+
+        for(int i=0;i<n-1;i++)
+        {
+            int l=i+1;
+            int r=n-1;
+            while(l<r)
+            {
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum==target)
+                {
+                    return sum;
+                }
+                else if(sum>target)
+                {
+                    r--;
+                }
+                else
+                {
+                    l++;
+                }
+                if(abs(sum-target)<ans)
+                {
+                    ans=min(ans,abs(sum-target));
+                    res=sum;
+                }
+            }
+        }
+        return res;    
     }
 };
