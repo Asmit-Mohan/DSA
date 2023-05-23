d8dd743bc30866b2cf745200eb06dbfbb6c8b121
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2023-05-23 18:11:43 +0530

    Update Stickler Thief.cpp

--- Dynamic Programming/Stickler Thief.cpp
+++ Dynamic Programming/Stickler Thief.cpp
@@ -37,28 +37,26 @@ class Solution
 
 class Solution
 {
-    public:
-    int FindMaxSum(int arr[], int n)
+public:
+    int rob(vector<int>& nums)
     {
+        int n=nums.size();
+        if(n==0)
+        {
+            return 0;
+        }
         if(n==1)
         {
-            return arr[0];
+            return nums[0];
         }
-        if(n==2)
+        int dp[100]={0};
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+
+        for(int i=2;i<n;i++)
         {
-            return max(arr[0],arr[1]);
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
         }
-        
-        int dp[n+1];
-        dp[0]=arr[0];
-        dp[1]=arr[1];
-        dp[2]=arr[0]+arr[2];
-        
-        for(int i=3;i<n;i++)
-        {
-            dp[i] = arr[i] + max(dp[i-2],dp[i-3]);
-        }
-        
-        return max(dp[n-1],dp[n-2]);
+        return dp[n-1];    
     }
 };
