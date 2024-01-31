7b986ab8689559e6483a9287f309ce08fd02981f
ASMIT MOHAN <83742032+Asmit-Mohan@users.noreply.github.com>
2024-01-31 23:27:59 +0530

    Update Evaluation of Postfix Expression.cpp
    Added Leetcode Version

--- Stack/Evaluation of Postfix Expression.cpp
+++ Stack/Evaluation of Postfix Expression.cpp
@@ -1,4 +1,97 @@
-/* Time :- O(N) Space :- O(N) */
+/* Leetcode Version */
+
+class Solution
+{
+public:
+
+bool is_digits(string &str)
+{
+    int start = (str[0] == '-' && str.length()>1) ? 1 : 0;
+    for (int i = start;i<str.length();i++)
+    {
+        char ch  = str[i];
+        if (!(ch >= 48 && ch <= 57))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+    char assignOperator(string &s)
+    {
+        char op;
+        if(s=="/")
+        {
+            op='/';
+        }
+        else if(s=="*")
+        {
+            op='*';
+        }
+        else if(s=="+")
+        {
+            op='+';
+        }
+        else
+        {
+            op='-';
+        }
+        return op;
+    }
+    int evaluatePostfix(vector<string>&S)
+    {
+        stack<int>s;
+        char op;
+        
+        for(int i=0;i<S.size();i++)
+        {
+            if(is_digits(S[i]))
+            {
+                s.push(stoi(S[i]));
+            }
+            else
+            {
+                op = assignOperator(S[i]);
+                int val1=s.top();
+                s.pop();
+                int val2=s.top();
+                s.pop();
+                
+                switch(op)
+                {
+                    case '+':
+                    s.push(val2+val1);
+                    break;
+                    
+                    case '-':
+                    s.push(val2-val1);
+                    break;
+                    
+                    case '*':
+                    s.push(val2*val1);
+                    break;
+                    
+                    case '/':
+                    if (val1 != 0)
+                    {
+                        s.push(val2 / val1);
+                    }
+                    else
+                    {
+                        return 0;  
+                    }
+                    break;
+                }
+            }
+        }
+        return s.top();
+    }
+    int evalRPN(vector<string>& tokens)
+    {
+        return evaluatePostfix(tokens); 
+    }
+};
+/* GFG Version Time :- O(N) Space :- O(N) */
 
 class Solution
 {
